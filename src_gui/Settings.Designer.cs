275fefc74842632fd4f97cc220881f13ec6ae79d
Matěj Zábský <mzabsky@gmail.com>
2009-12-28 19:26:57 +0000

    Studio: Saving and loading config now finally works

---
+++ src_gui/Settings.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace GeoGen_Studio
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.settingsGrid = new System.Windows.Forms.PropertyGrid();
+            this.save = new System.Windows.Forms.Button();
+            this.cancel = new System.Windows.Forms.Button();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.settingsGrid);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.cancel);
+            this.splitContainer1.Panel2.Controls.Add(this.save);
+            this.splitContainer1.Size = new System.Drawing.Size(917, 618);
+            this.splitContainer1.SplitterDistance = 567;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // settingsGrid
+            // 
+            this.settingsGrid.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.settingsGrid.Location = new System.Drawing.Point(0, 0);
+            this.settingsGrid.Name = "settingsGrid";
+            this.settingsGrid.Size = new System.Drawing.Size(917, 567);
+            this.settingsGrid.TabIndex = 0;
+            // 
+            // save
+            // 
+            this.save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.save.Location = new System.Drawing.Point(757, 10);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(148, 25);
+            this.save.TabIndex = 0;
+            this.save.Text = "Save Settings";
+            this.save.UseVisualStyleBackColor = true;
+            // 
+            // cancel
+            // 
+            this.cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancel.Location = new System.Drawing.Point(603, 10);
+            this.cancel.Name = "cancel";
+            this.cancel.Size = new System.Drawing.Size(148, 25);
+            this.cancel.TabIndex = 1;
+            this.cancel.Text = "Cancel";
+            this.cancel.UseVisualStyleBackColor = true;
+            // 
+            // Settings
+            // 
+            this.AcceptButton = this.save;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cancel;
+            this.ClientSize = new System.Drawing.Size(917, 618);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Settings";
+            this.Text = "Settings";
+            this.Load += new System.EventHandler(this.Settings_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PropertyGrid settingsGrid;
+        private System.Windows.Forms.Button cancel;
+        private System.Windows.Forms.Button save;
+    }
+}
---
+++ src_gui/Settings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GeoGen_Studio
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Settings_Load(object sender, EventArgs e)
+        {
+            settingsGrid.SelectedObject = Main.Get().GetConfig();
+        }
+    }
+}
