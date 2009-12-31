10beb36492ab070d207e94bc9da3bee2b43335e7
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2009-12-31 09:15:37 +0000

    Studio: Make the About dialog fade in and fade out (just some eyecandy)

--- trunk/src_gui/About.cs
+++ trunk/src_gui/About.cs
@@ -11,6 +11,8 @@ namespace GeoGen_Studio
 {
     public partial class About : Form
     {
+        bool modeIn = true; // fade in mode (fade out when false)
+
         public About()
         {
             InitializeComponent();
@@ -23,7 +25,36 @@ namespace GeoGen_Studio
 
         private void button_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.opacityTimer.Enabled = true;
+        }
+
+        private void About_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opacityTimer_Tick(object sender, EventArgs e)
+        {
+            if (modeIn == true)
+            {
+                this.Opacity += 0.04;
+
+                if (this.Opacity == 1.0)
+                {
+                    this.opacityTimer.Enabled = false;
+                    this.modeIn = false;
+                }
+            }
+            else
+            {
+                this.Opacity -= 0.04;
+
+                if (this.Opacity == 0.0)
+                {
+                    this.opacityTimer.Enabled = false;
+                    this.Close();
+                }
+            }
         }
     }
 }
