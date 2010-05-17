df68ad26746cdbaf66c885abebf826c51b9e03f8
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-05-17 17:54:56 +0000

    Core: Fixed division by zero in 2D::Combine.

--- trunk/src/ggen_data_2d.h
+++ trunk/src/ggen_data_2d.h
@@ -21,6 +21,9 @@
 
 #include "ggen_support.h"
 #include "ggen_path.h"
+#include <iostream>
+
+using namespace std;
 
 /**
  * GGen_Data_2D represents 2-dimensional array of values. Most straightforward interpretation of such array is a bitmap, where black represents some minimal value (usually 0) and white represents some maximal value (in case of 8 bit per channel bitmaps usually 255). You will probably realize many of the GGen_Data_2D functions strongly resemble functions from classical bitmap editors - <a href="#2d_add">adding</a> changes brightness, <a href="#2d_multiply">multiplying</a> changes contrast, <a href="#2d_transformvalues">curves</a>... Working with multiple 2D arrays with GGen is generally very similiar to working with layer enabled bitmap editor, just without a fancy GUI.
@@ -533,5 +536,11 @@ class GGen_Data_2D{
 		 * @return The minimum value found on the path.
 		 **/
 		GGen_Height GetMinValueOnPath(GGen_Path* path);
+
+
+
+		static void NumInstances(){
+			cout << GGen_Data_2D::num_instances<<endl;
+		}
 };
 
