aa2621fbcc360cc655a8c469ed41583415dc0167
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-09-04 15:56:52 +0000

    [WPF Branch] Studio: Implemented basic plug-in loader

--- branches/wpf/src_studio/core/GeoGen Studio Core/App.xaml.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.Reflection;
 
 namespace GeoGen_Studio
 {
@@ -12,5 +13,45 @@ namespace GeoGen_Studio
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            try
+            {
+                PlugInLoader loader = new PlugInLoader();
+
+                /* Load "plug-ins" from this executable */
+                loader.ParseAssembly(Assembly.GetExecutingAssembly());
+
+                /* Load plug-ins from the plug-in directory */
+                loader.ParseDirectory("./plugins");
+
+                /* Create plug-in instances and register their relationships */
+                loader.ExecuteAllRegistrators();
+
+                /* Check if all plug-ins successfully loaded */
+                List<string> messages = new List<string>();
+                foreach (Registrator registrator in loader.Registrators)
+                {
+                    if(!registrator.Failed) continue;
+
+                    string message = registrator.ToString() + ": " + registrator.FailureType.ToString();
+
+                    if(registrator.Exception != null){
+                        message += " (" + registrator.Exception + ")";
+                    }
+
+                    messages.Add(message);
+                }
+
+                if(messages.Count > 0){
+                    MessageBox.Show("One or more plug-ins completely or partially failed to load" + Environment.NewLine + Environment.NewLine + "Details:" + Environment.NewLine + Environment.NewLine + String.Join(Environment.NewLine, messages), "GeoGen Studio", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            /* Any unhandled exception in this phase means the application cannot continue */
+            catch(Exception e){
+                MessageBox.Show("GeoGen Studio plug-in loader failed with following message:" + Environment.NewLine + Environment.NewLine + e.Message, "GeoGen Studio", MessageBoxButton.OK, MessageBoxImage.Error);
+                Application.Current.Shutdown();
+            }
+        }
     }
 }
