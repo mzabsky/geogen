5bfded35b4ac34748d3ca8dd41e73607d7db8f71
Matěj Zábský <mzabsky@gmail.com>
2010-09-04 10:23:33 +0000

    [WPF Branch] Studio: Removed old WinForms Studio, created stub for a new one.

--- src_studio/core/GeoGen Studio Core/Properties/Resources.Designer.cs
+++ src_studio/core/GeoGen Studio Core/Properties/Resources.Designer.cs
@@ -59,33 +59,5 @@ namespace GeoGen_Studio.Properties {
                 resourceCulture = value;
             }
         }
-        
-        internal static System.Drawing.Bitmap vsobject_class {
-            get {
-                object obj = ResourceManager.GetObject("vsobject_class", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        internal static System.Drawing.Bitmap vsobject_constant {
-            get {
-                object obj = ResourceManager.GetObject("vsobject_constant", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        internal static System.Drawing.Bitmap vsobject_enumitem {
-            get {
-                object obj = ResourceManager.GetObject("vsobject_enumitem", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        internal static System.Drawing.Bitmap vsobject_method {
-            get {
-                object obj = ResourceManager.GetObject("vsobject_method", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
