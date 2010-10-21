fe4b147aa6680d076e5b5f70832dcc4b5a9569a1
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-21 16:00:32 +0000

    [WPF Branch] Studio: Added basic DockManager.

---
+++ branches/wpf/src_studio/plugins/DockManager/IDockManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace GeoGen_Studio
+{
+    public interface IDockManager: IPlugInInterface
+    {
+        bool AddAsDocumentContent(object content, string title, bool focus = false);
+        bool AddAsDockableContent(object content, string title, bool showInMainArea = false);
+        bool AddAsFloatableWindow(object content, string title, Size size, bool dockable = true);
+    }
+}
