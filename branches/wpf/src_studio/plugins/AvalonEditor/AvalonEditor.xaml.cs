a90308121f3e5caf637d0aec79d558bb20e0b17b
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-21 16:19:01 +0000

    [WPF Branch] Studio: Added AvalonEditor.

---
+++ branches/wpf/src_studio/plugins/AvalonEditor/AvalonEditor.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GeoGen_Studio
+{
+    public partial class AvalonEditor : UserControl, IPlugIn
+    {
+        public AvalonEditor()
+        {
+            InitializeComponent();
+        }
+
+        public void Register(IDockManager dockManager)
+        {
+            dockManager.AddAsDocumentContent(this, "Code", true);
+        }
+    }
+}
