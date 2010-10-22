38c3956e1b4b11862d4885204882f8bda0be2adb
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-22 10:45:52 +0000

    [WPF Branch] Studio: Actually change Messaging namespace to Utilities.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Messaging/MessageThrownEventHandler.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Messaging/MessageThrownEventHandler.cs
@@ -1,4 +1,4 @@
-﻿namespace GeoGen.Studio.Messaging
+﻿namespace GeoGen.Studio.Utilities.Messaging
 {
     public delegate void MessageThrownEventHandler(object sender, MessageThrownEventArgs args);
 }
