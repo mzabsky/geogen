9e651a1a000ed3a6f6a5995f7bf2b7afd4594e91
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-22 11:04:35 +0000

    [WPF Branch] Studio: Added Utilities.PlugInBase.Control.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Control.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Control.cs
@@ -1,13 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using GeoGen.Studio.Utilities.Messaging;
 
 namespace GeoGen.Studio.Utilities.PlugInBase
 {
-    class Control
+    class Control: UserControl, INotifyPropertyChanged
     {
+        public static readonly RoutedEvent MessageThrownEvent =
+            EventManager.RegisterRoutedEvent("MessageThrown", RoutingStrategy.Bubble,
+            typeof(MessageThrownEventHandler), typeof(Control));
 
+        public event PropertyChangedEventHandler PropertyChanged;
 
+        public event RoutedEventHandler MessageThrown
+        {
+            add
+            {
+                AddHandler(MessageThrownEvent, value);
+            }
+            remove
+            {
+                RemoveHandler(MessageThrownEvent, value);
+            }
+        }
+
+        protected void ThrowMessage(Message message)
+        {
+            RaiseEvent(new MessageThrownEventArgs(message, Control.MessageThrownEvent));
+        }
+
+        protected void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
     }
 }
