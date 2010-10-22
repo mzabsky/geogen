f7e8d6b2a40fd9a4f12e01cd008ec925e73e4fe7
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-22 09:29:23 +0000

    [WPF Branch] Studio: Added Utilities.FileDialog.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/FileDialog.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.Win32;
+using System.IO;
+
+namespace GeoGen.Studio.Utilities
+{
+    public static class FileDialog
+    {
+        private static string ShowBase(string path, string filter, bool isSaveDialog)
+        {
+            Microsoft.Win32.FileDialog dialog;
+
+            if (isSaveDialog) dialog = new SaveFileDialog();
+            else dialog = new OpenFileDialog();
+
+            dialog.Filter = filter;
+
+            // add Examples shortcut, if the directory exists
+            if (Directory.Exists(@"../examples"))
+            {
+                dialog.CustomPlaces.Add(new FileDialogCustomPlace(@"../examples"));
+            }
+
+            try
+            {
+                FileInfo info = new FileInfo(path);
+                dialog.InitialDirectory = info.DirectoryName;
+                dialog.FileName = info.Name;
+            }
+            // path  is incorrect -> let the dialog reset on defaults
+            catch { };
+
+            bool? result = dialog.ShowDialog();
+
+            if (result == true)
+            {
+                path = dialog.FileName;
+
+                return path;
+            }
+
+            // user pressed cancel
+            throw new Exception();
+        }
+
+        public static string ShowOpen(string path, string filter)
+        {
+            return FileDialog.ShowBase(path, filter, false);
+        }
+
+        public static string ShowSave(string path, string filter)
+        {
+            return FileDialog.ShowBase(path, filter, true);
+        }
+    }
+}
