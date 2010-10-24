1c2bf54ef0c7050c094139f716353f6fb80617e8
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-24 19:52:23 +0000

    [WPF Branch] Studio: Added basic configurability support (Utilities.Configurability).

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Configurability/ConfigurableAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GeoGen.Studio.Utilities.Configurability
+{
+    [AttributeUsage(AttributeTargets.Property, Inherited = true, AllowMultiple = false)]
+    public class ConfigurableAttribute : Attribute
+    {
+        public object DefaultValue { get; private set; }
+
+        public ConfigurableAttribute(object defaultValue)
+        {
+            this.DefaultValue = defaultValue;
+        }
+    }
+}
---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Configurability/PropertyStore.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace GeoGen.Studio.Utilities.Configurability
+{
+    [XmlRoot("PropertyStore")]
+    [EditorBrowsable(System.ComponentModel.EditorBrowsableState.Never)]
+    public class PropertyStore : Dictionary<string, Dictionary<string, object>>, IXmlSerializable
+    {
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            return null;
+        }
+
+        public void ReadXml(System.Xml.XmlReader reader)
+        {
+            bool wasEmpty = reader.IsEmptyElement;
+
+            reader.Read();
+
+            if (wasEmpty)
+            {
+                return;
+            }
+
+            while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+            {
+                reader.ReadStartElement("item");
+
+                reader.ReadStartElement("plugIn");
+                string plugInName = reader.ReadString();
+                reader.ReadEndElement();
+
+                reader.ReadStartElement("property");
+                string propertyName = reader.ReadString();
+                reader.ReadEndElement();
+
+                reader.ReadStartElement("type");
+                Type valueType = Type.GetType(reader.ReadString());
+                reader.ReadEndElement();
+
+                /* If the type failed to initialize, don't create the XmlSerializer object, the deserialization will
+                 * then crash on NullReferenceException and the catch statement will make sure the item is ignored. */
+                XmlSerializer valueSerializer = null;
+                if (valueType != null)
+                {
+                    valueSerializer = new XmlSerializer(valueType);
+                }
+
+                reader.ReadStartElement("value");
+
+                object value = null;
+                try
+                {
+                    value = valueSerializer.Deserialize(reader);
+                }
+                catch
+                {
+                    /* Try to resume deserialization on next item. */
+                    reader.Skip();
+                    reader.ReadEndElement(); // </value>
+                    reader.ReadEndElement(); // </item>
+                    reader.MoveToContent();
+
+                    continue;
+                }
+
+                reader.ReadEndElement(); // </value>
+                reader.ReadEndElement(); // </item>
+                reader.MoveToContent(); 
+
+                if(!this.ContainsKey(plugInName))
+                {
+                    this.Add(plugInName, new Dictionary<string, object>());
+                }
+
+                if(!this[plugInName].ContainsKey(propertyName))
+                {
+                    this[plugInName].Add(propertyName, value);
+                }
+                else
+                {
+                    this[plugInName][propertyName] = value;
+                }
+            }
+            reader.ReadEndElement();
+        }
+
+        public void WriteXml(System.Xml.XmlWriter writer)
+        {
+            XmlSerializer keySerializer = new XmlSerializer(typeof(Type));
+
+            foreach (string plugInName in this.Keys)
+            {
+                Dictionary<string, object> plugInProperties = this[plugInName];
+
+                foreach(string propertyName in plugInProperties.Keys){
+                    object currentValue = plugInProperties[propertyName];
+
+                    XmlSerializer valueSerializer = new XmlSerializer(currentValue.GetType());
+
+                    writer.WriteStartElement("item");
+
+                    writer.WriteStartElement("plugIn");
+                    writer.WriteValue(plugInName);
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("property");
+                    writer.WriteValue(propertyName);
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("type");
+                    writer.WriteValue(currentValue.GetType().AssemblyQualifiedName);
+                    writer.WriteEndElement();
+
+                    writer.WriteStartElement("value");
+                    valueSerializer.Serialize(writer, currentValue);
+                    writer.WriteEndElement();
+
+                    writer.WriteEndElement(); // </item>
+                }                
+            }
+        }
+    }
+}
