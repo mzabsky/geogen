2a67d8ebabc399d2dcfd84adee2a1f26e920c572
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-25 20:14:26 +0000

    [WPF Branch] Studio: Added documentation comments to PlugInAttribute.cs.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Plug-in Loader/PlugInAttribute.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Plug-in Loader/PlugInAttribute.cs
@@ -5,16 +5,35 @@ using System.Text;
 
 namespace GeoGen.Studio.PlugInLoader
 {
+    /// <summary>
+    /// Specifies whether creation of multiple plug-in instances is allowed in case plug-in's <see cref="Registrator"/>s is called with multiple parameter combinations.
+    /// </summary>
     public enum InstanceCount{
+        /// <summary>
+        /// Create one plug-in instance only and call the <see cref="Registrator"/>s on it.
+        /// </summary>
         One,
+        /// <summary>
+        /// Create a new plug-in instance for each <see cref="Registrator"/> call.
+        /// </summary>
         OnePerRegistration
     };
 
+    /// <summary>
+    /// Attribute specifying how <see cref="PlugInLoader"/> handles the plug-in. */
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = true, AllowMultiple = false)]
     public class PlugInAttribute : Attribute
     {
+        /// <summary>
+        /// Specifies whether creation of multiple plug-in instances is allowed in case plug-in's <see cref="Registrator"/>s is called with multiple parameter combinations.
+        /// </summary>
         public InstanceCount InstanceCount { get; private set; }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PlugInAttribute"/> class.
+        /// </summary>
+        /// <param name="instanceCount">Specifies whether creation of multiple plug-in instances is allowed in case plug-in's <see cref="Registrator"/>s is called with multiple parameter combinations.</param>
         public PlugInAttribute(InstanceCount instanceCount = InstanceCount.One)
         {
             this.InstanceCount = instanceCount;
