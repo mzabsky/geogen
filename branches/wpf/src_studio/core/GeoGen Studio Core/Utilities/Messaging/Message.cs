d60e023aef8f4cd79e59d2849f1bbefe6757f28c
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-25 14:21:13 +0000

    [WPF Branch] Studio: Message now also carries information about its creation time.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Messaging/Message.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Messaging/Message.cs
@@ -1,14 +1,18 @@
-﻿namespace GeoGen.Studio.Utilities.Messaging
+﻿using System;
+
+namespace GeoGen.Studio.Utilities.Messaging
 {
     public class Message
     {
         public string Text {get; set;}
         public MessageType Type {get; set;}
+        public DateTime DateTime { get; set; }
 
         public Message(string text, MessageType type = MessageType.Message)
         {
             this.Text = text;
             this.Type = type;
+            this.DateTime = DateTime.Now;
         }
     }
 }
