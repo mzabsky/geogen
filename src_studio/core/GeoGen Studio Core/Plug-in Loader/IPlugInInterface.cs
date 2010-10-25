9313dd26d50c40a8f7d0c27b08d2911afe25933b
Matěj Zábský <mzabsky@gmail.com>
2010-10-25 21:19:59 +0000

    [WPF Branch] Studio: Documented IPlugIn and IPlugInInterface.

--- src_studio/core/GeoGen Studio Core/Plug-in Loader/IPlugInInterface.cs
+++ src_studio/core/GeoGen Studio Core/Plug-in Loader/IPlugInInterface.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace GeoGen.Studio.PlugInLoader
+﻿namespace GeoGen.Studio.PlugInLoader
 {
+    /// <summary>
+    /// Any interface inheriting this interface will be considered to be plug-in to plug-in dependency. <see cref="Registrator"/>s are not allowed to have any other types as their parameters.
+    /// </summary>
     public interface IPlugInInterface
     {
     }
