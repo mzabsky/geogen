fbcb1060bc04ffae50ce4447ed91702fc0e8181b
Matěj Zábský <mzabsky@gmail.com>
2010-10-28 17:23:40 +0000

    [WPF Branch] Studio: Added Collections.PriorityObservableCollection

---
+++ src_studio/core/GeoGen Studio Core/Utilities/Collections/PriorityComparer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoGen.Studio.Utilities.Collections
+{
+    public class PriorityComparer : IComparer<IPriority>
+    {
+        int Compare(IPriority a, IPriority b)
+        {
+            if (a.Priority > b.Priority)
+                return 1;
+            if (a.Priority < b.Priority)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}
---
+++ src_studio/core/GeoGen Studio Core/Utilities/Collections/PriorityObservableCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GeoGen.Studio.Utilities.Collections
+{ 
+    public class PriorityObservableCollection: SortedObservableCollection<IPriority>
+    {
+        public PriorityObservableCollection() : base(new PriorityComparer()) { }
+    }
+}
