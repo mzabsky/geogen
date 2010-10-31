594e456ad2ace30b6b7b3b88bdd953fa7e6b7c9c
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-10-31 19:15:54 +0000

    Studio: Let the uncaught exception handler catch exceptions from all threads.

--- trunk/src_gui/Program.cs
+++ trunk/src_gui/Program.cs
@@ -24,70 +24,80 @@ using System.Windows.Forms;
 namespace GeoGen_Studio
 {
     static class Program
-    {
+    {         
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
+#if DEBUG
+#else
+            // we will be catching "uncaught exception" and show them in some nice fashion
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            Application.ThreadException += UncaughtExceptionHandler;
+            AppDomain.CurrentDomain.UnhandledException += delegate(object o, UnhandledExceptionEventArgs a){
+                Program.UncaughtExceptionHandler(null, new System.Threading.ThreadExceptionEventArgs(a.ExceptionObject as Exception));
+            };
+#endif
             Main main = null;
 
-            // we will be catching "uncaught exception" and show them in some nice fashion
-#if DEBUG                 
-#else
-            try
-            {
-#endif
-                Application.EnableVisualStyles();
-                Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
 
-                main = new Main();
+            main = new Main();
 
-                if(args.Length > 0){
-                    try{
-                        if(System.IO.File.Exists(args[0])){
-                            main.fileFromShell = args[0];
-                        }
+            if(args.Length > 0){
+                try{
+                    if(System.IO.File.Exists(args[0])){
+                        main.fileFromShell = args[0];
                     }
-                    catch{}
                 }
+                catch{}
+            }
 
-                Application.Run(main);
 
-                // show the exception info dialog only in release mode
+
+                // emergency save will be done by the UncaughtExceptionHandler in release mode
 #if DEBUG 
                 System.IO.File.WriteAllText("../emergencyBackup.nut", main.editorBackup);    
-#else
-            }
-            catch (Exception e)
-            {
-                // don't allow any further exceptions out
+#endif
+
+                Application.Run(main);
+        }
+
+        static void UncaughtExceptionHandler(object sender, System.Threading.ThreadExceptionEventArgs args){
+            // don't allow any further exceptions out
+            try
+            {                
+
+                // first of all, make sure the edited script is not lost
+                System.IO.File.WriteAllText("../emergencyBackup.nut", GeoGen_Studio.Main.Get().editorBackup);
+
+                // also write the exception to disk
+                System.IO.File.WriteAllText("../exceptionInfo.txt", args.Exception.ToString());
+
+                // hide the loading dialog (if it is displayed)
                 try
                 {
-                    // first of all, make sure the edited script is not lost
-                    System.IO.File.WriteAllText("../emergencyBackup.nut", main.editorBackup);
-
-                    // also write the exception to disk
-                    System.IO.File.WriteAllText("../exceptionInfo.txt", e.ToString());
-
-                    // hide the loading dialog (if it is displayed)
-                    try{
-                        main.loading.Hide();
-                    }
-                    catch {}
-
-
-                    Crash dialog = new Crash();
-
-                    dialog.errorInfo.Text = e.ToString();
-
-                    dialog.ShowDialog();
-
+                    GeoGen_Studio.Main.Get().loading.Hide();
                 }
-                catch { };
+                catch { }
+
+
+                Crash dialog = new Crash();
+
+                dialog.errorInfo.Text = GeoGen_Studio.Main.Get().ToString();
+
+                dialog.ShowDialog();
             }
-#endif
+            catch { }
+            finally{
+                /* Kill the process the the program doesn't freeze after user clicks OK in the exception dialog
+                 * (the exception handled by the AppDomain.CurrentDomain.UnhandledException handler doesn't appear
+                 * as caught). */
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
         }
     }
 }
