91c8083f2ce74ccf8103b54f0c6e38c3fe1abd55
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-12-10 23:12:59 +0000

    [WPF Branch] Studio: Doc-commented BindingManager-related classes.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Binding/BindingException.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Binding/BindingException.cs
@@ -2,9 +2,22 @@
 
 namespace GeoGen.Studio.Utilities.Binding
 {
+    /// <summary>
+    /// Represents an exception thrown during property synchronization.
+    /// </summary>
     public class BindingException: Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindingException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
         public BindingException(string message): base(message) {}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BindingException"/> class.
+        /// </summary>
+        /// <param name="message">The message.</param>
+        /// <param name="innerException">The inner exception.</param>
         public BindingException(string message, Exception innerException) : base(message, innerException) { }
     }
 }
--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Binding/InvalidBoundPropertyException.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Binding/InvalidBoundPropertyException.cs
@@ -2,6 +2,9 @@
 
 namespace GeoGen.Studio.Utilities.Binding
 {
+    /// <summary>
+    /// Represents an exception thrown when either of bound properties is accessible or the properties' type doesn't match.
+    /// </summary>
     public class InvalidBoundPropertyException: BindingException
     {
         public InvalidBoundPropertyException(string message): base(message) {}
