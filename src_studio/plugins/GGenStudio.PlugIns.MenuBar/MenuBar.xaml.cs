cb5b57a3341e91024889dc508cac62d2406eefbe
Matěj Zábský <mzabsky@gmail.com>
2010-12-10 23:03:12 +0000

    [WPF Branch] Studio: Added MenuBar plug-in.

---
+++ src_studio/plugins/GGenStudio.PlugIns.MenuBar/MenuBar.xaml.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GeoGen.Studio.Utilities;
+using GeoGen.Studio.Utilities.Collections;
+using System.Windows.Markup;
+using System.Collections;
+using System.Collections.ObjectModel;
+
+namespace GeoGen.Studio.PlugIns
+{
+    /// <summary>
+    /// A horizontal menu bar which displays items provided by other plug-ins.
+    /// </summary>
+    public partial class MenuBar : GeoGen.Studio.Utilities.PlugInBase.Control, IMenuBar
+    {
+        private static readonly DependencyProperty MenusProperty = DependencyProperty.Register(
+            "Menus", typeof(PriorityObservableCollection), typeof(MenuBar), new PropertyMetadata(new PriorityObservableCollection()));
+
+        private PriorityObservableCollection Menus
+        {
+            get
+            {
+                return (PriorityObservableCollection) GetValue(MenusProperty);
+            }
+            set
+            {
+                SetValue(MenusProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MenuBar"/> class.
+        /// </summary>
+        public MenuBar()
+        {
+            InitializeComponent();
+
+            // The menu contains only Exit item by default
+            MenuEntry fileMenu = new MenuEntry(
+                header: "File",
+                priority: Double.MaxValue
+            );
+            
+            this.AddMenu(fileMenu);
+            
+            fileMenu.Items.Add(
+                new MenuEntry(
+                    header: "Exit",
+                    priority: 0,
+                    command: new RelayCommand(param => App.Current.Shutdown()),
+                    inputGestureText: "Alt+F4"
+                )
+            );
+        }
+
+        /// <summary>
+        /// Plug-in registrator. Do not call manually.
+        /// </summary>
+        /// <param name="mainWindow">The main window.</param>
+        public void Register(IMainWindow mainWindow)
+        {
+            mainWindow.TopBars.Add(this);
+        }
+
+        /// <summary>
+        /// Merges one <see cref="MenuEntry"/> into the menu item hierarchy.
+        /// </summary>
+        /// <param name="menu">The menu item.</param>
+        public void AddMenu(MenuEntry menu)
+        {
+            // Null IS allowed (for simplicity's sake)
+            if (menu == null) return;
+
+            foreach (MenuEntry ownItem in this.Menus)
+            {
+                try
+                {
+                    if (ownItem.Header == menu.Header)
+                    {
+                        ownItem.MergeWith(menu);
+
+                        return;
+                    }
+                }
+                catch(Exception e){
+
+                }
+            }
+
+            this.Menus.Add(menu);
+        }
+
+        /// <summary>
+        /// Merges one <see cref="MenuItem"/> into the menu item hierarchy.
+        /// </summary>
+        /// <param name="menu">The menu item.</param>
+        public void AddMenu(MenuItem menu)
+        {          
+            this.AddMenu((MenuEntry) menu);
+        }
+    }
+}
---
+++ src_studio/plugins/GGenStudio.PlugIns.MenuBar/MenuElement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace GeoGen.Studio.PlugIns
+{
+    class MenuElement: System.Windows.Controls.Menu 
+    {
+        protected override DependencyObject GetContainerForItemOverride()
+        {
+            return new System.Windows.Controls.ContentPresenter();
+        }
+    }
+}
