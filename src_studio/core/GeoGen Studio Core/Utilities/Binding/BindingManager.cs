44a5fccaf5a4982091964267bb5298e850f0d957
Matěj Zábský <mzabsky@gmail.com>
2010-12-10 23:12:59 +0000

    [WPF Branch] Studio: Doc-commented BindingManager-related classes.

--- src_studio/core/GeoGen Studio Core/Utilities/Binding/BindingManager.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Binding/BindingManager.cs
@@ -5,10 +5,23 @@ using System;
 
 namespace GeoGen.Studio.Utilities.Binding
 {
+    /// <summary>
+    /// Provides easy way to synchronize two properties of two object properties. Neither of these properties must be <see cref="System.Windows.DependencyProperty"/>, but the source property (both properties in case of two-way binding) must implement the <see cref="INotifyPropertyChanged"/> interface.
+    /// </summary>
+    /// <remarks>
+    /// Bindings created with this class must always be manually <see cref="BindingManager.Unbind">unbound</see> or memory leaks (zombie objects) may occur.
+    /// </remarks>
     public static class BindingManager
     {
         static private BindingTable bindings = new BindingTable();
 
+        /// <summary>
+        /// Creates an one-way binding between two properties. The binding has to be manually <see cref="BindingManager.UnbindOneWay">unbound</see> before the bound objects can be propertly released.
+        /// </summary>        
+        /// <param name="fromObject">Source object.</param>
+        /// <param name="fromProperty">Source property. This property must have public getter.</param>
+        /// <param name="toObject">Target object.</param>
+        /// <param name="toProperty">Target property. This property must have public getter and setter.</param>
         public static void BindOneWay(INotifyPropertyChanged fromObject, string fromProperty, object toObject, string toProperty)
         {
             BindingManager.bindings.Add(fromObject, fromProperty, toObject, toProperty);
@@ -17,12 +30,32 @@ namespace GeoGen.Studio.Utilities.Binding
             fromObject.PropertyChanged += BindingManager.OnBoundPropertyChanged;
         }
 
+        /// <summary>
+        /// Creates a two-way binding between two properties. The binding has to be manually <see cref="BindingManager.Unbind">unbound</see> before the bound objects can be propertly released.
+        /// </summary>
+        /// <remarks>
+        /// Both properties must have public getter and setter.
+        /// </remarks>
+        /// <param name="object1">Object 1.</param>
+        /// <param name="property1">Property 1.</param>
+        /// <param name="object2">Object 2.</param>
+        /// <param name="property2">Property 2.</param>
         public static void Bind(INotifyPropertyChanged object1, string property1, INotifyPropertyChanged object2, string property2)
         {
             BindingManager.BindOneWay(object1, property1, object2, property2);
             BindingManager.BindOneWay(object2, property2, object1, property1);
         }
 
+        /// <summary>
+        /// Removes an one-way binding between two properties (if it exists).
+        /// </summary>
+        /// <remarks>
+        /// This method can be used to remove only one direction of a two-way binding created with <see cref="BindingManager.Bind"/>.
+        /// </remarks>
+        /// <param name="fromObject">Source object.</param>
+        /// <param name="fromProperty">Source property.</param>
+        /// <param name="toObject">Target object.</param>
+        /// <param name="toProperty">Target property.</param>
         public static void UnbindOneWay(INotifyPropertyChanged fromObject, string fromProperty = null, object toObject = null, string toProperty = null)
         {
             BindingManager.bindings.Remove(fromObject, fromProperty, toObject, toProperty);
@@ -33,6 +66,13 @@ namespace GeoGen.Studio.Utilities.Binding
             }
         }
 
+        /// <summary>
+        /// Removes both one-way and two-way bindings between two properties (if any exist).
+        /// </summary>
+        /// <param name="object1">Object 1.</param>
+        /// <param name="property1">Property 1.</param>
+        /// <param name="object2">Object 2.</param>
+        /// <param name="property2">Property 2.</param>
         public static void Unbind(INotifyPropertyChanged object1, string property1 = null, INotifyPropertyChanged object2 = null, string property2 = null)
         {
             BindingManager.UnbindOneWay(object1, property1, object2, property2);
