e82071e32756f93b34a20bb15f41bf6ecfb99714
Matěj Zábský <mzabsky@gmail.com>
2010-12-11 12:33:02 +0000

    [WPF Branch] Studio: Added new PlugInBase Object.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Object.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.ComponentModel;
+using GeoGen.Studio.Utilities.Messaging;
+using GeoGen.Studio.PlugInLoader;
+
+namespace GeoGen.Studio.Utilities.PlugInBase
+{
+    public class Object : IPlugIn, IMessageProvider, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public event MessageThrownEventHandler MessageThrown;
+
+        protected void ThrowMessage(Message message)
+        {
+            MessageThrown(this, new MessageThrownEventArgs(message));
+        }
+
+        protected void NotifyPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
