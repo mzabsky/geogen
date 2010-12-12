c8d7ff531a6fe0b3a4cfd07f2f755e9178df0741
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-12-12 23:12:33 +0000

    [WPF Branch] Studio: GeneratorUI now handles hotkeys.

--- branches/wpf/src_studio/plugins/GeneratorUI/GeneratorUI.cs
+++ branches/wpf/src_studio/plugins/GeneratorUI/GeneratorUI.cs
@@ -1,12 +1,28 @@
-﻿using GeoGen.Studio.Utilities;
-using System;
+﻿using System;
+using System.Windows.Input;
+using GeoGen.Studio.Utilities;
 
 namespace GeoGen.Studio.PlugIns
 {
     class GeneratorUI: GeoGen.Studio.Utilities.PlugInBase.Object
     {
-        public void Register(IGenerator generator, IEditor editor, IMenuBar menuBar)
+        public void Register(IGenerator generator, IEditor editor, IMenuBar menuBar, IMainWindow mainWindow)
         {
+            ICommand executeCommand = new RelayCommand(
+                param => generator.Start(editor.Text),
+                param => generator.IsReady
+            );
+
+            ICommand abortCommand = new RelayCommand(
+                param => generator.Abort(),
+                param => !generator.IsReady
+            );
+
+            // Register hotkeys
+            mainWindow.RegisterInputGesture(new KeyGesture(Key.F5), executeCommand);
+            mainWindow.RegisterInputGesture(new KeyGesture(Key.F6), abortCommand);
+
+            // Register window menu entries
             MenuEntry generatorMenu = new MenuEntry(
                 header: "Generator",
                 priority: -4,
@@ -16,19 +32,13 @@ namespace GeoGen.Studio.PlugIns
                         header: "Execute",
                         priority: 10,
                         inputGestureText: "F5",
-                        command: new RelayCommand(
-                            param => generator.Start(editor.Text),
-                            param => generator.IsReady
-                        )
+                        command: executeCommand
                     ),
                     new MenuEntry(
                         header: "Abort",
                         priority: 9,
                         inputGestureText: "F6",
-                        command: new RelayCommand(
-                            param => generator.Abort(),
-                            param => !generator.IsReady
-                        )
+                        command: abortCommand
                     ),
                 }
             );
