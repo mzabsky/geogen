f7ddf63dd1f52b8798baf64de15c3b4295540ea0
Matěj Zábský <mzabsky@gmail.com>
2010-12-12 13:11:52 +0000

    [WPF Branch] Studio: Added new plug-in GeneratorUI.

---
+++ src_studio/plugins/GeneratorUI/GeneratorUI.cs
@@ -0,0 +1,40 @@
+﻿using GeoGen.Studio.Utilities;
+using GeoGen.Studio.Utilities.Collections;
+using System;
+
+namespace GeoGen.Studio.PlugIns
+{
+    class GeneratorUI: GeoGen.Studio.Utilities.PlugInBase.Object
+    {
+        public void Register(IGenerator generator, IEditor editor, IMenuBar menuBar)
+        {
+            MenuEntry generatorMenu = new MenuEntry(
+                header: "Generator",
+                priority: -4,
+                items: new PriorityObservableCollection()
+                {
+                    new MenuEntry(
+                        header: "Execute",
+                        priority: 10,
+                        inputGestureText: "F5",
+                        command: new RelayCommand(
+                            param => generator.Start(editor.Text),
+                            param => generator.IsReady
+                        )
+                    ),
+                    new MenuEntry(
+                        header: "Abort",
+                        priority: 9,
+                        inputGestureText: "F6",
+                        command: new RelayCommand(
+                            param => generator.Abort(),
+                            param => !generator.IsReady
+                        )
+                    ),
+                }
+            );
+
+            menuBar.AddMenu(generatorMenu);
+        }
+    }
+}
