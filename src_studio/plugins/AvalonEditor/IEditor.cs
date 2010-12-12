dd24b55fe1da9306b26b465f669211945d9dee9f
Matěj Zábský <mzabsky@gmail.com>
2010-12-12 13:10:33 +0000

    [WPF Branch] Studio: Editor now implements interfaces IEditor and ITextProvider.

---
+++ src_studio/plugins/AvalonEditor/IEditor.cs
@@ -0,0 +1,10 @@
+﻿using GeoGen.Studio.PlugInLoader;
+using System.ComponentModel;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public interface IEditor: IPlugInInterface, INotifyPropertyChanged
+    {
+        string Text {get; set;}
+    }
+}
---
+++ src_studio/plugins/AvalonEditor/ITextProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel;
+using GeoGen.Studio.PlugInLoader;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public interface ITextProvider : IPlugInInterface, INotifyPropertyChanged
+    {
+        string Text {get;}
+    }
+}
