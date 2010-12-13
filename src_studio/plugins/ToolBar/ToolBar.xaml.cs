033fd39e7ea6c4a882056b7d458e3d990ee27917
Matěj Zábský <mzabsky@gmail.com>
2010-12-13 14:18:29 +0000

    [WPF Branch] Studio: Added new plug-in: ToolBar.

---
+++ src_studio/plugins/ToolBar/ToolBar.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using GeoGen.Studio.Utilities;
+using GeoGen.Studio.Utilities.Collections;
+using System.Windows.Markup;
+using System.Collections;
+using System.Collections.ObjectModel;
+using System.Windows.Controls.Primitives;
+
+namespace GeoGen.Studio.PlugIns
+{
+    /// <summary>
+    /// A horizontal tool bar which displays items provided by other plug-ins.
+    /// </summary>
+    public partial class ToolBar : GeoGen.Studio.Utilities.PlugInBase.Control
+    {
+        private static readonly DependencyProperty ToolBarsProperty = DependencyProperty.Register(
+            "ToolBars", typeof(ToolBarEntryObservableCollection), typeof(ToolBar), new PropertyMetadata(new ToolBarEntryObservableCollection()));
+
+        private ToolBarEntryObservableCollection ToolBars
+        {
+            get
+            {
+                return (ToolBarEntryObservableCollection)GetValue(ToolBarsProperty);
+            }
+            set
+            {
+                SetValue(ToolBarsProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ToolBar"/> class.
+        /// </summary>
+        public ToolBar()
+        {
+            InitializeComponent();
+
+            this.ToolBars.Add(new ToolBarButton(
+                toolTip: "Test",
+                icon: "C:\\Users\\Matej\\Pictures\\hello-kitty-1.jpg"
+            ));
+
+            this.ToolBars.Add(new ToolBarSeparator());
+
+            this.ToolBars.Add(new ToolBarCheckableButton(
+                icon: "C:\\Users\\Matej\\Pictures\\hello-kitty-1.jpg",
+                isChecked: true
+            ));
+        }
+
+        /// <summary>
+        /// Adds the toolbar into the main window.
+        /// </summary>
+        /// <param name="mainWindow">The main window.</param>
+        public void Register(IMainWindow mainWindow)
+        {
+            mainWindow.TopBars.Add(this);
+        }
+
+        /// <summary>
+        /// Adds one <see cref="ToolBarEntry"/> to the tool bar.
+        /// </summary>
+        /// <param name="ToolBar">The item.</param>
+        public void AddToolBar(ToolBarEntry ToolBar)
+        {
+            // Null IS allowed (for simplicity's sake)
+            if (ToolBar == null) return;
+
+            this.ToolBars.Add(ToolBar);
+        }
+
+        /// <summary>
+        /// Adds one <see cref="Button"/> to the tool bar.
+        /// </summary>
+        /// <param name="entry">The item.</param>
+        public void AddToolBar(Button entry)
+        {
+            this.AddToolBar((ToolBarEntry)entry);
+        }
+
+        /// <summary>
+        /// Adds one <see cref="Separator"/> to the tool bar.
+        /// </summary>
+        /// <param name="entry">The item.</param>
+        public void AddToolBar(Separator entry)
+        {
+            this.AddToolBar((ToolBarEntry)entry);
+        }
+
+        /// <summary>
+        /// Adds one <see cref="ToggleButton"/> to the tool bar.
+        /// </summary>
+        /// <param name="entry">The item.</param>
+        public void AddToolBar(ToggleButton entry)
+        {
+            this.AddToolBar((ToolBarEntry)entry);
+        }
+    }
+}
---
+++ src_studio/plugins/ToolBar/ToolBarCheckableButton.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public class ToolBarCheckableButton: ToolBarButton
+    {
+        private static readonly DependencyProperty IsCheckedBindingProperty = DependencyProperty.Register(
+            "IsCheckedBinding", typeof(Binding), typeof(ToolBarCheckableButton), new PropertyMetadata(null));
+
+        public Binding IsCheckedBinding
+        {
+            get
+            {
+                return (Binding)GetValue(IsCheckedBindingProperty);
+            }
+            set
+            {
+                SetValue(IsCheckedBindingProperty, value);
+            }
+        }
+
+        private static readonly DependencyProperty IsCheckedProperty = DependencyProperty.Register(
+            "IsChecked", typeof(bool), typeof(ToolBarCheckableButton), new PropertyMetadata(null));
+
+        public bool IsChecked
+        {
+            get
+            {
+                return (bool)GetValue(IsCheckedProperty);
+            }
+            set
+            {
+                SetValue(IsCheckedProperty, value);
+            }
+        }
+
+        public ToolBarCheckableButton(){}
+
+        public ToolBarCheckableButton(object icon, double priority = 0, ICommand command = null, object toolTip = null, bool isChecked = false, Binding isCheckedBinding = null, object dataContext = null)
+        {
+            //this.Icon = icon;
+            if(icon is string)
+            {
+                Image iconImage = new Image();
+                iconImage.Source = new BitmapImage(new Uri(icon as string));
+                iconImage.Width = 16;
+                iconImage.Height = 16;                    
+                    
+                this.Icon = iconImage;
+            }
+            else 
+            {
+                this.Icon = icon;
+            }
+
+            this.IsChecked = isChecked;
+            this.IsCheckedBinding = isCheckedBinding;
+            this.Priority = priority;
+            this.Command = command;
+            this.ToolTip = ToolTip;
+            this.DataContext = dataContext;
+
+            if (IsCheckedBinding != null)
+            {
+                if (isCheckedBinding.Source == null)
+                {
+                    isCheckedBinding.Source = this.DataContext;
+                }
+
+                BindingOperations.SetBinding(this, ToolBarCheckableButton.IsCheckedProperty, this.IsCheckedBinding);
+            }
+        }
+    }
+}
