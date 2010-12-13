f59cd6bbc9130594502cea8d4237543ebb35f3c1
Matěj Zábský <mzabsky@gmail.com>
2010-12-13 23:12:58 +0000

    [WPF Branch] Studio: Added Utilities.ContextManager.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/Context/Context.cs
@@ -0,0 +1,23 @@
+﻿namespace GeoGen.Studio.Utilities.Context
+{
+    /// <summary>
+    /// Represents an application-wide context.
+    /// </summary>
+    public sealed class Context
+    {
+        /// <summary>
+        /// Label of the <see cref="Context"/>. Two <see cref="Context">contexts</see> are not considered equal only because they have the same label (reference comparison is performed).
+        /// </summary>
+        /// <value>The label.</value>
+        public string Label { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Context"/> class.
+        /// </summary>
+        /// <param name="text">The label.</param>
+        public Context(string label)
+        {
+            this.Label = label;
+        }
+    }
+}
