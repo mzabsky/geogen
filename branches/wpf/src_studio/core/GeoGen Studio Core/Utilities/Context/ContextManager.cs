c1eb70f0171542728e16a3a5c386192b76802b54
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-12-13 23:12:58 +0000

    [WPF Branch] Studio: Added Utilities.ContextManager.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Context/ContextManager.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Threading;
+
+namespace GeoGen.Studio.Utilities.Context
+{
+    /// <summary>
+    /// Provides another mode of communication between independent plug-ins. Allows multiple listeners to enter or leave application-wide <see cref="Context">Contexts</see> and to query the presence of specific <see cref="Context"/> (given the caller knows the <see cref="Context"/>).
+    /// </summary>
+    public static class ContextManager
+    {
+        private static List<Context> contexts = new List<Context>();
+
+        /// <summary>
+        /// Occurs when the manager enter or leaves a <see cref="Context"/>.
+        /// </summary>
+        public static event EventHandler ContextChanged;
+
+        /// <summary>
+        /// Enters given <see cref="Context"/> (unless it already active).
+        /// </summary>
+        /// <param name="context">The <see cref="Context"/>.</param>
+        /// <returns><c>true</c> if the <see cref="Context"/> was not active yet; otherwise <c>false</c>.</returns>
+        public static bool EnterContext(Context context)
+        {
+            lock (ContextManager.contexts)
+            {
+                // Can't enter one context twice
+                if (ContextManager.contexts.Contains(context))
+                {
+                    return false;
+                }
+                else
+                {
+                    ContextManager.contexts.Add(context);
+                    ContextManager.OnContextChanged();
+                    return true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Leaves given <see cref="Context"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="Context"/>.</param>
+        /// <returns><c>false</c> if the <see cref="Context"/> was not active; otherwise <c>true</c>.</returns>
+        public static bool LeaveContext(Context context)
+        {
+            lock (ContextManager.contexts)
+            {
+                if (ContextManager.contexts.Contains(context))
+                {
+                    ContextManager.contexts.Remove(context);
+                    ContextManager.OnContextChanged();
+                    return true;
+                }
+                else
+                {                    
+                    // The manager was not in that context at all.
+                    return false;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a list of active <see cref="Context"/> (known to the caller).
+        /// </summary>
+        /// <param name="knownContexts">Known <see cref="Context">contexts</see>.</param>
+        /// <returns>List of known active <see cref="Context">contexts</see>.</returns>
+        public static IEnumerable<Context> GetKnownActiveContexts(IEnumerable<Context> knownContexts)
+        {
+            lock (ContextManager.contexts)
+            {
+                return Enumerable.Intersect(ContextManager.contexts as IEnumerable<Context>, knownContexts);
+            }
+        }
+
+        /// <summary>
+        /// Gets the most recently entered of active <see cref="Context">contexts</see> (known to the caller).
+        /// </summary>
+        /// <param name="knownContexts">Known <see cref="Context">contexts</see>.</param>
+        /// <returns>List of known active <see cref="Context">contexts</see>.</returns>
+        public static Context GetTopMostKnownActiveContext(IEnumerable<Context> knownContexts)
+        {
+            return Enumerable.Last(Enumerable.Intersect(ContextManager.contexts as IEnumerable<Context>, knownContexts));
+        }
+
+        /// <summary>
+        /// Determines whether the specified <see cref="Context"/> is active.
+        /// </summary>
+        /// <param name="context">The <see cref="Context"/>.</param>
+        /// <returns>
+        /// 	<c>true</c> if the <see cref="Context"/> is active; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsContextActive(Context context)
+        {
+            lock (ContextManager.contexts){
+                return ContextManager.contexts.Contains(context);
+            }
+        }        
+
+        private static void OnContextChanged()
+        {
+            if(ContextManager.ContextChanged != null)
+            {
+                Dispatcher.CurrentDispatcher.Invoke(DispatcherPriority.Background, (Action)delegate()
+                {
+                    ContextManager.ContextChanged(null, new EventArgs());
+                });
+            }
+        }
+    }
+}
