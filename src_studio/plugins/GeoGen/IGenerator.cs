4008f27567d60615710e974288b467e4322bd15e
Matěj Zábský <mzabsky@gmail.com>
2010-12-14 19:22:09 +0000

    [WPF Branch] Studio: Added events Aborted, Started and Starting to the GGen plug-in.

--- src_studio/plugins/GeoGen/IGenerator.cs
+++ src_studio/plugins/GeoGen/IGenerator.cs
@@ -9,11 +9,23 @@ namespace GeoGen.Studio.PlugIns
 {
     public interface IGenerator : IPlugInInterface, INotifyPropertyChanged
     {
-        event GenerationFailedEventHandler GenerationFailed;
-        event GenerationFinishedEventHandler GenerationFinished;
+        event GenerationStartingEventHandler Starting;
+        event GenerationStartedEventHandler Started;
         event EventHandler HeaderLoaded;
-        
+        event EventHandler Aborted;
+        event GenerationFailedEventHandler Failed;
+        event GenerationFinishedEventHandler Finished;
+
+        /// <summary>
+        /// Indicates whether the generator is ready to accept requests to run scripts.
+        /// </summary>
+        /// <value><c>true</c> if the generator is ready; otherwise, <c>false</c>.</value>
         bool IsReady { get;}
+
+        /// <summary>
+        /// A value indicating the progress within current map script in percents. The value is 0 if no full script is running. 
+        /// </summary>
+        /// <value>The progress.</value>
         double Progress { get; }
         ObservableCollection<HeightData> Maps { get; }
         ObservableCollection<ScriptArg> Args { get; }
