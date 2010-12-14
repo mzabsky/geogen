5e3831b105444fbb250adfe46be93b60a9b04db7
Matěj Zábský <mzabsky@gmail.com>
2010-12-14 10:34:11 +0000

    [WPF Branch] Studio: ToolBar implements IToolBar.

---
+++ src_studio/plugins/ToolBar/IToolBar.cs
@@ -0,0 +1,14 @@
+﻿using GeoGen.Studio.PlugInLoader;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public interface IToolBar: IPlugInInterface
+    {
+        void AddItem(Separator item);
+        void AddItem(Button item);
+        void AddItem(ToggleButton item);
+        void AddItem(ToolBarEntry item);
+    }
+}
