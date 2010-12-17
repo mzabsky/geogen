e83e2d2200f39606d15c2d411af6592b4436c1e8
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-12-17 22:47:13 +0000

    [WPF Branch] Studio: Added new plug-in: ParamGrid.

---
+++ branches/wpf/src_studio/plugins/ParamGrid/ParamGridTemplateSelector.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows.Controls;
+using System.Windows;
+using GeoGen.Net;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public class ParamGridTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate BoolTemplate { get; set; }
+        public DataTemplate IntTemplate { get; set; }
+        public DataTemplate EnumTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item is ScriptArg)
+            {
+                ScriptArg arg = (ScriptArg)item;
+
+                switch (arg.Type)
+                {
+                    case ScriptArgType.Bool: return BoolTemplate;
+                    case ScriptArgType.Int: return IntTemplate;
+                    case ScriptArgType.Enum: return EnumTemplate;
+                }
+
+                return null;
+            }
+            return base.SelectTemplate(item, container);
+        }
+
+        public ParamGridTemplateSelector()
+        {
+            this.BoolTemplate = null;
+            this.IntTemplate = null;
+            this.EnumTemplate = null;
+        }
+    }
+}
