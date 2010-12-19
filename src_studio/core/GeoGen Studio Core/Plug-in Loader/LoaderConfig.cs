78f3b4e9d4fe8087983243178ad349db6c70ddf4
Matěj Zábský <mzabsky@gmail.com>
2010-12-19 14:05:07 +0000

    [WPF Branch] Studio: Added LoaderConfig to keep track of enabled and disabled plug-ins.

---
+++ src_studio/core/GeoGen Studio Core/Plug-in Loader/LoaderConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using GeoGen.Studio.Utilities.Configurability;
+
+namespace GeoGen.Studio.PlugInLoader
+{
+    internal sealed class LoaderConfig: IConfigurable
+    {
+        [Configurable]
+        public List<Type> EnabledPlugIns {get; private set;}
+
+        [Configurable]
+        public List<Type> DisabledPlugIns { get; private set; }
+
+        public LoaderConfig()
+        {
+            this.EnabledPlugIns = new List<Type>();
+            this.DisabledPlugIns = new List<Type>();
+        }
+    }
+}
