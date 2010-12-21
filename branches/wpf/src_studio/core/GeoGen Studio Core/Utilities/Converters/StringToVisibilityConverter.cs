fc962a4775bdf53b368c24b22a983e4164997420
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2010-12-21 22:24:30 +0000

    [WPF Branch] Studio: Fixed a typo in StringToVisibilityConverter.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Converters/StringToVisibilityConverter.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Converters/StringToVisibilityConverter.cs
@@ -6,7 +6,7 @@ using System.Windows.Data;
 namespace GeoGen.Studio.Utilities.Converters
 {
     /// <summary>
-    /// Converts a string to <see cref="Visibility"/>. Empty string (or null) will be converted to value Coollapsed, any other string will be converted to Visible.
+    /// Converts a string to <see cref="Visibility"/>. Empty string (or null) will be converted to value Collapsed, any other string will be converted to Visible.
     /// </summary>
     [ValueConversion(typeof(string), typeof(Visibility))]
     public class StringToVisibilityConverter : IValueConverter
