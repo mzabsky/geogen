36ff5d129e7762ab705a9c9a1060b4a13aa11aac
Matěj Zábský <mzabsky@gmail.com>
2010-12-21 22:22:21 +0000

    [WPF Branch] Studio: Added StringToVisibilityConverter.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/Converters/StringToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GeoGen.Studio.Utilities.Converters
+{
+    /// <summary>
+    /// Converts a string to <see cref="Visibility"/>. Empty string (or null) will be converted to value Coollapsed, any other string will be converted to Visible.
+    /// </summary>
+    [ValueConversion(typeof(string), typeof(Visibility))]
+    public class StringToVisibilityConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if (value == null || (string)value == "") return Visibility.Collapsed;
+                else return Visibility.Visible;
+            }
+            catch { }
+            return "<Unknown value>";
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
