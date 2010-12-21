afe2f72e0f88a8289389ed3c4db95e3422c570d7
Matěj Zábský <mzabsky@gmail.com>
2010-12-21 21:36:11 +0000

    [WPF Branch] Studio: Created a new "Converters" namespace in utils and added a BoolToYesNoConverter into it.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/Converters/BoolToYesNoConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace GeoGen.Studio.Utilities.Converters
+{
+    /// <summary>
+    /// Converts a boolean value to a "Yes"/"No" string value.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(String))]
+    public class BoolToYesNoConverter: IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                if ((bool)value) return "Yes";
+                else return "No";
+            }
+            catch { }
+            return "<Unknown value>";
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
