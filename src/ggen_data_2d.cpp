32a19c7b6444cfcb93b98d351b3295fd01f362cd
Matěj Zábský <mzabsky@gmail.com>
2011-01-05 22:05:29 +0000

    Core: Removed debugging condition from the BicubicInterpolation.

--- src/ggen_data_2d.cpp
+++ src/ggen_data_2d.cpp
@@ -794,11 +794,6 @@ GGen_Height Noise_BicubicInterpolation(double p[4][4]){
 		(a30 + a31 * 0.5 + a32 * 0.5 * 0.5 + a33 * 0.5 * 0.5 * 0.5) * 0.5 * 0.5 * 0.5;
 
 	// Check for overflows.
-	if(!(value >= GGEN_MIN_HEIGHT && value <= GGEN_MAX_HEIGHT)){
-		cout << p[0][0] << " " << p[0][1] <<" " << p[0][2]<<" " <<p[0][3]<<" " <<p[1][0]<<" " <<p[0][0]<<" " <<p[1][1]<<" " <<p[1][2]<<" " <<p[1][2]<<" " <<p[1][3]<<" " <<p[2][0]<<" " <<p[2][1]<<" " <<p[2][2]<<" " <<p[2][3]<<" " <<p[3][0]<<" " <<p[3][1]<<" " <<p[3][2]<<" " <<p[3][3];
-		cout << value;
-	}
-
 	GGen_Script_Assert(value >= GGEN_MIN_HEIGHT && value <= GGEN_MAX_HEIGHT);
 
 	return (GGen_Height) value;
