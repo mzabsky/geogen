441e2ac404105ec3d2d0daaaa152a929a4cd3158
Matěj Zábský <mzabsky@gmail.com>
2011-01-25 19:42:36 +0000

    [WPF Branch] Studio: Improved coding style of Messaging classes.

--- src_studio/core/GeoGen Studio Core/Utilities/Messaging/Message.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Messaging/Message.cs
@@ -2,11 +2,33 @@
 
 namespace GeoGen.Studio.Utilities.Messaging
 {
+    /// <summary>
+    /// Represents a message with additional information about its severity and occurence time.
+    /// </summary>
     public class Message
     {
+        /// <summary>
+        /// Text of the message.
+        /// </summary>
+        /// <value>The text.</value>
         public string Text {get; set;}
+
+        /// <summary>
+        /// Type of the message.
+        /// </summary>
+        /// <value>The type.</value>
         public MessageType Type {get; set;}
+
+        /// <summary>
+        /// Time of occurence.
+        /// </summary>
+        /// <value>The time of occurence.</value>
         public DateTime DateTime { get; set; }
+
+        /// <summary>
+        /// Short string representation of time of occurence.
+        /// </summary>
+        /// <value>The time string.</value>
         public string DateTimeString
         {
             get
@@ -15,6 +37,11 @@ namespace GeoGen.Studio.Utilities.Messaging
             }
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Message"/> class.
+        /// </summary>
+        /// <param name="text">The text.</param>
+        /// <param name="type">The type.</param>
         public Message(string text, MessageType type = MessageType.Message)
         {
             this.Text = text;
