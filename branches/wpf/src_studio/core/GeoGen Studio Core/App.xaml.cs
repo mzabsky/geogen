8e9d499a8bc2ff850d4c44a5593689a8726d971f
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-01-25 20:10:52 +0000

    [WPF Branch] Studio: Improved coding style for App.

--- branches/wpf/src_studio/core/GeoGen Studio Core/App.xaml.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/App.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
 using System.Windows;
 using System.Reflection;
 using GeoGen.Studio.PlugInLoader;
@@ -10,24 +7,28 @@ using GeoGen.Studio.PlugInLoader;
 namespace GeoGen.Studio
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    /// Implementation of <see cref="Application"/> for this program.
     /// </summary>
     public partial class App : Application
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="App"/> class.
+        /// </summary>
+        /// <exception cref="T:System.InvalidOperationException">More than one instance of the <see cref="T:System.Windows.Application"/> class is created per <see cref="T:System.AppDomain"/>.</exception>
         public App()
         {
             try
             {
-                /* Load "plug-ins" from this executable */
+                // Load "plug-ins" from this executable.
                 Loader.ParseAssembly(Assembly.GetExecutingAssembly());
 
-                /* Load plug-ins from the plug-in directory */
+                // Load plug-ins from the plug-in directory.
                 Loader.ParseDirectory("./plugins");
 
-                /* Create plug-in instances and register their relationships */
+                // Create plug-in instances and register their relationships.
                 Loader.ExecuteAllRegistrators();
 
-                /* Check if all plug-ins successfully loaded */
+                // Check if all plug-ins successfully loaded.
                 List<string> messages = new List<string>();
                 foreach (Registrator registrator in Loader.Registrators)
                 {
@@ -46,7 +47,7 @@ namespace GeoGen.Studio
                     MessageBox.Show("One or more plug-ins completely or partially failed to load" + Environment.NewLine + Environment.NewLine + "Details:" + Environment.NewLine + Environment.NewLine + String.Join(Environment.NewLine, messages), "GeoGen Studio", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
-            /* Any unhandled exception in this phase means the application cannot continue */
+            // Any unhandled exception in this phase means the application cannot continue.
             catch(Exception e){
                 MessageBox.Show("GeoGen Studio plug-in loader failed with following message:" + Environment.NewLine + Environment.NewLine + e.Message, "GeoGen Studio", MessageBoxButton.OK, MessageBoxImage.Error);
                 Application.Current.Shutdown();
