09e6434606c791100cb2a35f097fed53989e9756
Matěj Zábský <mzabsky@gmail.com>
2011-01-25 20:01:24 +0000

    [WPF Branch] Studio: Improved coding style for PlugInBase classes.

--- src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Object.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Object.cs
@@ -1,13 +1,16 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.ComponentModel;
-using GeoGen.Studio.Utilities.Messaging;
+﻿using System.ComponentModel;
 using GeoGen.Studio.PlugInLoader;
 
 namespace GeoGen.Studio.Utilities.PlugInBase
 {
+    /// <summary>
+    /// Base class for plug-ins that are not based on any class (other than <see cref="object"/>). Provides <see cref="INotifyPropertyChanged"/> implementation.
+    /// </summary>
     public class Object : IPlugIn, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string info)
--- src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Window.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Window.cs
@@ -1,13 +1,16 @@
-﻿using System.Windows;
-using System.Windows.Controls;
-using System.ComponentModel;
-using GeoGen.Studio.Utilities.Messaging;
+﻿using System.ComponentModel;
 using GeoGen.Studio.PlugInLoader;
 
 namespace GeoGen.Studio.Utilities.PlugInBase
 {
+    /// <summary>
+    /// Base class for plug-ins extending WPF <see cref="System.Windows.Window"/>. Provides <see cref="INotifyPropertyChanged"/> implementation.
+    /// </summary>
     public class Window : System.Windows.Window, IPlugIn, INotifyPropertyChanged
     {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
 
         protected void OnPropertyChanged(string info)
