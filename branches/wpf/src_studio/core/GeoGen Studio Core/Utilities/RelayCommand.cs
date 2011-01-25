e697b4aa1ab1b987925a01c1d54965c72f199137
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-01-25 20:13:12 +0000

    [WPF Branch] Studio: Improved coding style for RelayCommand.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/RelayCommand.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/RelayCommand.cs
@@ -4,6 +4,9 @@ using System.Windows.Input;
 
 namespace GeoGen.Studio.Utilities
 {
+    /// <summary>
+    /// Provides an easy way to define a <see cref="ICommand"/> from one or two methods.
+    /// </summary>
     public class RelayCommand : ICommand
     {
         #region Fields
@@ -32,18 +35,32 @@ namespace GeoGen.Studio.Utilities
 
         #region ICommand Members
 
+        /// <summary>
+        /// Defines the method that determines whether the command can execute in its current state.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
+        /// <returns>
+        /// true if this command can be executed; otherwise, false.
+        /// </returns>
         [DebuggerStepThrough]
         public bool CanExecute(object parameter)
         {
             return _canExecute == null ? true : _canExecute(parameter);
         }
 
+        /// <summary>
+        /// Occurs when changes occur that affect whether or not the command should execute.
+        /// </summary>
         public event EventHandler CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
         }
 
+        /// <summary>
+        /// Defines the method to be called when the command is invoked.
+        /// </summary>
+        /// <param name="parameter">Data used by the command.  If the command does not require data to be passed, this object can be set to null.</param>
         public void Execute(object parameter)
         {
             _execute(parameter);
