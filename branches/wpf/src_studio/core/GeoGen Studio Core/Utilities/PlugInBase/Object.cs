e2154ac57f6b0670a39859123001a2c92f8b96f6
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-01-29 23:28:44 +0000

    [WPF Branch] Studio: Added ObservableObject class.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Object.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/PlugInBase/Object.cs
@@ -6,19 +6,7 @@ namespace GeoGen.Studio.Utilities.PlugInBase
     /// <summary>
     /// Base class for plug-ins that are not based on any class (other than <see cref="object"/>). Provides <see cref="INotifyPropertyChanged"/> implementation.
     /// </summary>
-    public class Object : IPlugIn, INotifyPropertyChanged
+    public class ObservableObject : IPlugIn
     {
-        /// <summary>
-        /// Occurs when a property value changes.
-        /// </summary>
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void OnPropertyChanged(string info)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(info));
-            }
-        }
     }
 }
