1932d1d1595e149618d3577c9122d3a6a68bf1cf
Matěj Zábský <mzabsky@gmail.com>
2011-01-29 23:19:06 +0000

    [WPF Branch] Studio: Converters now implement singleton pattern.

--- src_studio/core/GeoGen Studio Core/Utilities/Converters/BoolToYesNoConverter.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Converters/BoolToYesNoConverter.cs
@@ -10,6 +10,19 @@ namespace GeoGen.Studio.Utilities.Converters
     [ValueConversion(typeof(bool), typeof(String))]
     public class BoolToYesNoConverter: IValueConverter
     {
+        private static readonly IValueConverter instance = new BoolToYesNoConverter();
+        /// <summary>
+        /// Gets instance of this converter.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static IValueConverter Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
         object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             try
