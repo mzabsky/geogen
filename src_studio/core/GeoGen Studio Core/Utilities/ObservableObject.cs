249f56a3493e544926ecca7de9f92841391ac461
Matěj Zábský <mzabsky@gmail.com>
2011-01-29 23:28:44 +0000

    [WPF Branch] Studio: Added ObservableObject class.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/ObservableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+namespace GeoGen.Studio.Utilities
+{
+    /// <summary>
+    /// Provides <see cref="INotifyPropertyChanged"/> implementation.
+    /// </summary>
+    public class ObservableObject: INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Occurs when a property value changes.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string info)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+            }
+        }
+    }
+}
