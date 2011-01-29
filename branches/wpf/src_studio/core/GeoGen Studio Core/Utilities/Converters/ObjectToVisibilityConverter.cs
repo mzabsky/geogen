a7de44886fe651fb925925e452251db80acbb1d6
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-01-29 23:21:42 +0000

    [WPF Branch] Studio: Added ObjectToVisibilityConverter.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Converters/ObjectToVisibilityConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GeoGen.Studio.Utilities.Converters
+{
+    /// <summary>
+    /// Converts an <see cref="object"/> to <see cref="Visibility"/>. Null will be converted to value Collapsed, any other string will be converted to Visible.
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(Visibility))]
+    public class ObjectToVisibilityConverter : IValueConverter
+    {
+        private static readonly IValueConverter instance = new ObjectToVisibilityConverter();
+        /// <summary>
+        /// Gets instance of this converter.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static IValueConverter Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return Visibility.Collapsed;
+
+            return Visibility.Visible;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
