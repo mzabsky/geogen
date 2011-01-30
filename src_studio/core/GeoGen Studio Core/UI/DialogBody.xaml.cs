eacfc4411dc3183a075416e34e51757ffc5aaeab
Matěj Zábský <mzabsky@gmail.com>
2011-01-30 16:23:47 +0000

    [WPF Branch] Studio: Added new UI namespace with DialogBody control.

---
+++ src_studio/core/GeoGen Studio Core/UI/DialogBody.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Collections.ObjectModel;
+
+namespace GeoGen.Studio.UI
+{
+    /// <summary>
+    /// Provides united layout for any dialog-type window.
+    /// </summary>
+    public partial class DialogBody
+    {
+        private static readonly DependencyProperty buttonsProperty = DependencyProperty.Register(
+            "Buttons", typeof(ObservableCollection<object>), typeof(DialogBody), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Collection of button displayed in the bottom bar.
+        /// </summary>
+        /// <value>The buttons.</value>
+        public ObservableCollection<object> Buttons
+        {
+            get
+            {
+                return (ObservableCollection<object>)GetValue(buttonsProperty);
+            }
+            set
+            {
+                SetValue(buttonsProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogBody"/> class.
+        /// </summary>
+        public DialogBody()
+        {
+            this.Buttons = new ObservableCollection<object>();
+            InitializeComponent();
+        }
+    }
+}
