16364df86a1a74578a03f340026a53ee89142e92
Matěj Zábský <mzabsky@gmail.com>
2011-02-15 22:18:02 +0000

    Core: Added new function: SimpleErosion. (do not confuse this with the advanced erosion filter being developed in the river branch).

--- src/ggen_squirrel.cpp
+++ src/ggen_squirrel.cpp
@@ -245,7 +245,8 @@ GGen_Squirrel::GGen_Squirrel(){
 		func(&GGen_Data_2D::Distort,_T("Distort")).
 		func(&GGen_Data_2D::NormalMap,_T("NormalMap")).
 		func(&GGen_Data_2D::NormalDifferenceMap,_T("NormalDifferenceMap")).
-		func(&GGen_Data_2D::GetNormal,_T("GetNormal"));
+		func(&GGen_Data_2D::GetNormal,_T("GetNormal")).
+		func(&GGen_Data_2D::SimpleErosion,_T("SimpleErosion"));
 
 	/* Class: GGen_Amplitudes */
 	SQClassDefNoConstructor<GGen_Amplitudes>(_SC("GGen_Amplitudes")).
