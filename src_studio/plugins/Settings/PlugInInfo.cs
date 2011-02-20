25540b21b748e073ee00819dcb5a83329620f7c4
Matěj Zábský <mzabsky@gmail.com>
2011-02-20 15:49:13 +0000

    [WPF Branch] Studio: Added Studio plug-in.

---
+++ src_studio/plugins/Settings/PlugInInfo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using GeoGen.Studio.PlugInLoader;
+using GeoGen.Studio.Utilities;
+using GeoGen.Studio.Utilities.Configurability;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public class PlugInInfo: ObservableObject, INotifyPropertyChanged
+    {
+        public PlugIn PlugIn { get; set; }
+        public List<ConfigurablePropertyInfo> Properties {get; set;}
+        public bool IsEnabled { get; set; }
+
+        public PlugInInfo(PlugInLoader.PlugIn plugIn)
+        {
+            this.IsEnabled = Loader.IsPlugInEnabled(plugIn);
+            this.PlugIn = plugIn;
+            this.Properties = new List<ConfigurablePropertyInfo>();            
+
+            // Not every plug-in is necessarily configurable
+            if (!typeof(IConfigurable).IsAssignableFrom(this.PlugIn.Type))
+            {
+                return ;
+            }
+
+            // Build the list of configurable properties
+            foreach (PropertyInfo property in plugIn.Type.GetProperties())
+            {              
+                ConfigurableAttribute configurableAttribute = Attribute.GetCustomAttribute(property, typeof(ConfigurableAttribute)) as ConfigurableAttribute;
+
+                /* Skip non-readable non-readable non-configurable properties. */
+                if (property.CanWrite && property.CanRead && configurableAttribute != null)
+                {
+                    ConfigurablePropertyInfo configurablePropertyInfo = new ConfigurablePropertyInfo();
+                    configurablePropertyInfo.Name = property.Name;
+                    configurablePropertyInfo.Property = property;
+                    configurablePropertyInfo.Type = property.PropertyType;
+                    configurablePropertyInfo.Description = "";
+
+                    if(this.PlugIn.IsRunning)
+                    {
+                        // The plug-in is running - get the value from its first instance
+                        configurablePropertyInfo.OriginalValue = property.GetValue(Enumerable.First(this.PlugIn.Instances), null);
+                    }
+                    else
+                    {
+                        // The plug-in is not running - get the value from the config file
+                        configurablePropertyInfo.OriginalValue = MainConfig.GetPropertyValue(property);
+                    }
+
+                    configurablePropertyInfo.Value = configurablePropertyInfo.OriginalValue;
+                    this.Properties.Add(configurablePropertyInfo);
+                }
+            }
+        }
+    }
+}
---
+++ src_studio/plugins/Settings/Settings.xaml.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Input;
+using System.ComponentModel;
+using GeoGen.Studio.Utilities;
+using GeoGen.Studio.PlugInLoader;
+using GeoGen.Studio.Utilities.Configurability;
+
+namespace GeoGen.Studio.PlugIns
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public sealed partial class Settings : GeoGen.Studio.Utilities.PlugInBase.Window, INotifyPropertyChanged
+    {
+        private readonly ICommand showCommand;
+        private readonly ICommand applyCommand;
+        private readonly ICommand closeCommand;
+        private readonly ICommand okCommand;
+        private readonly ICommand togglePlugInCommand;
+
+        public ICommand ShowCommand {
+            get
+            {
+                return this.showCommand;
+            }
+        }
+
+        public ICommand ApplyCommand {
+            get
+            {
+                return this.applyCommand;
+            }
+        }
+
+        public ICommand CloseCommand {
+            get
+            {
+                return this.closeCommand;
+            }
+        }
+
+        public ICommand OkCommand
+        {
+            get
+            {
+                return this.okCommand;
+            }
+        }
+
+        public ICommand TogglePlugInCommand
+        {
+            get
+            {
+                return this.togglePlugInCommand;
+            }
+        }
+
+        public IEnumerable<PlugInInfo> PlugIns {get; private set;}
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Settings"/> class.
+        /// </summary>
+        public Settings()
+        {
+            ICommand command = this.ShowCommand;
+
+            this.showCommand = new RelayCommand(
+                param => this.ShowWindow()
+            );
+
+            this.applyCommand = new RelayCommand(
+                param => this.ApplyChanges()
+            );
+
+            this.closeCommand = new RelayCommand(
+                param => this.Close()
+            );
+
+            this.okCommand = new RelayCommand(
+                param =>
+                {
+                    this.ApplyChanges(); 
+                    this.Close();
+                }
+            );
+
+            this.togglePlugInCommand = new RelayCommand(
+                param =>
+                {
+                    (param as PlugInInfo).IsEnabled = !(param as PlugInInfo).IsEnabled;
+                }
+            );
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Adds a button to the main menu bar.
+        /// </summary>
+        /// <param name="menuBar">The menu bar.</param>
+        public void Register(IMenuBar menuBar)
+        {
+            menuBar.AddMenu(
+                new MenuEntry(
+                    header: "Edit",
+                    priority: -1,
+                    items: new MenuEntryObservableCollection {
+                        new MenuSeparator(
+                            priority: -2000
+                        ),
+                        new MenuEntry(
+                            header: "Settings",                            
+                            priority: double.MinValue,
+                            command: this.showCommand,
+                            icon: "pack://application:,,,/GGenStudio.PlugIn.Settings;component/Images/Icons/settings.png"
+                        )
+                    }
+                )
+            );
+        }
+
+        private void ShowWindow()
+        {            
+            List<PlugInInfo> list = new List<PlugInInfo>();
+
+            foreach(PlugIn plugIn in Loader.PlugIns)
+            {
+                if (typeof(IConfigurable).IsAssignableFrom(plugIn.Type) && plugIn.IsRunning)
+                {
+                    MainConfig.SaveConfiguration(plugIn.Instances.Last() as IConfigurable);
+                }
+
+                list.Add(new PlugInInfo(plugIn));
+            }
+
+            this.PlugIns = list;
+
+            this.ShowDialog();
+        }
+
+        private void ApplyChanges()
+        {
+            foreach(PlugInInfo plugInInfo in this.PlugIns)
+            {
+                // Update enabled status
+                plugInInfo.PlugIn.IsEnabled = plugInInfo.IsEnabled;
+
+                // Do not apply configuration changes for non-configurable plug-ins
+                if(!typeof (IConfigurable).IsAssignableFrom(plugInInfo.PlugIn.Type))
+                {
+                    continue;
+                }
+
+                foreach(ConfigurablePropertyInfo property in plugInInfo.Properties)
+                {
+                    MainConfig.SavePropertyValue(property.Property, property.Value);
+
+                    foreach(IConfigurable instance in plugInInfo.PlugIn.Instances)
+                    {
+                        property.Property.SetValue(instance, property.Value, null);
+                    }                    
+                }
+            }
+        }
+
+        private void TogglePlugInEnabled(PlugInInfo plugInInfo)
+        {
+            plugInInfo.IsEnabled = !plugInInfo.IsEnabled;
+        }
+    }
+}
