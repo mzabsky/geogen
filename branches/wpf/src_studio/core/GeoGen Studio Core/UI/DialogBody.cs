327731a2184771d74b5eef605959e517d4f69bd7
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-02-26 21:06:15 +0000

    [WPF Branch] Studio: Fixed DialogBody so it can use XAML content.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/UI/DialogBody.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+
+namespace GeoGen.Studio.UI
+{
+    /// <summary>
+    /// Provides united layout for any dialog-type window.
+    /// </summary>
+    public class DialogBody: UserControl
+    {
+        public static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
+            "Buttons", typeof(IList), typeof(DialogBody), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Collection of buttons displayed in the bottom bar.
+        /// </summary>
+        /// <value>The buttons.</value>
+        public IList Buttons
+        {
+            get
+            {
+                return (IList)GetValue(ButtonsProperty);
+            }
+            set
+            {
+                SetValue(ButtonsProperty, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogBody"/> class.
+        /// </summary>
+        public DialogBody()
+        {
+            this.Buttons = new ArrayList();
+        }
+    }
+}
