6731a7b7ac255d54fdf07fed8501da87a725eab3
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-02-28 22:03:08 +0000

    [WPF Branch] Studio: Added FileService class to unify approach to opening, saving and closing files.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/IO/FileEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public delegate void FileEventHandler (object sender, FileEventArgs args);
+
+    public class FileEventArgs: EventArgs
+    {
+        public FileInfo FileInfo { get; private set; }
+
+        public FileEventArgs(FileInfo file)
+        {
+            this.FileInfo = file;
+        }
+    }
+}
