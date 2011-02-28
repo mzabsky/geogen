297cc5139652a6277b296e26c94b95ec0a052ac5
Matěj Zábský <mzabsky@gmail.com>
2011-02-28 22:03:08 +0000

    [WPF Branch] Studio: Added FileService class to unify approach to opening, saving and closing files.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/IO/FileService.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using GeoGen.Studio.Utilities.Collections;
+
+namespace GeoGen.Studio.PlugIns
+{
+    public static class FileService
+    {
+        public static UniqueObservableQueue<FileInfo> RecentFiles { get; private set; }
+
+        public static event FileEventHandler FileOpened;
+        public static event FileEventHandler FileClosed;
+        public static event FileEventHandler FileSaved;
+
+        static FileService()
+        {
+            FileService.RecentFiles = new UniqueObservableQueue<FileInfo>();
+        }
+
+        public static void OnFileOpened(object sender, FileInfo fileInfo)
+        {
+            FileService.RecentFiles.Add(fileInfo);
+
+            if(FileService.FileOpened != null)
+            {
+                FileService.FileOpened(sender, new FileEventArgs(fileInfo));
+            }
+        }
+
+        public static void OnFileSaved(object sender, FileInfo fileInfo)
+        {
+            FileService.RecentFiles.Add(fileInfo);
+
+            if (FileService.FileSaved != null)
+            {
+                FileService.FileSaved(sender, new FileEventArgs(fileInfo));
+            }
+        }
+
+        public static void OnFileClosed(object sender, FileInfo fileInfo)
+        {
+            if (FileService.FileClosed != null)
+            {
+                FileService.FileClosed(sender, new FileEventArgs(fileInfo));
+            }
+        }
+    }
+}
