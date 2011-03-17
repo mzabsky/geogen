b17e9595c839edfafdd6818cb5aa34f589cede62
Matěj Zábský <mzabsky@gmail.com>
2011-03-17 19:41:11 +0000

    [WPF Branch] Studio: Demagicized the UniqueObservableCollection capacity.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/IO/FileInfoObservableQueue.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+using GeoGen.Studio.Utilities.Collections;
+
+namespace GeoGen.Studio.Utilities.IO
+{
+    /// <summary>
+    /// Observable collection of files. Each file can be only once in the collection. The collection has total capacity of 20 entries.
+    /// </summary>
+    public class FileInfoObservableQueue: UniqueObservableQueue<FileInfo>, IXmlSerializable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileInfoObservableQueue"/> class.
+        /// </summary>
+        public FileInfoObservableQueue(): base(UniqueObservableQueue<FileInfo>.DefaultCapacity, new DelegateEqualityComparer<FileInfo>((a, b) => a.FullName == b.FullName))
+        {         
+        }
+
+        /// <exclude />
+        public System.Xml.Schema.XmlSchema GetSchema()
+        {
+            // Required by IXmlSerializable, but not necessary
+            return null;
+        }
+
+        /// <exclude />
+        public void ReadXml(System.Xml.XmlReader reader)
+        {
+            bool wasEmpty = reader.IsEmptyElement;
+
+            reader.Read();
+
+            if (wasEmpty)
+            {
+                return;
+            }
+
+            while (reader.NodeType != System.Xml.XmlNodeType.EndElement)
+            {
+                reader.ReadStartElement("item");
+                string fileName = reader.ReadString();
+                reader.ReadEndElement();
+
+                try
+                {
+                    this.Add(new FileInfo(fileName));   
+                }
+                catch(IOException)
+                {                    
+                }
+            }
+            reader.ReadEndElement();
+        }
+
+        /// <exclude />
+        public void WriteXml(System.Xml.XmlWriter writer)
+        {
+            foreach (FileInfo fileInfo in this)
+            {
+                writer.WriteStartElement("item");
+                writer.WriteValue(fileInfo.FullName);
+                writer.WriteEndElement();
+            }
+        }
+    }
+}
