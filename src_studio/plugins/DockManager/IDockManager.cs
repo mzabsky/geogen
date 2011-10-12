e8a7848c1b0cc89e7d7ab8a2b9317febcfd71cc7
Matěj Zábský <mzabsky@gmail.com>
2011-10-12 21:16:13 +0000

    [WPF Branch] Studio: Reworked the way DockManager determines locations for dockables.

--- src_studio/plugins/DockManager/IDockManager.cs
+++ src_studio/plugins/DockManager/IDockManager.cs
@@ -4,11 +4,31 @@ using GeoGen.Studio.PlugInLoader;
 
 namespace GeoGen.Studio.PlugIns
 {
+    public enum DockingLocation
+    {
+        Document,
+        Floating,
+        Left,
+        LeftTop,
+        LeftBottom,
+        Right,
+        RightTop,
+        RightBottom,
+        Top,
+        TopLeft,
+        TopRight,
+        Bottom,
+        BottomLeft,
+        BottomRight
+    }
+
     public interface IDockManager: IPlugInInterface
     {
         bool AddAsDocumentContent(object content, string title, bool focus = false);
-        bool AddAsDockableContent(object content, string title, bool showInMainArea = false);
+        bool AddAsDockableContent(object content, string title, DockingLocation preferredLocation);
         bool AddAsFloatableWindow(object content, string title, Size size, bool dockable = true);
         void Activate(object content);
+        object GetDockingState(object content);
+        object GetDockingStateByLocation(DockingLocation location);
     }
 }
--- src_studio/plugins/Messages/Messages.xaml.cs
+++ src_studio/plugins/Messages/Messages.xaml.cs
@@ -1,84 +1,89 @@
 ﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows;
 using System.Windows.Controls;
-using GeoGen.Studio.Utilities.Messaging;
-using System.Collections.ObjectModel;
 using GeoGen.Studio.PlugInLoader;
+using GeoGen.Studio.Utilities.Configurability;
+using GeoGen.Studio.Utilities.Messaging;
 
 namespace GeoGen.Studio.PlugIns
 {
-    /// <summary>
-    /// Interaction logic for UserControl1.xaml
-    /// </summary>
-    public partial class Messages : UserControl, IPlugIn
-    {
-        public ObservableCollection<Message> Items { get; private set; }
+	/// <summary>
+	/// Interaction logic for UserControl1.xaml
+	/// </summary>
+	public partial class Messages : UserControl, IPlugIn
+	{
+		public ObservableCollection<Message> Items { get; private set; }
 
-        private static readonly DependencyPropertyKey IsEmptyPropertyKey = DependencyProperty.RegisterReadOnly(
-            "IsEmpty", typeof(bool), typeof(Messages), new PropertyMetadata(true));
+		[Configurable(DefaultValue = DockingLocation.RightTop)]
+		public object DockingState {get; set;}
 
-        public static readonly DependencyProperty IsEmptyProperty = IsEmptyPropertyKey.DependencyProperty;
+		private static readonly DependencyPropertyKey IsEmptyPropertyKey = DependencyProperty.RegisterReadOnly(
+			"IsEmpty", typeof(bool), typeof(Messages), new PropertyMetadata(true));
 
-        public bool IsEmpty
-        {
-            get {
-                return (bool) GetValue(IsEmptyProperty); 
-            }
-            private set {
-                SetValue(IsEmptyPropertyKey, value); 
-            }
-        }
+		public static readonly DependencyProperty IsEmptyProperty = IsEmptyPropertyKey.DependencyProperty;
 
-        public Messages()
-        {
-            this.Items = new ObservableCollection<Message>();
-            this.Items.CollectionChanged += this.CollectionChanged;
-
-            foreach(Message message in Messenger.MessageHistory)
-            {
-                this.Items.Add(message);
-            }
-
-            Messenger.MessageThrown += delegate(object o, MessageThrownEventArgs args)
-            {
-                this.Items.Add(args.Message);
-            };     
-
-            InitializeComponent();
-        }
-
-        public void Register(IDockManager dockManager)
-        {
-            dockManager.AddAsDockableContent(this, "Messages", false);
-        }
-
-        private void CollectionChanged(object sender, EventArgs args)
-        {
-            this.IsEmpty = (this.Items.Count == 0);
-
-            if (this.scroller != null)
-            {
-                if (this.scroller.ScrollableHeight == this.scroller.VerticalOffset) this.scroller.ScrollToBottom();
-            }
-        }
-		
-		private void CopyToClipboard(object sender, RoutedEventArgs e){
-            Clipboard.SetText(((Message)((MenuItem)sender).DataContext).Text);
+		public bool IsEmpty
+		{
+			get {
+				return (bool) GetValue(IsEmptyProperty); 
+			}
+			private set {
+				SetValue(IsEmptyPropertyKey, value); 
+			}
 		}
 
-        private void Delete(object sender, RoutedEventArgs e)
-        {
-            this.Items.Remove((Message)((MenuItem)sender).DataContext);
-        }
+		public Messages()
+		{
+			this.Items = new ObservableCollection<Message>();
+			this.Items.CollectionChanged += this.CollectionChanged;
 
-        private void Clear(object sender, RoutedEventArgs e)
-        {
-            this.Items.Clear();
-        }
+			foreach(Message message in Messenger.MessageHistory)
+			{
+				this.Items.Add(message);
+			}
 
-        public void AddMessage(string message, MessageType type = MessageType.Message)
-        {
-            this.Items.Add(new Message(message, type));
-        }
+			Messenger.MessageThrown += delegate(object o, MessageThrownEventArgs args)
+			{
+				this.Items.Add(args.Message);
+			};     
+
+			InitializeComponent();
+			MainConfig.Register(this);
+		}
+
+		public void Register(IDockManager dockManager)
+		{
+			dockManager.AddAsDockableContent(this, "Messages", DockingLocation.RightBottom);
+		}
+
+		private void CollectionChanged(object sender, EventArgs args)
+		{
+			this.IsEmpty = (this.Items.Count == 0);
+
+			if (this.scroller != null)
+			{
+				if (this.scroller.ScrollableHeight == this.scroller.VerticalOffset) this.scroller.ScrollToBottom();
+			}
+		}
+		
+		private void CopyToClipboard(object sender, RoutedEventArgs e){
+			Clipboard.SetText(((Message)((MenuItem)sender).DataContext).Text);
+		}
+
+		private void Delete(object sender, RoutedEventArgs e)
+		{
+			this.Items.Remove((Message)((MenuItem)sender).DataContext);
+		}
+
+		private void Clear(object sender, RoutedEventArgs e)
+		{
+			this.Items.Clear();
+		}
+
+		public void AddMessage(string message, MessageType type = MessageType.Message)
+		{
+			this.Items.Add(new Message(message, type));
+		}
 	}
 }
