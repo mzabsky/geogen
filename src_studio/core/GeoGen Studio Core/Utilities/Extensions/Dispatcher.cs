d13c0d39c1ed28a728361deafe7e1672d8e8fe26
Matěj Zábský <mzabsky@gmail.com>
2011-11-22 21:21:37 +0000

    [WPF Branch] Studio: Added Dispatcher helper extension class.

---
+++ src_studio/core/GeoGen Studio Core/Utilities/Extensions/Dispatcher.cs
@@ -0,0 +1,21 @@
+﻿namespace GeoGen.Studio.Utilities.Extensions
+{
+    using System.Windows.Threading;
+
+    static class DispatcherExtensions
+    {
+        public static void DoEvents(this Dispatcher dispatcher){
+            DispatcherFrame frame = new DispatcherFrame();
+
+            dispatcher.BeginInvoke(
+                DispatcherPriority.Background,
+                new DispatcherOperationCallback(delegate(object o) {
+                    ((DispatcherFrame)o).Continue = false;
+                    return null;
+                }),
+                frame);
+
+            Dispatcher.PushFrame(frame);
+        }
+    }
+}
