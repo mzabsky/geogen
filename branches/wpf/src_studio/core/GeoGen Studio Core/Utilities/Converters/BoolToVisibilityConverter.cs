151dd5466d475ef9e750ab66f906bcf330bcbb52
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-11-25 09:23:54 +0000

    [WPF Branch] Studio: Added BoolToVisibilityConverter and CollectionToVisibilityConverter.

---
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace GeoGen.Studio.Utilities.Converters
+{
+	/// <summary>
+	/// Converts a bool to <see cref="Visibility"/>. False (or null) will be converted to value Collapsed, any true will be converted to Visible.
+	/// </summary>
+	[ValueConversion(typeof(bool), typeof(Visibility))]
+	public sealed class BoolToVisibilityConverter : IValueConverter
+	{
+		private static readonly IValueConverter instance = new BoolToVisibilityConverter();
+		/// <summary>
+		/// Gets instance of this converter.
+		/// </summary>
+		/// <value>The instance.</value>
+		public static IValueConverter Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				if (value == null || !((bool) value)) return Visibility.Collapsed;
+
+				return Visibility.Visible;
+			}
+			catch (InvalidCastException) { }
+			return "<Unknown value>";
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
+
+
