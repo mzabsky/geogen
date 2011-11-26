b0011dd36e288310180172e702fe45bacc45fb02
Matěj Zábský <mzabsky@gmail.com>
2011-11-26 10:01:54 +0000

    [WPF Branch] Studio: Moved IMainWIndow into interfaces namespace.

--- src_studio/plugins/MenuBar/MenuBar.xaml.cs
+++ src_studio/plugins/MenuBar/MenuBar.xaml.cs
@@ -1,121 +1,122 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using GeoGen.Studio.Utilities;
-using GeoGen.Studio.Utilities.Collections;
-using System.Windows.Markup;
-using System.Collections;
-using System.Collections.ObjectModel;
-
-namespace GeoGen.Studio.PlugIns
+﻿namespace GeoGen.Studio.PlugIns
 {
-    /// <summary>
-    /// A horizontal menu bar which displays items provided by other plug-ins.
-    /// </summary>
-    public partial class MenuBar : GeoGen.Studio.Utilities.PlugInBase.ControlBase, IMenuBar, IPriority
-    {
-        public double Priority
-        {
-            get
-            {
-                return 1;
-            }
-        }
+	using System;
+	using System.Collections;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Linq;
+	using System.Text;
+	using System.Windows;
+	using System.Windows.Controls;
+	using System.Windows.Data;
+	using System.Windows.Documents;
+	using System.Windows.Input;
+	using System.Windows.Markup;
+	using System.Windows.Media;
+	using System.Windows.Media.Imaging;
+	using System.Windows.Navigation;
+	using System.Windows.Shapes;
+	using GeoGen.Studio.PlugIns.Interfaces;
+	using GeoGen.Studio.Utilities;
+	using GeoGen.Studio.Utilities.Collections;
 
-        private static readonly DependencyProperty MenusProperty = DependencyProperty.Register(
-            "Menus", typeof(MenuEntryObservableCollection), typeof(MenuBar), new PropertyMetadata(new MenuEntryObservableCollection()));
+	/// <summary>
+	/// A horizontal menu bar which displays items provided by other plug-ins.
+	/// </summary>
+	public partial class MenuBar : GeoGen.Studio.Utilities.PlugInBase.ControlBase, IMenuBar, IPriority
+	{
+		public double Priority
+		{
+			get
+			{
+				return 1;
+			}
+		}
 
-        private MenuEntryObservableCollection Menus
-        {
-            get
-            {
-                return (MenuEntryObservableCollection)GetValue(MenusProperty);
-            }
-            set
-            {
-                SetValue(MenusProperty, value);
-            }
-        }
+		private static readonly DependencyProperty MenusProperty = DependencyProperty.Register(
+			"Menus", typeof(MenuEntryObservableCollection), typeof(MenuBar), new PropertyMetadata(new MenuEntryObservableCollection()));
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="MenuBar"/> class.
-        /// </summary>
-        public MenuBar()
-        {
-            InitializeComponent();
+		private MenuEntryObservableCollection Menus
+		{
+			get
+			{
+				return (MenuEntryObservableCollection)GetValue(MenusProperty);
+			}
+			set
+			{
+				SetValue(MenusProperty, value);
+			}
+		}
 
-            // The menu contains only Exit item by default
-            MenuEntry fileMenu = new MenuEntry(
-                header: "File",
-                priority: 0
-            );
-            
-            this.AddMenu(fileMenu);
-            
-            fileMenu.Items.Add(
-                new MenuEntry(
-                    header: "Exit",
-                    priority: Double.MinValue,
-                    command: new RelayCommand(param => App.Current.Shutdown()),
-                    inputGestureText: "Alt+F4"
-                )
-            );
-        }
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MenuBar"/> class.
+		/// </summary>
+		public MenuBar()
+		{
+			InitializeComponent();
 
-        /// <summary>
-        /// Plug-in registrator. Do not call manually.
-        /// </summary>
-        /// <param name="mainWindow">The main window.</param>
-        public void Register(IMainWindow mainWindow)
-        {
-            mainWindow.TopBars.Add(this);
-        }
+			// The menu contains only Exit item by default
+			MenuEntry fileMenu = new MenuEntry(
+				header: "File",
+				priority: 0
+			);
+			
+			this.AddMenu(fileMenu);
+			
+			fileMenu.Items.Add(
+				new MenuEntry(
+					header: "Exit",
+					priority: Double.MinValue,
+					command: new RelayCommand(param => App.Current.Shutdown()),
+					inputGestureText: "Alt+F4"
+				)
+			);
+		}
 
-        /// <summary>
-        /// Merges one <see cref="MenuEntry"/> into the menu item hierarchy.
-        /// </summary>
-        /// <param name="menu">The menu item.</param>
-        public void AddMenu(MenuEntry menu)
-        {
-            // Null IS allowed (for simplicity's sake)
-            if (menu == null) return;
+		/// <summary>
+		/// Plug-in registrator. Do not call manually.
+		/// </summary>
+		/// <param name="mainWindow">The main window.</param>
+		public void Register(IMainWindow mainWindow)
+		{
+			mainWindow.TopBars.Add(this);
+		}
 
-            foreach (MenuEntry ownItem in this.Menus)
-            {
-                try
-                {
-                    if (ownItem.Header == menu.Header)
-                    {
-                        ownItem.MergeWith(menu);
+		/// <summary>
+		/// Merges one <see cref="MenuEntry"/> into the menu item hierarchy.
+		/// </summary>
+		/// <param name="menu">The menu item.</param>
+		public void AddMenu(MenuEntry menu)
+		{
+			// Null IS allowed (for simplicity's sake)
+			if (menu == null) return;
 
-                        return;
-                    }
-                }
-                catch(Exception e){
+			foreach (MenuEntry ownItem in this.Menus)
+			{
+				try
+				{
+					if (ownItem.Header == menu.Header)
+					{
+						ownItem.MergeWith(menu);
 
-                }
-            }
+						return;
+					}
+				}
+				catch(Exception e){
 
-            this.Menus.Add(menu);
-        }
+				}
+			}
 
-        /// <summary>
-        /// Merges one <see cref="MenuItem"/> into the menu item hierarchy.
-        /// </summary>
-        /// <param name="menu">The menu item.</param>
-        public void AddMenu(MenuItem menu)
-        {          
-            this.AddMenu((MenuEntry) menu);
-        }
-    }
+			this.Menus.Add(menu);
+		}
+
+		/// <summary>
+		/// Merges one <see cref="MenuItem"/> into the menu item hierarchy.
+		/// </summary>
+		/// <param name="menu">The menu item.</param>
+		public void AddMenu(MenuItem menu)
+		{          
+			this.AddMenu((MenuEntry) menu);
+		}
+	}
 }
