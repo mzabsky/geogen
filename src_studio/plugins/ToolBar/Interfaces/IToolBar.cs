4e0a3e1dd85515be771cffc12b287a478b96719f
Matěj Zábský <mzabsky@gmail.com>
2011-11-27 09:49:57 +0000

    [WPF Branch] Studio: IToolBar now also implements IControl.

--- src_studio/plugins/ToolBar/Interfaces/IToolBar.cs
+++ src_studio/plugins/ToolBar/Interfaces/IToolBar.cs
@@ -2,35 +2,36 @@
 {
 	using System.Windows.Controls;
 	using System.Windows.Controls.Primitives;
+	using GeoGen.Studio.PlugInLoader;
 	using GeoGen.Studio.PlugIns.ToolBars;
 
-    /// <summary>
-    /// Base for other toolbar-related interfaces.
-    /// </summary>
-    public interface IToolBar
-    {
-        /// <summary>
-        /// Adds a WPF separator to the toolbar.
-        /// </summary>
-        /// <param name="item">The separator.</param>
-        void AddItem(Separator item);
+	/// <summary>
+	/// Base for other toolbar-related interfaces.
+	/// </summary>
+	public interface IToolBar : IPlugInInterface, IControl
+	{
+		/// <summary>
+		/// Adds a WPF separator to the toolbar.
+		/// </summary>
+		/// <param name="item">The separator.</param>
+		void AddItem(Separator item);
 
-        /// <summary>
-        /// Adds a common WPF button to the toolbar.
-        /// </summary>
-        /// <param name="item">The button.</param>
-        void AddItem(Button item);
+		/// <summary>
+		/// Adds a common WPF button to the toolbar.
+		/// </summary>
+		/// <param name="item">The button.</param>
+		void AddItem(Button item);
 
-        /// <summary>
-        /// Adds a WPF toggle button to the toolbar.
-        /// </summary>
-        /// <param name="item">The button.</param>
-        void AddItem(ToggleButton item);
+		/// <summary>
+		/// Adds a WPF toggle button to the toolbar.
+		/// </summary>
+		/// <param name="item">The button.</param>
+		void AddItem(ToggleButton item);
 
-        /// <summary>
-        /// Adds a <see cref="ToolBarEntry"/> to the toolbar.
-        /// </summary>
-        /// <param name="item">The item.</param>
-        void AddItem(ToolBarEntry item);
-    }
+		/// <summary>
+		/// Adds a <see cref="ToolBarEntry"/> to the toolbar.
+		/// </summary>
+		/// <param name="item">The item.</param>
+		void AddItem(ToolBarEntry item);
+	}
 }
