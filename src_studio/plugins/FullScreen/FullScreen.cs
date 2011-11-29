37fbc891fd40698fe53d860cb8768f80e81c7eb0
Matěj Zábský <mzabsky@gmail.com>
2011-11-29 16:47:49 +0000

    [WPF Branch] Studio: Full screen now covers taskbar.

--- src_studio/plugins/FullScreen/FullScreen.cs
+++ src_studio/plugins/FullScreen/FullScreen.cs
@@ -7,14 +7,15 @@
 	using System.Windows.Controls;
 	using System.Windows.Data;
 	using System.Windows.Input;
+	using System.Windows.Media;	
 	using GeoGen.Studio.PlugInLoader;
 	using GeoGen.Studio.PlugIns.Interfaces;
 	using GeoGen.Studio.PlugIns.MenuBars;
 	using GeoGen.Studio.PlugIns.ToolBars;
 	using GeoGen.Studio.Utilities;
-	using GeoGen.Studio.Utilities.PlugInBase;	
+	using GeoGen.Studio.Utilities.PlugInBase;
 
-	public class FullScreen: ObjectBase, IPlugIn, INotifyPropertyChanged
+	public sealed class FullScreen: ObjectBase, IPlugIn, INotifyPropertyChanged
 	{
 		private Window mainWindow;
 		private List<Control> bars = new List<Control>();
@@ -35,7 +36,7 @@
 
 		public FullScreen()
 		{
-			this.toggleFullScreenCommand = new RelayCommand(p => this.ToggleFullScreen());
+			this.toggleFullScreenCommand = new RelayCommand(p => this.ToggleFullScreen());			
 		}
 
 		public void Register(IMainWindow mainWindow){
@@ -45,8 +46,8 @@
 				if (this.mainWindow.WindowStyle == WindowStyle.None)
 				{
 					this.ToggleFullScreen();
-				}
-			};
+				}				
+			};			
 		}
 
 		public void Register(IMainWindowToolBar toolBar)
@@ -98,30 +99,93 @@
 			if (this.mainWindow.WindowStyle != WindowStyle.None)
 			{
 				this.windowStateBackup = this.mainWindow.WindowState;
-				this.mainWindow.WindowState = WindowState.Maximized;
+				this.mainWindow.WindowState = WindowState.Normal;
+				
 				this.mainWindow.WindowStyle = WindowStyle.None;
+				this.mainWindow.WindowState = WindowState.Maximized;
+				this.mainWindow.Topmost = true;
+				this.mainWindow.Top = 0;
+				this.mainWindow.Left = 0;
 
-				Rect screenBounds = WpfScreen.GetScreen(this.mainWindow).DeviceBounds;
-				this.mainWindow.Width = screenBounds.Width;
-				this.mainWindow.Height = screenBounds.Height;
+				this.HideBars();
 
-				foreach (Control bar in this.bars)
-				{
-					bar.Visibility = Visibility.Collapsed;
-				}
+				this.mainWindow.MouseMove += this.HandleMouseMove;
 			}
 			else
 			{
+				this.mainWindow.ResizeMode = ResizeMode.CanResize;
 				this.mainWindow.WindowState = this.windowStateBackup;
 				this.mainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+				this.mainWindow.Topmost = false;
 
-				foreach (Control bar in this.bars)
-				{
-					bar.Visibility = Visibility.Visible;
-				}
+				this.ShowBars();
+
+				this.mainWindow.MouseMove -= this.HandleMouseMove;
 			}
 
 			GeoGen.Studio.App.Current.Dispatcher.BeginInvoke((Action)delegate{this.OnPropertyChanged("IsFullScreen");});
+		}
+
+		private void ShowBars(){
+			foreach (Control bar in this.bars)
+			{
+				bar.Visibility = Visibility.Visible;
+			}
+		}
+
+		private void HideBars()
+		{
+			foreach (Control bar in this.bars)
+			{
+				bar.Visibility = Visibility.Collapsed;
+			}
+		}
+
+		private void HandleMouseMove(object sender, MouseEventArgs args){
+			// Display the bars if the mouse is on top border of the screen
+			if (args.MouseDevice.GetPosition(this.mainWindow).Y <= 1)
+			{
+				this.ShowBars();
+				this.mainWindow.MouseMove -= this.HandleMouseMove;
+				
+				foreach (Control bar in this.bars){
+					bar.MouseLeave += this.HandleMouseLeave;
+					bar.IsHitTestVisible = true;
+				}
+			}
+		}
+
+		private void HandleMouseLeave(object sender, MouseEventArgs args)
+		{
+			// Check if the mouse is still above some of the bars
+			bool barWasHit = false;
+			VisualTreeHelper.HitTest(
+				reference: this.mainWindow,
+				filterCallback: null,
+				resultCallback: (HitTestResultCallback) delegate(HitTestResult p){
+					barWasHit |= this.bars.Contains(p.VisualHit as Control);
+					return HitTestResultBehavior.Continue;//barWasHit ? HitTestResultBehavior.Stop : HitTestResultBehavior.Continue;
+				},
+				hitTestParameters: new PointHitTestParameters(args.MouseDevice.GetPosition(this.mainWindow))
+			);
+
+			//VisualTreeHelper.
+
+			// If the mouse is away from any bar, hide the bars
+			if (!barWasHit)
+			{
+				foreach (Control bar in this.bars)
+				{
+					bar.MouseLeave -= this.HandleMouseLeave;
+				}
+
+				this.HideBars();
+				this.mainWindow.MouseMove += this.HandleMouseMove;
+			}
+			else
+			{
+				Console.Write("sad");
+			}			
 		}
 	}
 }
