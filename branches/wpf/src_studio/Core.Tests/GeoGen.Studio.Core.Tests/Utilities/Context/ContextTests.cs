7473584e9153b1a7aff00d8ad96f7ab705956468
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-11-29 18:12:48 +0000

    [WPF Branch] Studio: Added the 3D viewer to the solution.

---
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Context/ContextTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace GeoGen.Studio.Utilities.Context
+{
+    [TestFixture]
+    class ContextTests
+    {
+        [Test]
+        public void Context_Construct_LabelsMatch()
+        {
+            var label = "test";
+            var context = new Context(label);
+            Assert.AreEqual(context.Label, label);
+        }
+
+        [Test]
+        public void Equals_OneContext_True()
+        {
+            var context = new Context("test");
+            Assert.IsTrue(context.Equals(context));
+        }
+
+        [Test]
+        public void Equals_SameLabel_False()
+        {
+            var context1 = new Context("test");
+            var context2 = new Context("test");
+            Assert.IsFalse(context1.Equals(context2));
+        }
+    }
+}
