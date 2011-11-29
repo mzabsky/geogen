3975e08d88a7e7f06e48b5006efab284361c4abc
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2011-11-29 22:15:26 +0000

    [WPF Branch] Studio: Added new plug-in: ScriptValidator.

---
+++ branches/wpf/src_studio/ScriptValidator/ScriptValidator.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GeoGen.Studio.Utilities.PlugInBase;
+using System.ComponentModel;
+using System.Windows.Media;
+
+namespace GeoGen.Studio.PlugIns
+{
+	public sealed class ScriptValidator: ObjectBase
+	{
+		private IGenerator generator;
+		private IEditor editor;
+
+		private bool isSyntaxCheckScheduled;
+
+		public void Register(IGenerator generator, IEditor editor)
+		{
+			this.editor = editor;
+			this.generator = generator;
+
+			editor.PropertyChanged += delegate(object o, PropertyChangedEventArgs args){
+				if (args.PropertyName != "Text")
+				{
+					return;
+				}
+
+				this.ScheduleSyntaxCheck();
+			};
+
+			generator.Started += delegate {
+				this.isSyntaxCheckScheduled = false;
+			};
+
+			generator.HeaderLoaded += delegate {
+				// The header was executed successfuly - it is valid
+				this.HideError();
+
+				// ... and recheck if the script text changed in the meantime
+				this.StartCheckIfScheduled();
+			};
+
+			generator.Finished += delegate {
+				// The generator might have run for considerable amount of time, the script might
+				// have to be rechecked.
+				this.StartCheckIfScheduled();
+			};
+
+			generator.Failed += delegate (object o, GenerationFailedEventArgs args){
+				if(!args.IsHeaderLoaded){
+					this.DisplayError();
+				}
+			};
+		}
+
+		public void StartCheck(){
+			generator.Start(this.editor.Text, true);
+		}
+
+		public void StartCheckIfScheduled()
+		{
+			if (!this.isSyntaxCheckScheduled)
+			{
+				return;
+			}
+
+			this.StartCheck();
+		}
+
+		public void ScheduleSyntaxCheck()
+		{
+			if(this.isSyntaxCheckScheduled){
+				// A check is already waiting to be performed.
+				return;
+			}
+			else if (!generator.IsReady)
+			{
+				// The generator is not available right now, do it later.
+				this.isSyntaxCheckScheduled = true;
+			}
+			else
+			{
+				// The generator is free to go without waiting.
+				this.StartCheck();
+			}
+		}
+
+		private void DisplayError()
+		{
+			this.editor.Control.BorderBrush = new SolidColorBrush(Colors.Red);
+			this.editor.Control.BorderThickness = new System.Windows.Thickness(1);
+		}
+
+		private void HideError()
+		{
+			this.editor.Control.BorderBrush = null;
+			this.editor.Control.BorderThickness = new System.Windows.Thickness(0);
+		}
+	}
+}
