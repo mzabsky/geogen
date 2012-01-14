dba423c0275dcdd5e03f0c625226a744f6fc84b8
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-01-14 22:46:45 +0000

    [WPF Branch] Tests: Covered PlugInBase classes.

--- branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/ObservableObjectTests.cs
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/ObservableObjectTests.cs
@@ -1,10 +1,6 @@
 ﻿namespace GeoGen.Studio.Utilities
 {
-	using System;
-	using System.Collections.Generic;
 	using System.ComponentModel;
-	using System.Linq;
-	using System.Text;
 
 	using GeoGen.Studio.Utilities.Testing;
 
