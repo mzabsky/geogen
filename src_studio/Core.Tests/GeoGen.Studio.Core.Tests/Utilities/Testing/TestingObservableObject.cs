e3d9bf149b724c0b5133510276dc2ea551044579
Matěj Zábský <mzabsky@gmail.com>
2012-01-14 22:01:44 +0000

    [WPF Branch] Tests: Added a support ObservableObject testing class.

---
+++ src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Testing/TestingObservableObject.cs
@@ -0,0 +1,29 @@
+﻿namespace GeoGen.Studio.Utilities.Testing
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Text;
+
+	/// <summary>
+	/// Helper class for <see cref="ObservableObjectTests"/>.
+	/// </summary>
+	public class TestingObservableObject : ObservableObject
+	{
+		private int someProperty;
+		
+		public int SomeProperty
+		{
+			get
+			{
+				return someProperty;
+			}
+
+			set
+			{
+				this.someProperty = value;
+				this.OnPropertyChanged("SomeProperty");
+			}
+		}
+	}
+}
