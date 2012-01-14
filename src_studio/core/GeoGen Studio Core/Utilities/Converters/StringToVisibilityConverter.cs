0c88e2f7935eaa106868a8e015c0a13347310673
Matěj Zábský <mzabsky@gmail.com>
2012-01-14 19:02:09 +0000

    [WPF Branch] Studio: Changed StringToVisibilityConverter to return invalid when it gets non-string non-null object.

--- src_studio/core/GeoGen Studio Core/Utilities/Converters/StringToVisibilityConverter.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Converters/StringToVisibilityConverter.cs
@@ -1,44 +1,45 @@
-﻿using System;
-using System.Globalization;
-using System.Windows;
-using System.Windows.Data;
-
-namespace GeoGen.Studio.Utilities.Converters
+﻿namespace GeoGen.Studio.Utilities.Converters
 {
-    /// <summary>
-    /// Converts a string to <see cref="Visibility"/>. Empty string (or null) will be converted to value Collapsed, any other string will be converted to Visible.
-    /// </summary>
-    [ValueConversion(typeof(string), typeof(Visibility))]
-    public sealed class StringToVisibilityConverter : IValueConverter
-    {
-        private static readonly IValueConverter instance = new StringToVisibilityConverter();
-        /// <summary>
-        /// Gets instance of this converter.
-        /// </summary>
-        /// <value>The instance.</value>
-        public static IValueConverter Instance
-        {
-            get
-            {
-                return instance;
-            }
-        }
-        
-        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            try
-            {
-                if (string.IsNullOrEmpty(value as string)) return Visibility.Collapsed;
-                
-                return Visibility.Visible;
-            }
-            catch (InvalidCastException) { }
-            return "<Unknown value>";
-        }
+	using System;
+	using System.Globalization;
+	using System.Windows;
+	using System.Windows.Data;
 
-        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return null;
-        }
-    }
+	/// <summary>
+	/// Converts a string to <see cref="Visibility"/>. Empty string (or null) will be converted to value Collapsed, any other string will be converted to Visible.
+	/// </summary>
+	[ValueConversion(typeof(string), typeof(Visibility))]
+	public sealed class StringToVisibilityConverter : IValueConverter
+	{
+		private static readonly IValueConverter instance = new StringToVisibilityConverter();
+
+		/// <summary>
+		/// Gets instance of this converter.
+		/// </summary>
+		/// <value>The instance.</value>
+		public static IValueConverter Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			try
+			{
+				if (string.IsNullOrEmpty((string)value)) return Visibility.Collapsed;
+
+				return Visibility.Visible;
+			}
+			catch (InvalidCastException) { }
+			return "<Unknown value>";
+		}
+
+		object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
 }
