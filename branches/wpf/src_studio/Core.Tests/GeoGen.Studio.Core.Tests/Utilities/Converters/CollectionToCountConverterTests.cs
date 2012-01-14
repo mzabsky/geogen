b39d82f405b4b66d2881b4c0a93f5d9fb9077f76
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-01-14 19:10:06 +0000

    [WPF Branch] Tests: Converters are now 100% covered.

---
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Converters/CollectionToCountConverterTests.cs
@@ -0,0 +1,58 @@
+﻿namespace GeoGen.Studio.Utilities.Converters
+{
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Windows.Data;
+	using NUnit.Framework;
+
+	// ReSharper disable InconsistentNaming
+	/// <summary>
+	/// Tests for <see cref="CollectionToCountConverter"/> class.
+	/// </summary>
+	[TestFixture]
+	public class CollectionToCountConverterTests
+	{
+		/// <summary>
+		/// Tests that instance getter really returns valids instance.
+		/// </summary>
+		[Test]
+		public void TestInstance_ReturnsInstance()
+		{
+			Assert.IsInstanceOf<CollectionToCountConverter>(CollectionToCountConverter.Instance);
+		}
+
+		/// <summary>
+		/// Tests that a collection gets converted to its count.
+		/// </summary>
+		[Test]
+		public void TestConvert_Collection_ReturnsCount()
+		{
+			IValueConverter converter = new CollectionToCountConverter();
+			int result = (int)converter.Convert(new List<int> { 2, 4, 6 }, typeof(int), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, 3);
+		}
+
+		/// <summary>
+		/// Tests that a non-collection gets converted to -1.
+		/// </summary>
+		[Test]
+		public void TestConvert_Invalid_ReturnsMinusOne()
+		{
+			IValueConverter converter = new CollectionToCountConverter();
+			int result = (int)converter.Convert(null, typeof(int), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, -1);
+		}
+
+		/// <summary>
+		/// Tests that ConverBack doesn't crash.
+		/// </summary>
+		[Test]
+		public void TestConvertBack_Null_NoException()
+		{
+			IValueConverter converter = new CollectionToCountConverter();
+			converter.ConvertBack(null, typeof(int), null, CultureInfo.CurrentCulture);
+		}
+	}
+}
---
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Converters/CollectionToVisibilityConverterTests.cs
@@ -0,0 +1,74 @@
+﻿namespace GeoGen.Studio.Utilities.Converters
+{
+	using System.Collections.Generic;
+	using System.Globalization;
+	using System.Linq;
+	using System.Windows;
+	using System.Windows.Data;
+
+	using NUnit.Framework;
+
+	// ReSharper disable InconsistentNaming
+
+	/// <summary>
+	/// Tests for <see cref="CollectionToVisibilityConverter"/> class.
+	/// </summary>
+	[TestFixture]
+	public class CollectionToVisibilityConverterTests
+	{
+		/// <summary>
+		/// Tests that instance getter really returns valids instance.
+		/// </summary>
+		[Test]
+		public void TestInstance_ReturnsInstance()
+		{
+			Assert.IsInstanceOf<CollectionToVisibilityConverter>(CollectionToVisibilityConverter.Instance);
+		}
+
+		/// <summary>
+		/// Tests that collection gets converted to <see cref="Visibility.Visible"/>.
+		/// </summary>
+		[Test]
+		public void TestConvert_Collection_ReturnsVisible()
+		{
+			IValueConverter converter = new CollectionToVisibilityConverter();
+			Visibility result = (Visibility)converter.Convert(new List<int> { 2, 4, 6 }, typeof(Visibility), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, Visibility.Visible);
+		}
+
+		/// <summary>
+		/// Tests that empty collection gets converted to <see cref="Visibility.Collapsed"/>.
+		/// </summary>
+		[Test]
+		public void TestConvert_Empty_ReturnsCollapsed()
+		{
+			IValueConverter converter = new CollectionToVisibilityConverter();
+			Visibility result = (Visibility)converter.Convert(Enumerable.Empty<int>(), typeof(Visibility), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, Visibility.Collapsed);
+		}
+
+		/// <summary>
+		/// Tests that anything else gets converted to "Unknown value".
+		/// </summary>
+		[Test]
+		public void TestConvert_Invalid_ReturnsInvalid()
+		{
+			IValueConverter converter = new CollectionToVisibilityConverter();
+			string result = (string)converter.Convert(new object(), typeof(Visibility), null, CultureInfo.CurrentCulture);
+
+			Assert.AreEqual(result, "<Unknown value>");
+		}
+
+		/// <summary>
+		/// Tests that ConverBack doesn't crash.
+		/// </summary>
+		[Test]
+		public void TestConvertBack_Null_NoException()
+		{
+			IValueConverter converter = new CollectionToVisibilityConverter();
+			converter.ConvertBack(null, typeof(int), null, CultureInfo.CurrentCulture);
+		}
+	}
+}
