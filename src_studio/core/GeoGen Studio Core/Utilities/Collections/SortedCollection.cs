8d8b445f7facecac3fe2273d44c4865d938d3819
Matěj Zábský <mzabsky@gmail.com>
2012-01-18 15:07:35 +0000

    [WPF Branch] Studio: Simplified and refactored SortedCollection.

--- src_studio/core/GeoGen Studio Core/Utilities/Collections/SortedCollection.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Collections/SortedCollection.cs
@@ -1,398 +1,332 @@
-﻿//http://softcollections.codeplex.com/
-
-using System;
-using System.Collections.Generic;
-using System.Collections;
-
+﻿// http://softcollections.codeplex.com/
 namespace GeoGen.Studio.Utilities.Collections
 {
-	using System.ComponentModel;
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
 
-	/// <summary>
-	/// Represents a dynamic data collection. The items are sorted by a given comparer.
-	/// </summary>
-	public class SortedCollection<TValue> : IList<TValue>, IList
-	{
-		// Fields
-		private const int defaultCapacity = 4;
+    /// <summary>
+    /// Represents a dynamic data collection. The items are sorted by a given <see cref="IComparer"/>.
+    /// </summary>
+    public class SortedCollection<TValue> : IList<TValue>, IList
+    {
+        /// <summary>
+        /// Default value for Capacity property.
+        /// </summary>
+        private const int DefaultCapacity = 4;
 
-		private static readonly TValue[] emptyValues;
+        /// <summary>
+        /// Array used when there are no items in the collection. Shared by all instances.
+        /// </summary>
+        private static readonly TValue[] EmptyBackingArray;
 
-		protected IComparer<TValue> comparer;
-		private TValue[] values;
-		private int size;
-		// for enumeration
-		private int version;
+        /// <summary>
+        /// Actual collection holding the backingArray of this collection.
+        /// </summary>
+        private TValue[] backingArray;
 
-		public bool IsFixedSize { get { return true;} }
-		public bool IsSynchronized { get { return false; } }
-		public object SyncRoot { get { return new object(); } }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedCollection&lt;TValue&gt;"/> class.
+        /// </summary>
+        /// <param name="comparer">The comparer.</param>
+        public SortedCollection(IComparer<TValue> comparer)
+        {
+            this.backingArray = EmptyBackingArray;
+            this.Comparer = comparer;
+        }
 
-		static SortedCollection()
-		{
-			emptyValues = new TValue[0];
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SortedCollection&lt;TValue&gt;"/> class.
+        /// </summary>
+        public SortedCollection()
+        {
+            this.backingArray = EmptyBackingArray;
+            this.Comparer = Comparer<TValue>.Default;
+        }
 
+        /// <summary>
+        /// Initializes the <see cref="SortedCollection&lt;TValue&gt;"/> class.
+        /// </summary>
+        static SortedCollection()
+        {
+            EmptyBackingArray = new TValue[0];
+        }
 
-		/// <summary>
-		/// Initializes a new empty instance of the <see cref="SortedCollection&lt;TValue&gt;"/> class.
-		/// </summary>
-		public SortedCollection()
-		{
-			this.values = emptyValues;
-			this.comparer = Comparer<TValue>.Default;
-		}
+        /// <summary>
+        /// Capacity of the collection.
+        /// </summary>
+        /// <value>The capacity.</value>
+        public int Capacity
+        {
+            get { return this.backingArray.Length; }
+            set
+            {
+                if (this.backingArray.Length == value)
+                {
+                    return;
+                }
 
-		/// <summary>
-		/// Initializes a new empty instance of the <see cref="SortedCollection&lt;TValue&gt;"/> class.
-		/// </summary>
-		/// <param name="comparer">The comparer.</param>
-		public SortedCollection(IComparer<TValue> comparer)
-		{
-			this.values = emptyValues;
-			this.comparer = comparer;
-		}
+                if (value < this.Count)
+                {
+                    throw new ArgumentException("Too small capacity.");
+                }
 
-		// Methods
-		private void CheckCapacity(int min)
-		{
-			// double the capacity
-			int num = this.values.Length == 0 ? defaultCapacity : this.values.Length * 2;
-			if (min > num)
-			{
-				num = min;
-			}
-			this.Capacity = num;
-		}
+                if (value > 0)
+                {
+                    var tempValues = new TValue[value];
+                    if (this.Count > 0)
+                    {
+                        // copy only when size is greater than zero
+                        Array.Copy(this.backingArray, 0, tempValues, 0, this.Count);
+                    }
+                    this.backingArray = tempValues;
+                }
+                else
+                {
+                    this.backingArray = EmptyBackingArray;
+                }
+            }
+        }
 
-		public virtual void Insert(int index, TValue value)
-		{
-			if (value == null)
-			{
-				throw new ArgumentException("Value can't be null.");
-			}
-			if (index < 0 || index > this.size)
-			{
-				throw new ArgumentOutOfRangeException();
-			}
-			if (this.size == this.values.Length)
-			{
-				this.CheckCapacity(this.size + 1);
-			}
-			if (index < this.size)
-			{
-				Array.Copy(this.values, index, this.values, index + 1, this.size - index);
-			}
-			this.SubscribeItem(value);
-			this.values[index] = value;
-			this.size++;
-			this.version++;
-		}
+        public IComparer<TValue> Comparer { get; protected set; }
 
-		public virtual void Insert(int index, object value)
-		{
-			this.Insert(index, (TValue) value);
-		}
+        public int Count { get; private set; }
 
-		public void Add(TValue value)
-		{
-			if (value == null)
-			{
-				throw new ArgumentException("Value can't be null");
-			}
-			// check where the element should be placed
-			int index = Array.BinarySearch(values, 0, this.size, value, this.comparer);
-			if (index < 0)
-			{
-				// xor
-				index = ~index;
-			}
-			//else{
-				while(index < this.size && this.comparer.Compare(this.values[index],value) == 0)
-				{
-					index++;
-				}
+        public virtual TValue this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
 
-				/*if(index < this.size)
-				{
-					index++;
-				}*/
-			//}
-			Insert(index, value);
-		}
+                return this.backingArray[index];
+            }
 
-		public int Add(object value)
-		{
-			this.Add((TValue)value);
-			return this.IndexOf((TValue)value);
-		}
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
 
-		public virtual void Clear()
-		{
-			foreach (var value in this.values)
-			{
-				this.UnsubscribeItem(value);
-			}
+                this.UnsubscribeItem(this.backingArray[index]);
+                this.SubscribeItem(value);
+                this.backingArray[index] = value;
+            }
+        }
 
-			this.version++;
-			Array.Clear(this.values, 0, this.size);
-			this.size = 0;
-		}
+        bool ICollection.IsSynchronized
+        {
+            get { return false; }
+        }
 
-		public int IndexOf(TValue value)
-		{
-			if (value == null)
-			{
-				throw new ArgumentException("Value can't be null.");
-			}
-			int index = Array.BinarySearch(values, 0, this.size, value, this.comparer);
-			if (index >= 0)
-			{
-				return index;
-			}
-			return -1;
-		}
+        object ICollection.SyncRoot
+        {
+            get { return new object(); }
+        }
 
-		public int IndexOf(object value)
-		{
-			return this.IndexOf((TValue) value);
-		}
+        bool ICollection<TValue>.IsReadOnly
+        {
+            get { return false; }
+        }
 
-		public bool Contains(TValue value)
-		{
-			return this.IndexOf(value) >= 0;
-		}
+        bool IList.IsFixedSize
+        {
+            get { return true; }
+        }
 
-		public bool Contains(object value)
-		{
-			return this.Contains((TValue) value);
-		}
+        bool IList.IsReadOnly
+        {
+            get { return false; }
+        }
 
-		public void CopyTo(TValue[] array, int arrayIndex)
-		{
-			Array.Copy(this.values, 0, array, arrayIndex, this.size);
-		}
+        object IList.this[int index]
+        {
+            get
+            {
+                return this[index];
+            }
 
-		public void CopyTo(System.Array array, int arrayIndex)
-		{
-			Array.Copy(this.values, 0, array, arrayIndex, this.size);
-		}
+            set
+            {
+                this[index] = (TValue)value;
+            }
+        }
 
-		public int Count
-		{
-			get { return this.size; }
-		}
+        public void Add(TValue value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            // check where the element should be placed
+            int index = Array.BinarySearch(this.backingArray, 0, this.Count, value, this.Comparer);
+            if (index < 0)
+            {
+                // xor
+                index = ~index;
+            }
 
-		public bool IsReadOnly
-		{
-			get { return false; }
-		}
+            while (index < this.Count && this.Comparer.Compare(this.backingArray[index], value) == 0)
+            {
+                index++;
+            }
 
-		public bool Remove(TValue value)
-		{
-			int index = this.IndexOf(value);
-			if (index < 0)
-			{
-				return false;
-			}
-			RemoveAt(index);
-			return true;
-		}
+            Insert(index, value);
+        }
 
-		public void Remove(object value)
-		{
-			this.Remove((TValue) value);
-		}
+        public virtual void Clear()
+        {
+            foreach (var value in this.backingArray)
+            {
+                this.UnsubscribeItem(value);
+            }
 
-		public IEnumerator<TValue> GetEnumerator()
-		{
-			return new SortedCollectionEnumerator(this);
-		}
+            Array.Clear(this.backingArray, 0, this.Count);
+            this.Count = 0;
+        }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return new SortedCollectionEnumerator(this);
-		}
+        public bool Contains(TValue value)
+        {
+            return this.IndexOf(value) >= 0;
+        }
 
+        public void CopyTo(TValue[] array, int arrayIndex)
+        {
+            Array.Copy(this.backingArray, 0, array, arrayIndex, this.Count);
+        }
 
-		/// <summary>
-		/// Capacity of the collection.
-		/// </summary>
-		/// <value>The capacity.</value>
-		public int Capacity
-		{
-			get { return this.values.Length; }
-			set
-			{
-				if (this.values.Length != value)
-				{
-					if (value < this.size)
-					{
-						throw new ArgumentException("Too small capacity.");
-					}
-					if (value > 0)
-					{
-						TValue[] tempValues = new TValue[value];
-						if (this.size > 0)
-						{
-							// copy only when size is greater than zero
-							Array.Copy(this.values, 0, tempValues, 0, this.size);
-						}
-						this.values = tempValues;
-					}
-					else
-					{
-						this.values = emptyValues;
-					}
-				}
-			}
-		}
+        public IEnumerator<TValue> GetEnumerator()
+        {
+            // Skip the 0s at the end of the pre-allocated array.
+            return this.backingArray.Take(this.Count).GetEnumerator();
+        }
 
-		public virtual void RemoveAt(int index)
-		{
-			if (index < 0 || index >= this.size)
-			{
-				throw new ArgumentOutOfRangeException();
-			}
+        public int IndexOf(TValue value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            int index = Array.BinarySearch(this.backingArray, 0, this.Count, value, this.Comparer);
+            if (index >= 0)
+            {
+                return index;
+            }
+            return -1;
+        }
 
-			this.UnsubscribeItem(this[index]);
-			this.size--;
-			this.version++;			
-			Array.Copy(this.values, index + 1, this.values, index, this.size - index);
-			this.values[this.size] = default(TValue);
-		}
+        public virtual void Insert(int index, TValue value)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException("value");
+            }
+            if (index < 0 || index > this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (this.Count == this.backingArray.Length)
+            {
+                this.ExpandBackingArray();
+            }
+            if (index < this.Count)
+            {
+                Array.Copy(this.backingArray, index, this.backingArray, index + 1, this.Count - index);
+            }
+            this.SubscribeItem(value);
+            this.backingArray[index] = value;
+            this.Count++;
+        }
 
-		public virtual TValue this[int index]
-		{
-			get
-			{
-				if (index < 0 || index >= this.size)
-				{
-					throw new ArgumentOutOfRangeException();
-				}
+        public bool Remove(TValue value)
+        {
+            int index = this.IndexOf(value);
+            if (index < 0)
+            {
+                return false;
+            }
+            RemoveAt(index);
+            return true;
+        }
 
-				return this.values[index];
-			}
+        public virtual void RemoveAt(int index)
+        {
+            if (index < 0 || index >= this.Count)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
 
-			set
-			{
-				if (index < 0 || index >= this.size)
-				{
-					throw new ArgumentOutOfRangeException();
-				}
+            this.UnsubscribeItem(this[index]);
+            this.Count--;
+            Array.Copy(this.backingArray, index + 1, this.backingArray, index, this.Count - index);
+            this.backingArray[this.Count] = default(TValue);
+        }
 
-				this.UnsubscribeItem(this.values[index]);
-				this.SubscribeItem(value);
-				this.values[index] = value;
-				this.version++;
-			}
-		}
+        /// <summary>
+        /// Called when a new item is added to the collection - allows subclasses to subscribe to events on the item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        protected virtual void SubscribeItem(TValue item)
+        {
+        }
 
-		object IList.this[int index]
-		{
-			get
-			{
-				if (index < 0 || index >= this.size)
-				{
-					throw new ArgumentOutOfRangeException();
-				}
+        /// <summary>
+        /// Called when a new item is removed from the collection - allows subclasses to unsubscribe from events on the item.
+        /// </summary>
+        /// <param name="item">The item.</param>
+        protected virtual void UnsubscribeItem(TValue item)
+        {
+        }
 
-				return this.values[index];
-			}
+        /// <summary>
+        /// Expands capacity of the backing collection.
+        /// </summary>
+        private void ExpandBackingArray()
+        {
+            // double the capacity
+            int num = this.backingArray.Length == 0 ? DefaultCapacity : this.backingArray.Length * 2;
 
-			set
-			{
-				if (index < 0 || index >= this.size)
-				{
-					throw new ArgumentOutOfRangeException();
-				}
+            this.Capacity = num;
+        }
 
-				this.UnsubscribeItem(this.values[index]);
-				this.SubscribeItem((TValue)value);
-				this.values[index] = (TValue)value;
-				this.version++;
-			}
-		}
+        void ICollection.CopyTo(System.Array array, int arrayIndex)
+        {
+            Array.Copy(this.backingArray, 0, array, arrayIndex, this.Count);
+        }
 
-		protected virtual void SubscribeItem(TValue item)
-		{
-			
-		}
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
 
-		protected virtual void UnsubscribeItem(TValue item)
-		{
+        int IList.Add(object value)
+        {
+            this.Add((TValue)value);
+            return this.IndexOf((TValue)value);
+        }
 
-		}
+        bool IList.Contains(object value)
+        {
+            return this.Contains((TValue)value);
+        }
 
-		[Serializable]
-		private sealed class SortedCollectionEnumerator : IEnumerator<TValue>
-		{
-			// Fields
-			private readonly SortedCollection<TValue> collection;
-			private TValue currentValue;
-			private int index;
-			private readonly int version;
+        int IList.IndexOf(object value)
+        {
+            return this.IndexOf((TValue)value);
+        }
 
-			// Methods
-			internal SortedCollectionEnumerator(SortedCollection<TValue> collection)
-			{
-				this.collection = collection;
-				this.version = collection.version;
-			}
+        void IList.Insert(int index, object value)
+        {
+            this.Insert(index, (TValue)value);
+        }
 
-			public void Dispose()
-			{
-				this.index = 0;
-				this.currentValue = default(TValue);
-			}
-
-			public bool MoveNext()
-			{
-				if (this.version != this.collection.version)
-				{
-					throw new ArgumentException("Collection was changed while iterating!");
-				}
-				if (this.index < this.collection.Count)
-				{
-					this.currentValue = this.collection.values[this.index];
-					this.index++;
-					return true;
-				}
-				this.index = this.collection.Count + 1;
-				this.currentValue = default(TValue);
-				return false;
-			}
-
-			void IEnumerator.Reset()
-			{
-				if (this.version != this.collection.version)
-				{
-					throw new ArgumentException("Collection was changed while iterating!");
-				}
-				this.index = 0;
-				this.currentValue = default(TValue);
-			}
-
-			// Properties
-			public TValue Current
-			{
-				get
-				{
-					return this.currentValue;
-				}
-			}
-
-			object IEnumerator.Current
-			{
-				get
-				{
-					if ((this.index == 0) || (this.index == this.collection.Count + 1))
-					{
-						throw new ArgumentException("Enumerator not initialized. Call MoveNext first.");
-					}
-					return this.currentValue;
-				}
-			}
-		}
-	}
+        void IList.Remove(object value)
+        {
+            this.Remove((TValue)value);
+        }
+    }
 }
