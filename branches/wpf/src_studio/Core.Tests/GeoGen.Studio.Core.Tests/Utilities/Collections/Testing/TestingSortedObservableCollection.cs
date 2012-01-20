89d053e445473c7461e774411ecf7724a053295b
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-01-20 20:41:28 +0000

    [WPF Branch] Tests: Covered SortedObservableCollection.

---
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Collections/Testing/TestingSortedObservableCollection.cs
@@ -0,0 +1,44 @@
+﻿namespace GeoGen.Studio.Utilities.Collections.Testing
+{
+    using System;
+    using System.ComponentModel;
+
+    /// <summary>
+    /// Concrete implementation of <see cref="SortedObservableCollection{TValue}"/> for testing.
+    /// </summary>
+    public class TestingSortedObservableCollection : SortedObservableCollection<TestingPriorityObservable>
+    {
+        private readonly Action<string> itemChangedCallback;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestingSortedObservableCollection"/> class.
+        /// </summary>
+        public TestingSortedObservableCollection()
+        {
+            this.Comparer = new PriorityComparer();
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TestingSortedObservableCollection"/> class.
+        /// </summary>
+        /// <param name="itemChangedCallback">The item changed callback.</param>
+        public TestingSortedObservableCollection(Action<string> itemChangedCallback) :
+            base(new PriorityComparer())
+        {
+            this.itemChangedCallback = itemChangedCallback;
+        }
+
+        /// <summary>
+        /// Invoked by the superclass when item changes - triggers the item changed callback.
+        /// </summary>
+        /// <param name="o">Sender.</param>
+        /// <param name="args">The <see cref="System.ComponentModel.PropertyChangedEventArgs"/> instance containing the event data.</param>
+        protected override void ItemPropertyChanged(object o, PropertyChangedEventArgs args)
+        {
+            if (itemChangedCallback != null)
+            {
+                itemChangedCallback(args.PropertyName);
+            }
+        }
+    }
+}
