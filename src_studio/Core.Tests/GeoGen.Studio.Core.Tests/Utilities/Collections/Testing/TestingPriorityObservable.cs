a0642aacfb0f3c1747e899bfb19a914230702018
Matěj Zábský <mzabsky@gmail.com>
2012-01-20 20:41:28 +0000

    [WPF Branch] Tests: Covered SortedObservableCollection.

---
+++ src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Collections/Testing/TestingPriorityObservable.cs
@@ -0,0 +1,31 @@
+﻿namespace GeoGen.Studio.Utilities.Collections.Testing
+{
+    using System.ComponentModel;
+
+    /// <summary>
+    /// <see cref="ObservableObject"/> with single property for observable collection testing purposes.
+    /// </summary>
+    public class TestingPriorityObservable : ObservableObject, IPriority, INotifyPropertyChanged
+    {
+        private double priority;
+
+        /// <summary>
+        /// Property used to test <see cref="INotifyPropertyChanged"/>.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public double Priority {
+            get
+            {
+                return this.priority;
+            }
+
+            set
+            {
+                this.priority = value;
+                this.OnPropertyChanged("Priority");
+            }
+        }
+    }
+}
