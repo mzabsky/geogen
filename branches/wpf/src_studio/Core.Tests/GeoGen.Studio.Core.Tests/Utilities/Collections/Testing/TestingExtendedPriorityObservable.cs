5f4b19c10f27c72b8ecdcc0dd78b30d7167536ef
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-01-21 20:48:33 +0000

    [WPF Branch] Tests: Covered PriorityObservableCollection and PriorityObservableCollection<T>.

---
+++ branches/wpf/src_studio/Core.Tests/GeoGen.Studio.Core.Tests/Utilities/Collections/Testing/TestingExtendedPriorityObservable.cs
@@ -0,0 +1,22 @@
+﻿namespace GeoGen.Studio.Utilities.Collections.Testing
+{
+    /// <summary>
+    /// Adds one more property to test that <see cref="PriorityObservableCollection{T}"/> does not react on non-priority properties.
+    /// </summary>
+    class TestingExtendedPriorityObservable : TestingPriorityObservable
+    {
+        /// <summary>
+        /// Property which is not Priority.
+        /// </summary>
+        /// <value>
+        /// Irrelevant value.
+        /// </value>
+        public double NotPriority
+        {
+            set
+            {
+                this.OnPropertyChanged("NotPriority");
+            }
+        }
+    }
+}
