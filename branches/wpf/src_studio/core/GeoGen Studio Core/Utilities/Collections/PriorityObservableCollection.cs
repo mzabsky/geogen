cd4e9732676f853da6478bcb87e03e887f4ca308
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-01-21 20:20:16 +0000

    [WPF Branch] Studio: Fixed PriorityObservableCollection not updating when item's priority changes because of misspelled property name.

--- branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Collections/PriorityObservableCollection.cs
+++ branches/wpf/src_studio/core/GeoGen Studio Core/Utilities/Collections/PriorityObservableCollection.cs
@@ -1,46 +1,49 @@
 ﻿namespace GeoGen.Studio.Utilities.Collections
 {
-	using System.Collections.Generic;
-	using System.ComponentModel;
+    using System.Collections.Generic;
+    using System.ComponentModel;
 
-	/// <summary>
-	/// Represents a dynamic data collection that provides notifications when items get added, removed, or when the whole list is refreshed. The items are sorted by their <see cref="IPriority">priority</see>.
-	/// </summary>
-	public class PriorityObservableCollection: SortedObservableCollection<IPriority>
-	{
-		/// <summary>
-		/// Initializes a new empty instance of the <see cref="PriorityObservableCollection"/> class.
-		/// </summary>
-		public PriorityObservableCollection() : base(new PriorityComparer()) { }
+    /// <summary>
+    /// Represents a dynamic data collection that provides notifications when items get added, removed, or when the whole list is refreshed. The items are sorted by their <see cref="IPriority">priority</see>.
+    /// </summary>
+    public class PriorityObservableCollection: SortedObservableCollection<IPriority>
+    {
+        /// <summary>
+        /// Initializes a new empty instance of the <see cref="PriorityObservableCollection"/> class.
+        /// </summary>
+        public PriorityObservableCollection() : base(new PriorityComparer()) { }
 
-		protected override void ItemPropertyChanged(object o, PropertyChangedEventArgs args)
-		{
-			if (args.PropertyName != "IPriority")
-			{
-				return;
-			}
-			
-			this.Remove((IPriority)o);
-			this.Add((IPriority)o);
-		}
-	}
+        protected override void ItemPropertyChanged(object o, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName != "Priority")
+            {
+                return;
+            }
+            
+            this.Remove((IPriority)o);
+            this.Add((IPriority)o);
+        }
+    }
 
-	public class PriorityObservableCollection<T>: SortedObservableCollection<T> where T:IPriority
-	{
-		public PriorityObservableCollection()
-		{
-			this.comparer = (IComparer<T>) new PriorityComparer();
-		}
+    /// <summary>
+    /// Represents a dynamic data collection that provides notifications when items get added, removed, or when the whole list is refreshed. The items are sorted by their <see cref="IPriority">priority</see>.
+    /// </summary>
+    public class PriorityObservableCollection<T>: SortedObservableCollection<T> where T:IPriority
+    {
+        public PriorityObservableCollection()
+        {
+            this.Comparer = (IComparer<T>) new PriorityComparer();
+        }
 
-		protected override void ItemPropertyChanged(object o, PropertyChangedEventArgs args)
-		{
-			if (args.PropertyName != "Priority")
-			{
-				return;
-			}
+        protected override void ItemPropertyChanged(object o, PropertyChangedEventArgs args)
+        {
+            if (args.PropertyName != "Priority")
+            {
+                return;
+            }
 
-			this.Remove((T)o);
-			this.Add((T)o);
-		}
-	}
+            this.Remove((T)o);
+            this.Add((T)o);
+        }
+    }
 }
