587aec99a539dcffced7a9df694ca0c4d7d5a19a
Matěj Zábský <mzabsky@gmail.com>
2012-03-19 08:31:39 +0000

    [WPF Branch] Studio: Documented Context.

--- src_studio/core/GeoGen Studio Core/Utilities/Context/Context.cs
+++ src_studio/core/GeoGen Studio Core/Utilities/Context/Context.cs
@@ -25,9 +25,27 @@
             this.Label = label;
         }
 
+        /// <summary>
+        /// Determines whether the specified <see cref="System.Object"/> is equal to this instance.
+        /// </summary>
+        /// <param name="obj">The <see cref="System.Object"/> to compare with this instance.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified <see cref="System.Object"/> is equal to this instance; otherwise, <c>false</c>.
+        /// </returns>
         public override bool Equals(object obj)
         {
             return object.ReferenceEquals(this, obj);
+        }
+
+        /// <summary>
+        /// Returns a hash code for this instance.
+        /// </summary>
+        /// <returns>
+        /// A hash code for this instance, suitable for use in hashing algorithms and data structures like a hash table. 
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return this.Label != null ? this.Label.GetHashCode() : 0;
         }
     }
 }
