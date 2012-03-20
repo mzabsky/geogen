b646f6f5330f5ae0ceb1fe3f712568fa1e3565e1
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-03-20 13:41:22 +0000

    [WPF Branch] Studio: Moved Logger to correct directory.

--- branches/wpf/src_studio/plugins/Settings/Settings.xaml.cs
+++ branches/wpf/src_studio/plugins/Settings/Settings.xaml.cs
@@ -1,61 +1,29 @@
 ﻿namespace GeoGen.Studio.PlugIns
 {
-	using System.ComponentModel;
 	using System.Collections.Generic;
+	using System.ComponentModel;
 	using System.Linq;
 	using System.Windows.Input;
+
 	using GeoGen.Studio.PlugInLoader;
 	using GeoGen.Studio.PlugIns.Interfaces;
 	using GeoGen.Studio.PlugIns.MenuBars;
-	using GeoGen.Studio.Utilities;    
+	using GeoGen.Studio.Utilities;
 	using GeoGen.Studio.Utilities.Persistence;
 
 	public sealed partial class Settings : GeoGen.Studio.Utilities.PlugInBase.WindowBase, INotifyPropertyChanged
 	{
-		private readonly ICommand showCommand;
-		private readonly ICommand applyCommand;
-		private readonly ICommand closeCommand;
-		private readonly ICommand okCommand;
-		private readonly ICommand togglePlugInCommand;
+		public ICommand ShowCommand { get; private set; }
 
-		public ICommand ShowCommand {
-			get
-			{
-				return this.showCommand;
-			}
-		}
+		public ICommand ApplyCommand { get; private set; }
 
-		public ICommand ApplyCommand {
-			get
-			{
-				return this.applyCommand;
-			}
-		}
+		public ICommand CloseCommand { get; private set; }
 
-		public ICommand CloseCommand {
-			get
-			{
-				return this.closeCommand;
-			}
-		}
+		public ICommand OkCommand { get; private set; }
 
-		public ICommand OkCommand
-		{
-			get
-			{
-				return this.okCommand;
-			}
-		}
+		public ICommand TogglePlugInCommand { get; private set; }
 
-		public ICommand TogglePlugInCommand
-		{
-			get
-			{
-				return this.togglePlugInCommand;
-			}
-		}
-
-		public IEnumerable<PlugInInfo> PlugIns {get; private set;}
+		internal IEnumerable<PlugInInfo> PlugIns { get; private set; }
 
 		[Persistent(DefaultValue = false, EnableVisualConfiguration = false)]
 		public bool ShowHiddenPlugIns { get; set; }
@@ -65,44 +33,27 @@
 		/// </summary>
 		public Settings()
 		{
-			this.PropertyChanged += delegate(object o, PropertyChangedEventArgs args)
-			{
-				if (args.PropertyName == "ShowHiddenPlugIns")
-				{
-					bool a = this.ShowHiddenPlugIns;
-					int i = 0;
-				}
-			};
-			
-			ICommand command = this.ShowCommand;
+			this.ShowCommand = new RelayCommand(
+				param => this.ShowWindow());
 
-			this.showCommand = new RelayCommand(
-				param => this.ShowWindow()
-			);
+			this.ApplyCommand = new RelayCommand(
+				param => this.ApplyChanges());
 
-			this.applyCommand = new RelayCommand(
-				param => this.ApplyChanges()
-			);
+			this.CloseCommand = new RelayCommand(
+				param => this.Hide());
 
-			this.closeCommand = new RelayCommand(
-				param => this.Hide()
-			);
-
-			this.okCommand = new RelayCommand(
+			this.OkCommand = new RelayCommand(
 				param =>
 				{
 					this.ApplyChanges(); 
 					this.Hide();
-				}
-			);
+				});
 
-			this.togglePlugInCommand = new RelayCommand(
+			this.TogglePlugInCommand = new RelayCommand(
 				param =>
 				{
-					(param as PlugInInfo).IsEnabled = !(param as PlugInInfo).IsEnabled;
-					int i = 0;
-				}
-			);
+					((PlugInInfo)param).IsEnabled = !((PlugInInfo)param).IsEnabled;
+				});
 
 			this.Closing += delegate(object sender, CancelEventArgs args)
 			{
@@ -127,28 +78,23 @@
 				new MenuEntry(
 					header: "Edit",
 					priority: -1,
-					items: new MenuEntryObservableCollection {
+					items: new MenuEntryObservableCollection
+					{
 						new MenuSeparator(
-							priority: -31
-						),
+							priority: -31),
 						new MenuEntry(
 							header: "Settings",                            
 							priority: -32,
-							command: this.showCommand,
-							icon: "pack://application:,,,/GGenStudio.PlugIn.Settings;component/Images/Icons/settings.png"
-						)
-					}
-				)
-			);
+							command: this.ShowCommand,
+							icon: "pack://application:,,,/GGenStudio.PlugIn.Settings;component/Images/Icons/settings.png")
+					}));
 		}
 
 		private void ShowWindow()
 		{            
-			List<PlugInInfo> list = new List<PlugInInfo>();
+			var list = new List<PlugInInfo>();
 
-			bool a = this.ShowHiddenPlugIns;
-
-			foreach(PlugIn plugIn in Loader.PlugIns)
+			foreach (PlugIn plugIn in Loader.PlugIns)
 			{               
 				MainConfig.SaveConfiguration(plugIn.Instances.Last());                
 
@@ -162,29 +108,22 @@
 
 		private void ApplyChanges()
 		{
-			foreach(PlugInInfo plugInInfo in this.PlugIns)
+			foreach (PlugInInfo plugInInfo in this.PlugIns)
 			{
 				// Update enabled status
 				plugInInfo.PlugIn.IsEnabled = plugInInfo.IsEnabled;
 
 				// Do not apply configuration changes for non-configurable plug-ins              
-				foreach(ConfigurablePropertyInfo property in plugInInfo.Properties)
+				foreach (ConfigurablePropertyInfo property in plugInInfo.Properties)
 				{
-					object o = property.Value;
-
 					MainConfig.SavePropertyValue(property.Property, property.Value);
 
-					foreach(object instance in plugInInfo.PlugIn.Instances)
+					foreach (object instance in plugInInfo.PlugIn.Instances)
 					{
 						property.Property.SetValue(instance, property.Value, null);
 					}                    
 				}
 			}
-		}
-
-		private void TogglePlugInEnabled(PlugInInfo plugInInfo)
-		{
-			plugInInfo.IsEnabled = !plugInInfo.IsEnabled;
 		}
 	}
 }
