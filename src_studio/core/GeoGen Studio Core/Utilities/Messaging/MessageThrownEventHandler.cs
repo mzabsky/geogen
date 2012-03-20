331d9d8d22017999337a305fdbd7ad5cb26d8134
Matěj Zábský <mzabsky@gmail.com>
2012-03-20 13:25:50 +0000

    [WPF Branch] Studio: Moved Core.Tests to correct directory.

--- src_studio/core/GeoGen Studio Core/Utilities/Messaging/MessageThrownEventHandler.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace GeoGen.Studio.Utilities.Messaging
-{
-    /// <summary>
-    /// Represents a method that will handle <see cref="Messenger.MessageSent"/> event.
-    /// </summary>
-    public delegate void MessageThrownEventHandler(object sender, MessageThrownEventArgs args);
-}
