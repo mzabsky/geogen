02702a7edb90b5dd4f61599bebf771e20a090b44
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-03-20 18:35:54 +0000

    [WPF Branch] Studio: Cleaned up the FileInfoExtensions.

--- branches/wpf/src_studio/Core/Utilities/Extensions/FileInfo.cs
+++ branches/wpf/src_studio/Core/Utilities/Extensions/FileInfo.cs
@@ -1,66 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using GeoGen.Studio.Utilities.Messaging;
-
-namespace GeoGen.Studio.Utilities.Extensions
+﻿namespace GeoGen.Studio.Utilities.Extensions
 {
+    using System;
+    using System.IO;
+    using System.Runtime.InteropServices;
+
+    using GeoGen.Studio.Utilities.Messaging;
+
+    /// <summary>
+    /// Extension methods for <see cref="FileInfo"/> class.
+    /// </summary>
     public static class FileInfoExtensions
     {
-        [DllImport(@"urlmon.dll", CharSet = CharSet.Auto)]
-        private extern static System.UInt32 FindMimeFromData(
-             System.UInt32 pBC,
-             [MarshalAs(UnmanagedType.LPStr)] System.String pwzUrl,
-             [MarshalAs(UnmanagedType.LPArray)] byte[] pBuffer,
-             System.UInt32 cbSize,
-             [MarshalAs(UnmanagedType.LPStr)] System.String pwzMimeProposed,
-             System.UInt32 dwMimeFlags,
-             out System.UInt32 ppwzMimeOut,
-             System.UInt32 dwReserved);
-
+        /// <summary>
+        /// Gets mime type of a file.
+        /// </summary>
+        /// <param name="fileInfo">The file info.</param>
+        /// <returns>The mime type.</returns>
         public static string GetMimeType(this FileInfo fileInfo)
         {
-            byte[] buffer = new byte[256];
+            var buffer = new byte[256];
 
             // Grab the first 256 bytes on the file.
-            using (FileStream fileStream = new FileStream(fileInfo.FullName, FileMode.Open))
+            using (var fileStream = new FileStream(fileInfo.FullName, FileMode.Open))
             {
                 if (fileStream.Length >= 256)
+                {
                     fileStream.Read(buffer, 0, 256);
+                }
                 else
+                {
                     fileStream.Read(buffer, 0, (int)fileStream.Length);
+                }
             }
 
             try
             {
-                System.UInt32 mimeType;
-                System.UInt32 returnValue = FindMimeFromData(0, null, buffer, 256, null, 0, out mimeType, 0);
-                System.IntPtr mimeTypePointer = new IntPtr(mimeType);
+                uint mimeType;
+                FindMimeFromData(0, null, buffer, 256, null, 0, out mimeType, 0);
+                var mimeTypePointer = new IntPtr(mimeType);
                 return Marshal.PtrToStringUni(mimeTypePointer);
             }
-
-            catch (Exception ex)
-            {
-               
+            catch (Exception)
+            {               
                 new Message(
                     string.Format("Could not identify file \"{0}\".", fileInfo.Name), 
-                    MessageType.Error
-                    ).Send();
-                return "";
+                    MessageType.Error).Send();
+
+                return string.Empty;
             }
         }
 
+        /// <summary>
+        /// Determines whether the file is a text file.
+        /// </summary>
+        /// <param name="fileInfo">The file info.</param>
+        /// <returns>
+        ///   <c>true</c> if the file is a text file; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsTextFile(this FileInfo fileInfo)
         {
-            return fileInfo.GetMimeType().IndexOf("text/") == 0;
+            return fileInfo.GetMimeType().IndexOf("text/", System.StringComparison.Ordinal) == 0;
         }
 
+        /// <summary>
+        /// Determines whether the specified file is image.
+        /// </summary>
+        /// <param name="fileInfo">The file info.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified file info is image; otherwise, <c>false</c>.
+        /// </returns>
         public static bool IsImage(this FileInfo fileInfo)
         {
-            return fileInfo.GetMimeType().IndexOf("image/") == 0;
+            return fileInfo.GetMimeType().IndexOf("image/", System.StringComparison.Ordinal) == 0;
         }
+
+        [DllImport(@"urlmon.dll", CharSet = CharSet.Auto)]
+        private extern static uint FindMimeFromData(
+             uint pBC,
+             [MarshalAs(UnmanagedType.LPStr)] string pwzUrl,
+             [MarshalAs(UnmanagedType.LPArray)] byte[] pBuffer,
+             uint cbSize,
+             [MarshalAs(UnmanagedType.LPStr)] string pwzMimeProposed,
+             uint dwMimeFlags,
+             out uint ppwzMimeOut,
+             uint dwReserved);
     }
 }
