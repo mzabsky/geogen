f75090ab2693307e5b5120606b9c6049495e8a53
Matěj Zábský <mzabsky@gmail.com>
2012-03-21 10:52:33 +0000

    [WPF Branch] Studio: Removed dead code from Settings.

---
+++ src_studio/plugins/Settings/SettingsViewModel.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="SettingsViewModel.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace GeoGen.Studio.PlugIns
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// View model for the Settings dialog.
+    /// </summary>
+    public class SettingsViewModel
+    {
+
+    }
+}
