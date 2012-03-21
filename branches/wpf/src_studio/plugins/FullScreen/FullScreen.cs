5ac75ef98312e8cf783fbec0ce46a3b549d6f2c8
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-03-21 11:55:57 +0000

    [WPF Branch] Studio: Cleaned up FullScreen.

--- branches/wpf/src_studio/plugins/FullScreen/FullScreen.cs
+++ branches/wpf/src_studio/plugins/FullScreen/FullScreen.cs
@@ -1,209 +1,282 @@
 ﻿namespace GeoGen.Studio.PlugIns
 {
-	using System;
-	using System.Collections.Generic;
-	using System.ComponentModel;
-	using System.Windows;
-	using System.Windows.Controls;
-	using System.Windows.Data;
-	using System.Windows.Input;
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Input;
 
-	using GeoGen.Studio.PlugInLoader;
-	using GeoGen.Studio.PlugIns.Interfaces;
-	using GeoGen.Studio.PlugIns.MenuBars;
-	using GeoGen.Studio.PlugIns.ToolBars;
-	using GeoGen.Studio.Utilities;
-	using GeoGen.Studio.Utilities.PlugInBase;
+    using GeoGen.Studio.PlugInLoader;
+    using GeoGen.Studio.PlugIns.Interfaces;
+    using GeoGen.Studio.PlugIns.MenuBars;
+    using GeoGen.Studio.PlugIns.ToolBars;
+    using GeoGen.Studio.Utilities;
+    using GeoGen.Studio.Utilities.PlugInBase;
 
-	public sealed class FullScreen : ObjectBase, INotifyPropertyChanged
-	{
-		public const string IconPathPrefix = "pack://application:,,,/GeoGen.Studio.PlugIns.FullScreen;component/Images/Icons/";
-		
-		private readonly List<Control> hideableBars = new List<Control>();
+    /// <summary>
+    /// Adds ability to switch the <see cref="IMainWindow">main window</see> to full screen.
+    /// </summary>
+    public sealed class FullScreen : ObjectBase, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Path where icons for this plug-in are located.
+        /// </summary>
+        public const string IconPathPrefix = "pack://application:,,,/GeoGen.Studio.PlugIns.FullScreen;component/Images/Icons/";
 
-		private readonly ICommand toggleFullScreenCommand;
+        /// <summary>
+        /// Collection of bars which can be hidden.
+        /// </summary>
+        private readonly List<Control> hideableBars = new List<Control>();
 
-		private Window mainWindow;
-		private WindowState windowStateBackup;		
+        /// <summary>
+        /// The main window
+        /// </summary>
+        private Window mainWindow;
 
-		public FullScreen()
-		{
-			this.toggleFullScreenCommand = new RelayCommand(p => this.ToggleFullScreen());			
-			this.AreBarsShown = true;
-		}
+        /// <summary>
+        /// Stores state of the window before it was switched to full screen.
+        /// </summary>
+        private WindowState windowStateBackup;
 
-		public bool IsFullScreen
-		{
-			get
-			{
-				return this.mainWindow.WindowStyle == WindowStyle.None;
-			}
-		}
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FullScreen"/> class.
+        /// </summary>
+        public FullScreen()
+        {
+            this.ToggleFullScreenCommand = new RelayCommand(p => this.ToggleFullScreen());            
+            this.AreBarsShown = true;
+        }
 
-		public bool AreBarsShown { get; set; }
+        /// <summary>
+        /// Gets or sets the toggle full screen command.
+        /// </summary>
+        /// <value>
+        /// The toggle full screen command.
+        /// </value>
+        public ICommand ToggleFullScreenCommand { get; set; }
 
-		public void Register(IMainWindow mainWindow)
-		{
-			this.mainWindow = mainWindow.Window;
+        /// <summary>
+        /// Gets a value indicating whether the window is in full screen mode.
+        /// </summary>
+        /// <value>
+        ///     <c>true</c> if this instance is full screen; otherwise, <c>false</c>.
+        /// </value>
+        public bool IsFullScreen
+        {
+            get
+            {
+                return this.mainWindow.WindowStyle == WindowStyle.None;
+            }
+        }
 
-			this.mainWindow.Deactivated += delegate
-			{
-				if (this.mainWindow.WindowStyle == WindowStyle.None)
-				{
-					this.ToggleFullScreen();
-				}				
-			};			
-		}
+        /// <summary>
+        /// Gets or sets a value indicating whether the main window bars are currently displayed.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if [are bars shown]; otherwise, <c>false</c>.
+        /// </value>
+        public bool AreBarsShown { get; set; }
 
-		[OptionalRegistrator]
-		public void Register(IMainWindowToolBar toolBar)
-		{
-			toolBar.AddItem(
-				new ToolBarCheckableButton(
-					icon: FullScreen.IconPathPrefix  + "fullscreen.png", 
-					priority: -10, 
-					command: this.toggleFullScreenCommand, 
-					toolTip: "Toggle full screen mode",					
-					isCheckedBinding: new Binding("IsFullScreen"),
-					dataContext: this));
+        /// <summary>
+        /// Enables the main window to be hidden.
+        /// </summary>
+        /// <param name="mainWindow">The main window.</param>
+        public void Register(IMainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow.Window;
 
-			this.hideableBars.Add(toolBar.Control);
-		}
+            this.mainWindow.Deactivated += delegate
+            {
+                if (this.mainWindow.WindowStyle == WindowStyle.None)
+                {
+                    this.ToggleFullScreen();
+                }                
+            };            
+        }
 
-		[OptionalRegistrator]
-		public void Register(IMenuBar menuBar)
-		{
-			menuBar.AddMenu(
-				new MenuEntry(
-					header: "View",
-					items: new MenuEntryObservableCollection
-					{
-						new MenuEntry(
-							header: "Full Screen",
-							priority: -10,
-							command: this.toggleFullScreenCommand,
-							inputGestureText: "F11",
-							icon: FullScreen.IconPathPrefix  + "fullscreen.png",
-							isCheckedBinding: new Binding("IsFullScreen"),
-							dataContext: this,
-							isCheckable: true)
-					}));
+        /// <summary>
+        /// Adds the Full Screen button to the main window toolbar.
+        /// </summary>
+        /// <param name="toolBar">The tool bar.</param>
+        [OptionalRegistrator]
+        public void Register(IMainWindowToolBar toolBar)
+        {
+            toolBar.AddItem(
+                new ToolBarCheckableButton(
+                    icon: FullScreen.IconPathPrefix  + "fullscreen.png", 
+                    priority: -10, 
+                    command: this.ToggleFullScreenCommand, 
+                    toolTip: "Toggle full screen mode",                    
+                    isCheckedBinding: new Binding("IsFullScreen"),
+                    dataContext: this));
 
-			this.hideableBars.Add(menuBar.Control);
-		}
+            this.hideableBars.Add(toolBar.Control);
+        }
 
-		[OptionalRegistrator]
-		public void Register(IStatusBar statusBar)
-		{
-			this.hideableBars.Add(statusBar.Control);
-		}
+        /// <summary>
+        /// Registers the main window menu bar.
+        /// </summary>
+        /// <param name="menuBar">The menu bar.</param>
+        [OptionalRegistrator]
+        public void Register(IMenuBar menuBar)
+        {
+            // TODO: IMainWindowMenuBar!
+            menuBar.AddMenu(
+                new MenuEntry(
+                    header: "View",
+                    items: new MenuEntryObservableCollection
+                    {
+                        new MenuEntry(
+                            header: "Full Screen",
+                            priority: -10,
+                            command: this.ToggleFullScreenCommand,
+                            inputGestureText: "F11",
+                            icon: FullScreen.IconPathPrefix  + "fullscreen.png",
+                            isCheckedBinding: new Binding("IsFullScreen"),
+                            dataContext: this,
+                            isCheckable: true)
+                    }));
 
-		private void ToggleFullScreen()
-		{
-			if (this.mainWindow.WindowStyle != WindowStyle.None)
-			{
-				this.windowStateBackup = this.mainWindow.WindowState;
-				this.mainWindow.WindowState = WindowState.Normal;
-				
-				this.mainWindow.WindowStyle = WindowStyle.None;
-				this.mainWindow.WindowState = WindowState.Maximized;
-				this.mainWindow.Topmost = true;
-				this.mainWindow.Top = 0;
-				this.mainWindow.Left = 0;
+            this.hideableBars.Add(menuBar.Control);
+        }
 
-				this.HideBars();
+        /// <summary>
+        /// Registers the status bar to be hidden when switching to full screen.
+        /// </summary>
+        /// <param name="statusBar">The status bar.</param>
+        [OptionalRegistrator]
+        public void Register(IStatusBar statusBar)
+        {
+            this.hideableBars.Add(statusBar.Control);
+        }
 
-				this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsClosed;
-			}
-			else
-			{
-				this.mainWindow.ResizeMode = ResizeMode.CanResize;
-				this.mainWindow.WindowState = this.windowStateBackup;
-				this.mainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
-				this.mainWindow.Topmost = false;
+        /// <summary>
+        /// Toggles the main window to/from full screen mode.
+        /// </summary>
+        private void ToggleFullScreen()
+        {
+            if (this.mainWindow.WindowStyle != WindowStyle.None)
+            {
+                this.windowStateBackup = this.mainWindow.WindowState;
+                this.mainWindow.WindowState = WindowState.Normal;
+                
+                this.mainWindow.WindowStyle = WindowStyle.None;
+                this.mainWindow.WindowState = WindowState.Maximized;
+                this.mainWindow.Topmost = true;
+                this.mainWindow.Top = 0;
+                this.mainWindow.Left = 0;
 
-				this.ShowBars();
+                this.HideBars();
 
-				this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsOpen;
-				this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsClosed;
-			}
+                this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsClosed;
+            }
+            else
+            {
+                this.mainWindow.ResizeMode = ResizeMode.CanResize;
+                this.mainWindow.WindowState = this.windowStateBackup;
+                this.mainWindow.WindowStyle = WindowStyle.SingleBorderWindow;
+                this.mainWindow.Topmost = false;
 
-			Application.Current.Dispatcher.BeginInvoke((Action)delegate
-			{
-				this.OnPropertyChanged("IsFullScreen");
-			});
-		}
+                this.ShowBars();
 
-		private void ShowBars()
-		{
-			if (this.AreBarsShown)
-			{
-				// Prevent the events from being hooked more than once
-				return;
-			}
+                this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsOpen;
+                this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsClosed;
+            }
 
-			foreach (Control bar in this.hideableBars)
-			{
-				bar.Visibility = Visibility.Visible;
-			}
+            Application.Current.Dispatcher.BeginInvoke((Action)delegate
+            {
+                this.OnPropertyChanged("IsFullScreen");
+            });
+        }
 
-			this.AreBarsShown = true;
+        /// <summary>
+        /// Unhides main window bars.
+        /// </summary>
+        private void ShowBars()
+        {
+            if (this.AreBarsShown)
+            {
+                // Prevent the events from being hooked more than once
+                return;
+            }
 
-			this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsClosed;
-			this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsOpen;
-		}
+            foreach (Control bar in this.hideableBars)
+            {
+                bar.Visibility = Visibility.Visible;
+            }
 
-		private void HideBars()
-		{
-			if (!this.AreBarsShown)
-			{
-				// Prevent the events from being hooked more than once
-				return;
-			}
+            this.AreBarsShown = true;
 
-			foreach (Control bar in this.hideableBars)
-			{				
-				bar.Visibility = Visibility.Collapsed;				
-			}
+            this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsClosed;
+            this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsOpen;
+        }
 
-			this.AreBarsShown = false;
+        /// <summary>
+        /// Hides the main window bars, which are registered to be hidden when switching to full screen mode.
+        /// </summary>
+        private void HideBars()
+        {
+            if (!this.AreBarsShown)
+            {
+                // Prevent the events from being hooked more than once
+                return;
+            }
 
-			this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsOpen;
-			this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsClosed;
-		}
+            foreach (Control bar in this.hideableBars)
+            {                
+                bar.Visibility = Visibility.Collapsed;                
+            }
 
-		private void HandleMouseMoveWithBarsClosed(object sender, MouseEventArgs args)
-		{
-			// Display the bars if the mouse is on top border of the screen
-			if (args.MouseDevice.GetPosition(this.mainWindow).Y <= 1)
-			{
-				this.ShowBars();
-			}
-		}
+            this.AreBarsShown = false;
 
-		private void HandleMouseMoveWithBarsOpen(object sender, MouseEventArgs args)
-		{
-			// Check if the mouse is still above one of the bars
-			bool barWasHit = false;
-			foreach (Control bar in this.hideableBars)
-			{
-				// The bars are supposed to be rectangular, don't perform full hit test
-				Point mousePoint = args.GetPosition(this.mainWindow);
+            this.mainWindow.MouseMove -= this.HandleMouseMoveWithBarsOpen;
+            this.mainWindow.MouseMove += this.HandleMouseMoveWithBarsClosed;
+        }
 
-				Point controlPoint = bar.TransformToAncestor(this.mainWindow).Transform(new Point(0, 0));
+        /// <summary>
+        /// Shows the bars if the mouse is hovering on top of the screen.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="args">The <see cref="System.Windows.Input.MouseEventArgs"/> instance containing the event data.</param>
+        private void HandleMouseMoveWithBarsClosed(object sender, MouseEventArgs args)
+        {
+            // Display the bars if the mouse is on top border of the screen
+            if (args.MouseDevice.GetPosition(this.mainWindow).Y <= 1)
+            {
+                this.ShowBars();
+            }
+        }
 
-				barWasHit |= 
-					mousePoint.X >= controlPoint.X &&
-					mousePoint.Y >= controlPoint.Y &&
-					mousePoint.X <= controlPoint.X + bar.ActualWidth &&
-					mousePoint.Y <= controlPoint.Y + bar.ActualHeight;
-			}
+        /// <summary>
+        /// Hides the bars which were shown by <see cref="HandleMouseMoveWithBarsClosed"/>.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="args">The <see cref="System.Windows.Input.MouseEventArgs"/> instance containing the event data.</param>
+        private void HandleMouseMoveWithBarsOpen(object sender, MouseEventArgs args)
+        {
+            // Check if the mouse is still above one of the bars
+            bool barWasHit = false;
+            foreach (Control bar in this.hideableBars)
+            {
+                // The bars are supposed to be rectangular, don't perform full hit test
+                Point mousePoint = args.GetPosition(this.mainWindow);
 
-			// If the mouse is away from any bar, hide the bars
-			if (!barWasHit)
-			{
-				this.HideBars();				
-			}		
-		}
-	}
+                Point controlPoint = bar.TransformToAncestor(this.mainWindow).Transform(new Point(0, 0));
+
+                barWasHit |= 
+                    mousePoint.X >= controlPoint.X &&
+                    mousePoint.Y >= controlPoint.Y &&
+                    mousePoint.X <= controlPoint.X + bar.ActualWidth &&
+                    mousePoint.Y <= controlPoint.Y + bar.ActualHeight;
+            }
+
+            // If the mouse is away from any bar, hide the bars
+            if (!barWasHit)
+            {
+                this.HideBars();                
+            }        
+        }
+    }
 }
