dab16178682101025a76aaeab3d67925d76c65be
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-03-21 10:23:14 +0000

    [WPF Branch] Studio: Cleaned up Settings code, renamed several classes to more expressive names.

---
+++ branches/wpf/src_studio/plugins/Settings/SettingsPropertyItemTemplateSelector.cs
@@ -0,0 +1,97 @@
+﻿namespace GeoGen.Studio.PlugIns
+{
+    using System.Windows;
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Template selector for the Settings dialog.
+    /// </summary>
+    internal class SettingsPropertyItemTemplateSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// Gets or sets data template for the integer type property.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        public DataTemplate Int { get; set; }
+
+        /// <summary>
+        /// Gets or sets data template for the float type property.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        public DataTemplate Float { get; set; }
+
+        /// <summary>
+        /// Gets or sets data template for the boolean type property.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        public DataTemplate Bool { get; set; }
+
+        /// <summary>
+        /// Gets or sets data template for the string type property.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        public DataTemplate String { get; set; }
+
+        /// <summary>
+        /// Gets or sets data template used when the type cannot be decided.
+        /// </summary>
+        /// <value>
+        /// The template.
+        /// </value>
+        public DataTemplate UnknownType { get; set; }
+
+        /// <summary>
+        /// Returns a DataTemplate based on the type of the property.
+        /// </summary>
+        /// <param name="item">The data object for which to select the template.</param>
+        /// <param name="container">The data-bound object.</param>
+        /// <returns>
+        /// Returns a <see cref="T:System.Windows.DataTemplate"/> or null. The default value is null.
+        /// </returns>
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            var viewModel = (SettingsPropertyViewModel)item;
+
+            if (
+                viewModel.Value is int ||
+                viewModel.Value is uint ||
+                viewModel.Value is long ||
+                viewModel.Value is ulong ||
+                viewModel.Value is short ||
+                viewModel.Value is ushort ||
+                viewModel.Value is byte ||
+                viewModel.Value is float ||
+                viewModel.Value is double)
+            {
+                return this.Int;
+            }
+            else if (
+                viewModel.Value is float || 
+                viewModel.Value is double
+            )
+            {
+                return this.Float;
+            }
+            else if (viewModel.Value is bool)
+            {
+                return this.Bool;
+            }
+            else if (viewModel.Value is string)
+            {
+                return this.String;
+            }
+            else
+            {
+                return this.UnknownType;
+            }
+        }
+    }
+}
---
+++ branches/wpf/src_studio/plugins/Settings/SettingsPropertyViewModel.cs
@@ -0,0 +1,70 @@
+﻿namespace GeoGen.Studio.PlugIns
+{
+    using System;
+    using System.ComponentModel;
+    using System.Reflection;
+
+    using GeoGen.Studio.Utilities;
+
+    /// <summary>
+    /// Represents one plug-in persistent property within the Settings dialog.
+    /// </summary>
+    public class SettingsPropertyViewModel : ObservableObject, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// Gets or sets the name of the property.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets actual reflection representation of the property.
+        /// </summary>
+        /// <value>
+        /// The property.
+        /// </value>
+        public PropertyInfo Property { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of the plug-in.
+        /// </summary>
+        public Type Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the original value of the property (before it was modified within the Settings dialog).
+        /// </summary>
+        /// <value>
+        /// The original value.
+        /// </value>
+        public object OriginalValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the value of the property.
+        /// </summary>
+        /// <value>
+        /// The value.
+        /// </value>
+        public object Value { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description of the property.
+        /// </summary>
+        /// <value>
+        /// The description.
+        /// </value>
+        public object Description { get; set; }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String"/> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }    
+}
