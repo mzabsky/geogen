6dddcdb566cfcdef41403907848d46d50143fca4
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-03-22 22:20:43 +0000

    [WPF Branch] Studio: Improved coding style in DialogBody.

--- branches/wpf/src_studio/Core/UI/DialogBody.cs
+++ branches/wpf/src_studio/Core/UI/DialogBody.cs
@@ -1,20 +1,36 @@
-﻿using System.Collections;
-using System.Windows;
-using System.Collections.ObjectModel;
-using System.Windows.Controls;
-
-namespace GeoGen.Studio.UI
+﻿namespace GeoGen.Studio.UI
 {
+    using System.Collections;
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Provides united layout for any dialog-type window.
     /// </summary>
-    public class DialogBody: ContentControl
+    public class DialogBody : ContentControl
     {
-        public static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
+        /// <summary>
+        /// Dependecy property for <see cref="Buttons"/>.
+        /// </summary>
+        private static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
             "Buttons", typeof(IList), typeof(DialogBody), new PropertyMetadata(null));
 
         /// <summary>
-        /// Collection of buttons displayed in the bottom bar.
+        /// Dependecy property for <see cref="BottomBarContent"/>.
+        /// </summary>
+        private static readonly DependencyProperty BottomBarContentProperty = DependencyProperty.Register(
+            "BottomBarContent", typeof(object), typeof(DialogBody), new PropertyMetadata(null));
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DialogBody"/> class.
+        /// </summary>
+        public DialogBody()
+        {
+            this.Buttons = new ArrayList();
+        }
+
+        /// <summary>
+        /// Gets or sets collection of buttons displayed in the bottom bar.
         /// </summary>
         /// <value>The buttons.</value>
         public IList Buttons
@@ -23,37 +39,28 @@ namespace GeoGen.Studio.UI
             {
                 return (IList)GetValue(ButtonsProperty);
             }
+
             set
             {
                 SetValue(ButtonsProperty, value);
             }
-        }
-
-        public static readonly DependencyProperty BottomBarContentProperty = DependencyProperty.Register(
-            "BottomBarContent", typeof(object), typeof(DialogBody), new PropertyMetadata(null));
+        }        
 
         /// <summary>
-        /// Content to be displayed in the bottom part of the bottom bar.
+        /// Gets or sets content to be displayed in the bottom part of the bottom bar.
         /// </summary>
         /// <value>The content.</value>
         public object BottomBarContent
         {
             get
             {
-                return (object)GetValue(BottomBarContentProperty);
+                return GetValue(BottomBarContentProperty);
             }
+
             set
             {
                 SetValue(BottomBarContentProperty, value);
             }
-        }
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="DialogBody"/> class.
-        /// </summary>
-        public DialogBody()
-        {
-            this.Buttons = new ArrayList();
         }
     }
 }
