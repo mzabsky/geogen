5f77f1f954f58b87883abed804e93d456ec1779e
Matěj Zábský <mzabsky@gmail.com>
2012-03-25 21:07:27 +0000

    [WPF Branch] Studio: Added new message severity - Trace.

--- src_studio/Core/Utilities/Messaging/MessageType.cs
+++ src_studio/Core/Utilities/Messaging/MessageType.cs
@@ -9,13 +9,20 @@
         /// Indicates a state that has negative impact.
         /// </summary>
         Error, 
+
         /// <summary>
         /// Indicates a state that could potentially have a negative impact.
         /// </summary>
         Warning,
+
         /// <summary>
         /// Information without any negative impact.
         /// </summary>
-        Message
+        Message,
+
+        /// <summary>
+        /// Information not significant enough for normal user.
+        /// </summary>
+        Trace
     }
 }
