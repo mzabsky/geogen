715a4aab97007444462614c866bb3542f9b410be
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2012-04-23 16:16:49 +0000

    [WPF Branch] Studio: Added FullPathToFileNameConverter.

---
+++ branches/wpf/src_studio/Core/Utilities/Converters/FullPathToFileNameConverter.cs
@@ -0,0 +1,61 @@
+﻿namespace GeoGen.Studio.Utilities.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converts full file path to a file name.
+    /// </summary>
+    [ValueConversion(typeof(bool), typeof(String))]
+    public sealed class FullPathToFileNameConverter : IValueConverter
+    {
+        /// <summary>
+        /// Backing field for <see cref="Instance"/>.
+        /// </summary>
+        private static readonly IValueConverter instance = new FullPathToFileNameConverter();
+
+        /// <summary>
+        /// Gets instance of this converter.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static IValueConverter Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        /// <inheritdoc />
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                try
+                {
+                    var stringValue = (string)value;
+
+                    var lastSlashPosition = stringValue.LastIndexOfAny("\\/".ToCharArray());
+                    if (lastSlashPosition == -1)
+                    {
+                        return stringValue;
+                    }
+
+                    return stringValue.Substring(lastSlashPosition + 1);
+                }
+                catch (InvalidCastException)
+                {
+                }
+            }
+
+            return "<Unknown value>";
+        }
+
+        /// <inheritdoc />
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
