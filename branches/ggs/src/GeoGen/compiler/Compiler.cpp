11d9a3eafb644e46c8386e626e1250a956f2eb4f
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2013-11-17 11:44:22 +0000

    [GGS Branch] The parser is finally compilable.

--- branches/ggs/src/GeoGen/compiler/Compiler.cpp
+++ branches/ggs/src/GeoGen/compiler/Compiler.cpp
@@ -1,22 +1,39 @@
-#include "..\Grammar\output\GeoGenScriptLexer.hpp"
-#include "..\Grammar\output\GeoGenScriptParser.hpp"
-
 #include "Compiler.hpp"
+
+#include "../Grammar/output/GeoGenScriptLexer.h"
+#include "../Grammar/output/GeoGenScriptParser.h"
+
+#include "../antlr3/antlr3.h"
 
 using namespace std;
 using namespace geogen;
 using namespace geogen::compiler;
-using namespace geogen_generated;
 
 CompiledScript Compiler::CompileScript(std::string& code)
 {
-	GeoGenScriptLexer::InputStreamType input((const unsigned char*)code.c_str(), ANTLR_ENC_8BIT, code.length(), NULL);
-	GeoGenScriptLexer lxr(&input);
+	pANTLR3_INPUT_STREAM input;
+	pGeoGenScriptLexer lex;
+	pANTLR3_COMMON_TOKEN_STREAM tokens;
+	pGeoGenScriptParser parser;
 
-	GeoGenScriptParser::TokenStreamType tstream(ANTLR_SIZE_HINT, lxr.get_tokSource() );
-	GeoGenScriptParser psr(&tstream);
+	input = antlr3StringStreamNew((unsigned char*)code.c_str(), ANTLR3_ENC_8BIT, code.length(), NULL);
+	lex = GeoGenScriptLexerNew(input);
+	tokens = antlr3CommonTokenStreamSourceNew(ANTLR3_SIZE_HINT, TOKENSOURCE(lex));
+	parser = GeoGenScriptParserNew(tokens);
 
-	psr.script();
+	GeoGenScriptParser_script_return r = parser->script(parser);
+
+	pANTLR3_BASE_TREE tree = r.tree;
+
+	/*ExprTreeEvaluator eval;
+	int rr = eval.run(tree);
+	cout << "Evaluator result: " << rr << '\n';*/
+
+	parser->free(parser);
+	tokens->free(tokens);
+	lex->free(lex);
+	input->close(input);
+
 
 	return CompiledScript();
 }
