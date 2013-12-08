a9cea3c695f88cfd964b0270d89e9eecde2bc9f1
Matěj Zábský <mzabsky@gmail.com>
2013-12-08 17:30:02 +0000

    [GGS Branch] Added dtor to compiled script.

---
+++ src/GeoGen/compiler/CompiledScript.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#include "Compiler.hpp"
+
+#include "../antlr3/antlr3.h"
+
+#include "../Grammar/output/GeoGenScriptLexer.h"
+#include "../Grammar/output/GeoGenScriptParser.h"
+#include "../Grammar/output/GeoGenScriptDecls.h"
+
+#include "instructions/IfInstruction.hpp"
+
+using namespace std;
+using namespace geogen;
+using namespace geogen::compiler;
+//using namespace geogen_generated;
+
+Compiler::Compiler(){}
+
+CompiledScript::CompiledScript()
+{
+	this->metadata = NULL;
+}
+
+CompiledScript::~CompiledScript()
+{
+	if(this->metadata != NULL)
+	{
+		delete metadata;
+	}
+}
