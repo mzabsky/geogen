a1029646e7780e642161a2c060af99b72e7856a3
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-01-06 15:05:45 +0000

    [GGS Branch] Console now loads its input from file.

--- branches/ggs/src/Console/main.cpp
+++ branches/ggs/src/Console/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 #include "../GeoGen/GeoGen.hpp"
 #include "../GeoGen/compiler/Compiler.hpp"
@@ -16,61 +17,24 @@ KeyValSymbol: \
 	}\
 }\
 	*/
+	
+	std::stringstream  code;
+	std::ifstream file("testinput.txt");
+	std::string temp;
+	while (std::getline(file, temp)) 
+	{
+		code << temp << std::endl;
+	}
+
+
 	geogen::compiler::Compiler compiler;
 	//HeightMap.Empty()        
-	geogen::compiler::CompiledScript const* script = compiler.CompileScript(std::string("\
-\
-function aaa(x, y){\
-	1+4;\
-	return;\
-		return 5;\
-\
-\
-}  \
-1 / 5;\
-var a = 1 + 2;\
-var b = true;\
-var c = \"aaaa\";\
-\
-yield HeightMap.Empty() as \"aaa\";\
-yield HeightMap.Empty();\
-\
-if(2 == 3){\
-yield HeightMap.Empty();\
-}\
-\
-\
-if (2 == 3){\
-\
-yield HeightMap.Empty(); \
-} else {\
-yield HeightMap.Empty() as \"bbb\"; \
-}\
-\
-while(a == true){\
-	yield HeightMap.Empty() as \"ccc\"; \
-	break;\
-}\
-\
-for(var i = 0;i < 5;i+=1){\
-	yield HeightMap.Empty() as \"ddd\"; \
-}\
-\
-afield.bfield.cfield;\
-funcname(parama, paramb, paramc);\
-afield.bfield.cfield.funcname(parama, paramb, paramc);\
-afield.bfield.cfield[parama, paramb, paramc];\
-a ? b : c;\
-lvalue1 = 1 + 2;\
-lvalue2.fielda.fieldb = 1 + 2;\
-lvalue2[parama] = 1 + 2;\
-"));
+	geogen::compiler::CompiledScript const* script = compiler.CompileScript(code.str());
 
 	std::cout << script->GetRootCodeBlock().ToString();
 
-/*
 
-*/
+
 
 	geogen::Number n(2);
 	geogen::Number n2 = n / geogen::Number(3);
