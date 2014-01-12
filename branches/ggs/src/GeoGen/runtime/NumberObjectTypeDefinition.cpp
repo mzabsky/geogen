2186243ed4df5f9ef170e08f6fd3cd2a4a1d77b5
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-01-12 19:41:20 +0000

    [GGS Branch] Implemented NumberObject.

---
+++ branches/ggs/src/GeoGen/runtime/NumberObjectTypeDefinition.cpp
@@ -0,0 +1,53 @@
+#include "..\InternalErrorException.hpp"
+#include "..\Number.hpp"
+#include "NumberObjectTypeDefinition.hpp"
+#include "DynamicObject.hpp"
+#include "NumberObject.hpp"
+
+using namespace geogen;
+using namespace runtime;
+
+DynamicObject* NumberObjectTypeDefinition::CreateInstance(Number value) const
+{
+	return new NumberObject(this, value);
+}
+
+bool NumberObjectTypeDefinition::InstanceLessThan(DynamicObject const* a, DynamicObject const* b) const
+{
+	if (a->GetType() != this)
+	{
+		throw new InternalErrorException("Using InstanceLessThan on object of incorrect type.");
+	}
+
+	if (b->GetType() == this)
+	{
+		return ((NumberObject const*)a)->GetValue() < ((NumberObject const*)b)->GetValue();
+	}
+
+	return TypeDefinition::InstanceLessThan(a, b);
+}
+
+bool NumberObjectTypeDefinition::InstanceEqualsTo(DynamicObject const* a, DynamicObject const* b) const
+{
+	if (a->GetType() != this)
+	{
+		throw new InternalErrorException("Using InstanceEqualsTo on object of incorrect type.");
+	}
+
+	if (b->GetType() == this)
+	{
+		return ((NumberObject const*)a)->GetValue() == ((NumberObject const*)b)->GetValue();
+	}
+
+	return TypeDefinition::InstanceEqualsTo(a, b);
+}
+
+DynamicObject* NumberObjectTypeDefinition::Copy(DynamicObject const* a) const
+{
+	if (a->GetType() != this)
+	{
+		throw new InternalErrorException("Using Copy on object of incorrect type.");
+	}
+
+	return new NumberObject(this, ((NumberObject const*)a)->GetValue());
+}
