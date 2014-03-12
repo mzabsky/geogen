a8eee88692211bb36b7b6e06898e1627bb55fc0e
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-03-12 21:51:10 +0000

    [GGS Branch] Started VM initialization.

---
+++ branches/ggs/src/GeoGen/ApiUsageException.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <stdexcept>
+
+#include "ErrorCode.hpp"
+#include "GeoGenException.hpp"
+
+namespace geogen
+{
+	class ApiUsageException : public GeoGenException
+	{
+	private:
+		std::string message;
+	public:
+		explicit ApiUsageException(std::string const& message) :
+			GeoGenException(GGE4000_InternalError) {};
+
+		virtual std::string GetDetailMessage()
+		{
+			return "Incorrect GeoGen API usage dectected (" + message + ").";
+		}
+	};
+}
