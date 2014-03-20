d321de55dc18ac41bf22e4813b953fe537b36c35
Matěj Zábský <mzabsky@gmail.com>
2014-03-20 12:30:16 +0000

    [GGS Branch] Started libraries, improved work with defintion ownership.

--- src/GeoGen/runtime/SymbolDefinitionTable.hpp
+++ src/GeoGen/runtime/SymbolDefinitionTable.hpp
@@ -4,12 +4,13 @@
 #include <map>
 
 #include "../ErrorCode.hpp"
+#include "../ApiUsageException.hpp"
 
 namespace geogen 
 {
 	namespace runtime 
 	{		
-		template<class TSymbolBase>
+		template<class TSymbolBase, bool TOwning = false>
 		class SymbolDefinitionTable
 		{
 		private:
@@ -34,7 +35,7 @@ namespace geogen
 				another.table.clear();
 			}*/
 
-			inline TSymbolBase* GetItem(std::string symbolName)
+			/*inline TSymbolBase* GetItem(std::string symbolName)
 			{
 				std::map<std::string, TSymbolBase*>::iterator item = this->table.find(symbolName);
 				
@@ -43,9 +44,9 @@ namespace geogen
 				}
 
 				return (*item).second;
-			};
+			};*/
 
-			inline TSymbolBase const* GetItem(std::string symbolName) const
+			inline TSymbolBase const* GetItem(std::string const& symbolName) const
 			{
 				std::map<std::string, TSymbolBase*>::const_iterator item = this->table.find(symbolName);
 				
@@ -56,9 +57,25 @@ namespace geogen
 				return (*item).second;
 			};
 
-			inline bool ContainsItem(std::string symbolName) const { return this->table.find(symbolName) == this->table.end(); };
+			inline bool ContainsItem(std::string const& symbolName) const { return this->table.find(symbolName) == this->table.end(); };
 
-			inline bool AddItem(TSymbolBase* symbol, std::string symbolName) {
+			inline bool AddItem(TSymbolBase const* symbol, std::string const& symbolName) {
+				if (TOwning)
+				{
+					throw ApiUsageException("Cannot add const object to an exclusively owning collection.");
+				}
+
+				if (this->table.find(symbolName) != this->table.end()){
+					return false;
+				}
+
+				// We know only const operations will be done with the object (if the collection is not exclusively owning).
+				this->table[symbolName] = const_cast<TSymbolBase*>(symbol);
+
+				return true;
+			};
+
+			inline bool AddItem(TSymbolBase* symbol, std::string const& symbolName) {
 				if (this->table.find(symbolName) != this->table.end()){
 					return false;
 				}
@@ -73,9 +90,12 @@ namespace geogen
 
 			~SymbolDefinitionTable()
 			{
-				for (std::map<std::string, TSymbolBase*>::iterator it = this->table.begin(); it != this->table.end(); it++)
+				if (TOwning)
 				{
-					delete it->second;
+					for (std::map<std::string, TSymbolBase*>::iterator it = this->table.begin(); it != this->table.end(); it++)
+					{
+						delete it->second;
+					}
 				}
 			}
 		};
