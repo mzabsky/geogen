38c02adef9f5267b40bc7a0538daadf5eab2d901
Matěj Zábský <mzabsky@gmail.com>
2014-06-14 11:43:16 +0000

    [GGS Branch] Removed several unused fields.

--- src/GeoGen/runtime/CompiledScript.hpp
+++ src/GeoGen/runtime/CompiledScript.hpp
@@ -21,17 +21,12 @@ namespace geogen
 		class CompiledScript
 		{
 			private:	
-				//SymbolNameTable symbolNameTable;
-
 				SymbolDefinitionTable<VariableDefinition> globalVariableDefinitions;
 				SymbolDefinitionTable<FunctionDefinition> globalFunctionDefinitions;
 				SymbolDefinitionTable<TypeDefinition> typeDefinitions;
 
 				std::vector<FunctionDefinition*> ownedFunctionDefinitions;
 				std::vector<TypeDefinition*> ownedTypeDefinitions;
-
-				CodeBlock rootCodeBlock;
-				//CodeBlock initializationCodeBlock;
 
 				MetadataKeyValueCollection metadata;
 
@@ -42,15 +37,10 @@ namespace geogen
 				CompiledScript();
 				~CompiledScript();
 
-				//inline SymbolNameTable const& GetSymbolNameTable() const { return this->symbolNameTable; }
-				//inline SymbolNameTable& GetSymbolNameTable() { return this->symbolNameTable; }				
-
 				ScriptParameters CreateScriptParameters() const;
 
 				inline MetadataKeyValueCollection const& GetMetadata() const { return this->metadata; }
 				inline MetadataKeyValueCollection& GetMetadata() { return this->metadata; }
-
-				//inline void SetMetadata(MetadataKeyValueCollection* metadata) { this->metadata = metadata; }
 
 				inline SymbolDefinitionTable<VariableDefinition> const& GetGlobalVariableDefinitions() const { return this->globalVariableDefinitions; }
 				//inline SymbolDefinitionTable<VariableDefinition>& GetGlobalVariableDefinitions() { return this->globalVariableDefinitions; }
