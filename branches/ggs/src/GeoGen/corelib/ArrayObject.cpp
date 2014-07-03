6b11fbc72879bf188c93f162efa47e9ff8b56ee4
mzabsky <mzabsky@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-07-03 07:32:29 +0000

    [GGS Branch] Array now properly tracks the order of its elements.

--- branches/ggs/src/GeoGen/corelib/ArrayObject.cpp
+++ branches/ggs/src/GeoGen/corelib/ArrayObject.cpp
@@ -11,7 +11,7 @@ using namespace geogen::runtime;
 
 ArrayObject::~ArrayObject()
 {
-	for (iterator it = this->Begin(); it != this->End(); it++)
+	for (HashMap::iterator it = this->hashmap.begin(); it != this->hashmap.end(); it++)
 	{
 		it->first->RemoveRef(*this->GetMemberVariableTable().GetMemoryManager());
 		it->second->RemoveRef(*this->GetMemberVariableTable().GetMemoryManager());
@@ -25,7 +25,7 @@ ManagedObject* ArrayObject::Front(VirtualMachine* vm, CodeLocation location)
 		throw InvalidOperationOnEmptyArrayException(location, GG_STR("Front"));
 	}
 
-	return this->array.begin()->second;
+	return this->Get(vm, location, *this->list.begin());
 }
 
 ManagedObject* ArrayObject::Back(VirtualMachine* vm, CodeLocation location)
@@ -35,13 +35,13 @@ ManagedObject* ArrayObject::Back(VirtualMachine* vm, CodeLocation location)
 		throw InvalidOperationOnEmptyArrayException(location, GG_STR("Back"));
 	}
 
-	return this->array.rbegin()->second;
+	return this->Get(vm, location, *this->list.rbegin());
 }
 
 ManagedObject* ArrayObject::Get(VirtualMachine* vm, CodeLocation location, ManagedObject* key)
 {
-	iterator it = this->array.find(key);
-	if (it == this->End())
+	HashMap::iterator it = this->hashmap.find(key);
+	if (it == this->hashmap.end())
 	{
 		throw ArrayKeyNotFoundException(location, key->GetStringValue());
 	}
@@ -51,15 +51,17 @@ ManagedObject* ArrayObject::Get(VirtualMachine* vm, CodeLocation location, Manag
 
 void ArrayObject::Set(VirtualMachine* vm, CodeLocation location, ManagedObject* key, ManagedObject* value)
 {
-	iterator it = this->array.find(key);
-	if (it != this->End())
+	HashMap::iterator hashIt = this->hashmap.find(key);
+	if (hashIt != this->hashmap.end())
 	{
-		it->second->RemoveRef(vm->GetMemoryManager());
-		it->second = value;
+		hashIt->second->RemoveRef(vm->GetMemoryManager());
+		hashIt->second = value;
+
+		// TODO: Nemelo by zde byt addref?
 	}
 	else
 	{
-		this->array[key] = value;
+		this->hashmap[key] = value;
 		key->AddRef();
 
 		// Bump the max integer key if necessary
@@ -71,6 +73,8 @@ void ArrayObject::Set(VirtualMachine* vm, CodeLocation location, ManagedObject* 
 				this->maxIntegerKey = static_cast<int>(ceil(numberKey->GetValue()));
 			}
 		}
+
+		this->list.push_back(key);
 	}
 
 	value->AddRef();
@@ -83,10 +87,20 @@ void ArrayObject::PopFront(runtime::VirtualMachine* vm, CodeLocation location)
 		throw InvalidOperationOnEmptyArrayException(location, GG_STR("PopFront"));
 	}
 
-	iterator it = this->Begin();
-	it->first->RemoveRef(vm->GetMemoryManager());
-	it->second->RemoveRef(vm->GetMemoryManager());
-	this->array.erase(it);
+	iterator listIt = this->Begin();
+	ManagedObject* key = *listIt;
+
+	HashMap::iterator hashIt = this->hashmap.find(key);
+	if (hashIt == this->hashmap.end())
+	{
+		throw InternalErrorException("Array internal consistency error - item found in list, but not in hash map during PopFront.");
+	}
+
+	hashIt->first->RemoveRef(vm->GetMemoryManager());
+	hashIt->second->RemoveRef(vm->GetMemoryManager());
+
+	this->hashmap.erase(hashIt);
+	this->list.erase(listIt);
 }
 
 void ArrayObject::PopBack(runtime::VirtualMachine* vm, CodeLocation location)
@@ -96,10 +110,20 @@ void ArrayObject::PopBack(runtime::VirtualMachine* vm, CodeLocation location)
 		throw InvalidOperationOnEmptyArrayException(location, GG_STR("PopBack"));
 	}
 
-	iterator it = --this->End();
-	it->first->RemoveRef(vm->GetMemoryManager());
-	it->second->RemoveRef(vm->GetMemoryManager());
-	this->array.erase(it);
+	iterator listIt = --this->End();
+	ManagedObject* key = *listIt;
+
+	HashMap::iterator hashIt = this->hashmap.find(key);
+	if (hashIt == this->hashmap.end())
+	{
+		throw InternalErrorException("Array internal consistency error - item found in list, but not in hash map during PopBack.");
+	}
+
+	hashIt->first->RemoveRef(vm->GetMemoryManager());
+	hashIt->second->RemoveRef(vm->GetMemoryManager());
+
+	this->hashmap.erase(hashIt);
+	this->list.erase(listIt);
 }
 
 void ArrayObject::PushBack(VirtualMachine* vm, CodeLocation location, ManagedObject* object)
@@ -111,18 +135,18 @@ void ArrayObject::PushBack(VirtualMachine* vm, CodeLocation location, ManagedObj
 
 	if (key->GetRefCount() < 1)
 	{
-		throw InternalErrorException(GG_STR("PushBack could not push object into the array."));
+		throw InternalErrorException(GG_STR("PushBack could not push object into the hashmap."));
 	}
 }
 
 bool ArrayObject::ContainsKey(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* key)
 {
-	return this->array.find(key) != this->End();
+	return this->hashmap.find(key) != this->hashmap.end();
 }
 
 bool ArrayObject::ContainsValue(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* value)
 {	
-	for (const_iterator it = this->Begin(); it != this->End(); it++)
+	for (HashMap::const_iterator it = this->hashmap.begin(); it != this->hashmap.end(); it++)
 	{
 		if (value->GetType()->InstanceEqualsTo(value, it->second))
 		{
@@ -135,27 +159,40 @@ bool ArrayObject::ContainsValue(runtime::VirtualMachine* vm, CodeLocation locati
 
 void ArrayObject::RemoveKey(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* key)
 {
-	iterator it = this->array.find(key);
-	if (it == this->End())
+	HashMap::iterator hashIt = this->hashmap.find(key);
+	if (hashIt == this->hashmap.end())
 	{
 		throw ArrayKeyNotFoundException(location, key->GetStringValue());
 	}
 
-	it->first->RemoveRef(vm->GetMemoryManager());
-	it->second->RemoveRef(vm->GetMemoryManager());
-	this->array.erase(it);
+	List::iterator foundListIterator = this->End();
+	for (iterator listIt = this->Begin(); listIt != this->End(); listIt++)
+	{
+		if (key->GetType()->InstanceEqualsTo(key, *listIt))
+		{
+			foundListIterator = listIt;
+		}
+	}
+
+	if (foundListIterator == this->list.end())
+	{
+		throw InternalErrorException("Array internal consistency error - item found in hash map, but not in list during RemoveKey.");
+	}
+
+	this->list.erase(foundListIterator);
+
+	hashIt->first->RemoveRef(vm->GetMemoryManager());
+	hashIt->second->RemoveRef(vm->GetMemoryManager());
+	this->hashmap.erase(hashIt);
 }
 
 void ArrayObject::RemoveValue(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* value)
 {
-	for (const_iterator it = this->Begin(); it != this->End(); it++)
+	for (HashMap::iterator it = this->hashmap.begin(); it != this->hashmap.end(); it++)
 	{
 		if (value->GetType()->InstanceEqualsTo(value, it->second))
 		{
-			it->first->RemoveRef(vm->GetMemoryManager());
-			it->second->RemoveRef(vm->GetMemoryManager());
-
-			this->array.erase(it);
+			this->RemoveKey(vm, location, it->first);
 			return;
 		}
 	}
@@ -167,7 +204,7 @@ String ArrayObject::GetStringValue() const
 	StringStream ss;
 	ss << "{" << endl;
 
-	for (const_iterator it = this->Begin(); it != this->End(); it++)
+	for (HashMap::const_iterator it = this->hashmap.begin(); it != this->hashmap.end(); it++)
 	{
 		ss << "\t{" << it->first->GetStringValue() << "}: {" << it->second->GetStringValue() << "}" << endl;
 	}
--- branches/ggs/src/GeoGen/corelib/ArrayObject.hpp
+++ branches/ggs/src/GeoGen/corelib/ArrayObject.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <list>
 
 #include "../runtime/ManagedObject.hpp"
 
@@ -11,13 +12,16 @@ namespace geogen
 		class ArrayObject : public runtime::ManagedObject
 		{
 		public:
-			typedef std::map<ManagedObject*, ManagedObject*, geogen::runtime::CompareObjects> BackingCollection;
-			typedef BackingCollection::iterator iterator;
-			typedef BackingCollection::const_iterator const_iterator;
-			typedef BackingCollection::reverse_iterator reverse_iterator;
-			typedef BackingCollection::const_reverse_iterator const_reverse_iterator;
+			typedef std::map<ManagedObject*, ManagedObject*, geogen::runtime::CompareObjects> HashMap;
+			typedef std::list<ManagedObject*> List;
+			typedef List::iterator iterator;
+			typedef List::const_iterator const_iterator;
+			typedef List::reverse_iterator reverse_iterator;
+			typedef List::const_reverse_iterator const_reverse_iterator;
 		private:
-			BackingCollection array;
+			HashMap hashmap;
+			List list;
+
 			int maxIntegerKey;
 		public:
 			ArrayObject(runtime::VirtualMachine* vm, runtime::TypeDefinition const* type) : ManagedObject(vm, type), maxIntegerKey(-1)
@@ -26,20 +30,20 @@ namespace geogen
 
 			virtual ~ArrayObject();
 
-			inline BackingCollection& GetBackingCollection() { return this->array; }
-			inline BackingCollection const& GetBackingCollection() const { return this->array; }
+			//inline BackingCollection& GetBackingCollection() { return this->hashmap; }
+			//inline BackingCollection const& GetBackingCollection() const { return this->hashmap; }
 
-			inline const_iterator Begin() const { return *(const_iterator*)(&this->array.begin()); }
-			inline const_iterator End() const { return *(const_iterator*)(&this->array.end()); }
+			inline const_iterator Begin() const { return *(const_iterator*)(&this->list.begin()); }
+			inline const_iterator End() const { return *(const_iterator*)(&this->list.end()); }
 
-			inline iterator Begin() { return this->array.begin(); }
-			inline iterator End() { return this->array.end(); }
+			inline iterator Begin() { return this->list.begin(); }
+			inline iterator End() { return this->list.end(); }
 
-			inline const_reverse_iterator RBegin() const { return *(const_reverse_iterator*)(&this->array.rbegin()); }
-			inline const_reverse_iterator REnd() const { return *(const_reverse_iterator*)(&this->array.rend()); }
+			inline const_reverse_iterator RBegin() const { return *(const_reverse_iterator*)(&this->list.rbegin()); }
+			inline const_reverse_iterator REnd() const { return *(const_reverse_iterator*)(&this->list.rend()); }
 
-			inline reverse_iterator RBegin() { return this->array.rbegin(); }
-			inline reverse_iterator REnd() { return this->array.rend(); }
+			inline reverse_iterator RBegin() { return this->list.rbegin(); }
+			inline reverse_iterator REnd() { return this->list.rend(); }
 
 			void Set(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* key, ManagedObject* value);
 			ManagedObject* Get(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* key);
@@ -61,7 +65,7 @@ namespace geogen
 			void RemoveKey(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* key);
 			void RemoveValue(runtime::VirtualMachine* vm, CodeLocation location, ManagedObject* value);
 
-			inline unsigned Count() const { return this->array.size(); };
+			inline unsigned Count() const { return this->hashmap.size(); };
 
 			virtual String GetStringValue() const;
 		};
