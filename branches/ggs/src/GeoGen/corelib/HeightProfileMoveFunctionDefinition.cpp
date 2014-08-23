01d05f5b157f7021a20bdfeeb0872150362d5015
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-08-23 14:25:05 +0000

    [GGS Branch] Added HS.Move.

---
+++ branches/ggs/src/GeoGen/corelib/HeightProfileMoveFunctionDefinition.cpp
@@ -0,0 +1,43 @@
+#include "HeightProfileMoveFunctionDefinition.hpp"
+#include "../runtime/VirtualMachine.hpp"
+#include "../runtime/ManagedObject.hpp"
+#include "HeightProfileTypeDefinition.hpp"
+#include "HeightProfileMoveRenderingStep.hpp"
+#include "NumberTypeDefinition.hpp"
+#include "CoordinateTypeDefinition.hpp"
+#include "CoordinateObject.hpp"
+#include "HeightOverflowException.hpp"
+#include "UnknownRelativeCoordinateDirectionException.hpp"
+
+using namespace std;
+using namespace geogen;
+using namespace geogen::corelib;
+using namespace geogen::runtime;
+using namespace geogen::renderer;
+
+ManagedObject* HeightProfileMoveFunctionDefinition::CallNative(CodeLocation location, VirtualMachine* vm, ManagedObject* instance, vector<ManagedObject*> arguments) const
+{
+	CoordinateTypeDefinition const* coordinateTypeDefinition = dynamic_cast<CoordinateTypeDefinition const*>(vm->GetTypeDefinition(GG_STR("Coordinate")));
+	NumberTypeDefinition const* numberTypeDefinition = vm->GetNumberTypeDefinition();
+
+	vector<TypeDefinition const*> expectedTypes;
+	expectedTypes.push_back(coordinateTypeDefinition);
+
+	CoordinateObject* offsetObject = dynamic_cast<CoordinateObject*>(arguments[0]);
+
+	if (offsetObject->IsRelative())
+	{
+		throw UnknownRelativeCoordinateDirectionException(location);
+	}
+
+	Coordinate offset = offsetObject->GetValue();
+
+	vector<ManagedObjectHolder> convertedObjectHolders = this->CheckArguments(vm, location, expectedTypes, arguments);
+
+	vector<unsigned> argumentSlots;
+	unsigned returnObjectSlot = vm->GetRendererObjectSlotTable().GetObjectSlotByAddress(instance);
+	RenderingStep* renderingStep = new HeightProfileMoveRenderingStep(location, argumentSlots, returnObjectSlot, offset);
+	vm->GetRenderingSequence().AddStep(renderingStep);
+
+	return instance;
+}
