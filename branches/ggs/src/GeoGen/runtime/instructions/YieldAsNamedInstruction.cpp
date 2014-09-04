357cbb37337632eebbdc8a8231aada77ab1fc78b
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-09-04 21:01:31 +0000

    [GGS Branch] Don't generate yield steps for maps that are not to be returned.

--- branches/ggs/src/GeoGen/runtime/instructions/YieldAsNamedInstruction.cpp
+++ branches/ggs/src/GeoGen/runtime/instructions/YieldAsNamedInstruction.cpp
@@ -19,56 +19,59 @@ InstructionStepResult YieldAsNamedInstruction::Step(VirtualMachine* vm) const
 	yieldedValue->AddRef();
 	vm->GetObjectStack().Pop(vm);
 
-	vector<unsigned> argumentSlots;
-	argumentSlots.push_back(vm->GetRendererObjectSlotTable().GetObjectSlotByAddress(yieldedValue));
-	unsigned returnObjectSlot = vm->GetRendererObjectSlotTable().GetObjectSlotByAddress(yieldedValue);
-
-	Coordinate renderOriginX = vm->GetArguments().GetRenderOriginX();
-	Size1D renderWidth = vm->GetArguments().GetRenderWidth();
-	if (renderWidth == MAP_SIZE_AUTOMATIC)
+	if (vm->GetArguments().GetEnabledMaps().empty() || find(vm->GetArguments().GetEnabledMaps().begin(), vm->GetArguments().GetEnabledMaps().end(), this->functionName) != vm->GetArguments().GetEnabledMaps().end())
 	{
+		vector<unsigned> argumentSlots;
+		argumentSlots.push_back(vm->GetRendererObjectSlotTable().GetObjectSlotByAddress(yieldedValue));
+		unsigned returnObjectSlot = vm->GetRendererObjectSlotTable().GetObjectSlotByAddress(yieldedValue);
+
+		Coordinate renderOriginX = vm->GetArguments().GetRenderOriginX();
+		Size1D renderWidth = vm->GetArguments().GetRenderWidth();
+		if (renderWidth == MAP_SIZE_AUTOMATIC)
+		{
+			if (vm->GetArguments().GetMaxMapWidth() != MAP_SIZE_INFINITE)
+			{
+				renderWidth = vm->GetArguments().GetMapWidth();
+			}
+			else 
+			{
+				renderWidth = RENDER_SIZE_DEFAULT;
+			}
+		}
+
 		if (vm->GetArguments().GetMaxMapWidth() != MAP_SIZE_INFINITE)
 		{
-			renderWidth = vm->GetArguments().GetMapWidth();
+			renderOriginX = max(renderOriginX, 0);
+			renderWidth = min(Size1D(vm->GetArguments().GetMapWidth() * vm->GetArguments().GetRenderScale()) - renderOriginX, renderWidth);
 		}
-		else 
+
+		Coordinate renderOriginY = vm->GetArguments().GetRenderOriginY();
+		Size1D renderHeight = vm->GetArguments().GetRenderHeight();
+		if (renderHeight == MAP_SIZE_AUTOMATIC)
 		{
-			renderWidth = RENDER_SIZE_DEFAULT;
+			if (vm->GetArguments().GetMaxMapHeight() != MAP_SIZE_INFINITE)
+			{
+				renderHeight = vm->GetArguments().GetMapHeight();
+			}
+			else
+			{
+				renderHeight = RENDER_SIZE_DEFAULT;
+			}
 		}
-	}
 
-	if (vm->GetArguments().GetMaxMapWidth() != MAP_SIZE_INFINITE)
-	{
-		renderOriginX = max(renderOriginX, 0);
-		renderWidth = min(Size1D(vm->GetArguments().GetMapWidth() * vm->GetArguments().GetRenderScale()) - renderOriginX, renderWidth);
-	}
-
-	Coordinate renderOriginY = vm->GetArguments().GetRenderOriginY();
-	Size1D renderHeight = vm->GetArguments().GetRenderHeight();
-	if (renderHeight == MAP_SIZE_AUTOMATIC)
-	{
 		if (vm->GetArguments().GetMaxMapHeight() != MAP_SIZE_INFINITE)
 		{
-			renderHeight = vm->GetArguments().GetMapHeight();
+			renderOriginY = max(renderOriginY, 0);
+			renderHeight = min(Size1D(vm->GetArguments().GetMapHeight() * vm->GetArguments().GetRenderScale()) - renderOriginY, renderHeight);
 		}
-		else
-		{
-			renderHeight = RENDER_SIZE_DEFAULT;
-		}
+
+		//Rectangle mapRectangle = Rectangle(Point(0, 0), Size2D(vm->GetArguments().GetMapWidth(), vm->GetArguments().GetMapHeight()));
+		Rectangle renderRectangle = Rectangle(Point(vm->GetArguments().GetRenderOriginX(), vm->GetArguments().GetRenderOriginY()), Size2D(renderWidth, renderHeight));
+		//Rectangle actualRenderRectangle = Rectangle::Intersect(mapRectangle, renderRectangle);
+
+		RenderingStep* renderingStep = new YieldRenderingStep(this->GetLocation(), argumentSlots, returnObjectSlot, this->functionName, renderRectangle);
+		vm->GetRenderingSequence().AddStep(renderingStep);
 	}
-
-	if (vm->GetArguments().GetMaxMapHeight() != MAP_SIZE_INFINITE)
-	{
-		renderOriginY = max(renderOriginY, 0);
-		renderHeight = min(Size1D(vm->GetArguments().GetMapHeight() * vm->GetArguments().GetRenderScale()) - renderOriginY, renderHeight);
-	}
-
-	//Rectangle mapRectangle = Rectangle(Point(0, 0), Size2D(vm->GetArguments().GetMapWidth(), vm->GetArguments().GetMapHeight()));
-	Rectangle renderRectangle = Rectangle(Point(vm->GetArguments().GetRenderOriginX(), vm->GetArguments().GetRenderOriginY()), Size2D(renderWidth, renderHeight));
-	//Rectangle actualRenderRectangle = Rectangle::Intersect(mapRectangle, renderRectangle);
-
-	RenderingStep* renderingStep = new YieldRenderingStep(this->GetLocation(), argumentSlots, returnObjectSlot, this->functionName, renderRectangle);
-	vm->GetRenderingSequence().AddStep(renderingStep);
 
 	yieldedValue->RemoveRef(vm->GetMemoryManager());
 
