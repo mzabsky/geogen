91ff33898ecf31d1a8740c177bb9aa21fc395afa
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-09-14 21:31:16 +0000

    [GGS Branch] Added documentation to utils.

--- branches/ggs/src/GeoGen/utils/OwningMap.hpp
+++ branches/ggs/src/GeoGen/utils/OwningMap.hpp
@@ -8,12 +8,20 @@ namespace geogen
 {
 	namespace utils
 	{
-		template<typename T>
+		/// Base for classes that contains a collection of exclusively owned items indexed by string
+		/// names.
+		/// @tparam T Type of contained items (without *).
+		template<typename T>		
 		class OwningMap
 		{
 		protected:
 			std::map<String, T*> table;
 		public:
+
+			/// Adds an item to the collection. The collection will assume ownership of the pointer.
+			/// @param name The name.
+			/// @param value The item to be added.
+			/// @return true if the item was successfully added (the collection didn't contain an item with the same name yet).
 			bool AddItem(String const& name, T* value)
 			{
 				if (this->table.find(name) != this->table.end()){
@@ -25,15 +33,44 @@ namespace geogen
 				return true;
 			}
 
+			/// Gets the number of items in the collection.
+			/// @return Number of items in the collection.
 			inline unsigned Size() const { return this->table.size(); }
+
+			/// Gets an iterator pointing to the first item in the collection (in order by names).
+			/// @return An iterator.
 			inline typename std::map<String, T const*>::const_iterator Begin() const { return *(typename std::map<String, T const*>::const_iterator*)(&this->table.begin()); }
+
+			/// Gets the ending iterator.
+			/// @return An iterator.
 			inline typename std::map<String, T const*>::const_iterator End() const { return *(typename std::map<String, T const*>::const_iterator*)(&this->table.end()); }
+			
+			/// Gets an iterator pointing to the first item in the collection (in order by names).
+			/// @return An iterator.
 			inline typename std::map<String, T*>::iterator Begin() { return this->table.begin(); }
+			
+			/// Gets the ending iterator.
+			/// @return An iterator.
 			inline typename std::map<String, T*>::iterator End() { return this->table.end(); }
 
+			/// Gets an iterator pointing to an item with given name, or an ending iterator if the collection doesn't contain item with than name.
+			/// @param key The name.
+			/// @return An iterator.
 			inline typename std::map<String, T const*>::const_iterator FindItem(String const& key) const { return *(typename std::map<String, T const*>::const_iterator*)(&this->table.find(key)); }
+			
+			/// Gets an iterator pointing to an item with given name, or an ending iterator if the collection doesn't contain item with than name.
+			/// @param key The name.
+			/// @return An iterator.
 			inline typename std::map<String, T*>::iterator FindItem(String const& key) { return this->table.find(key); }
+
+			/// Determines whether the collection contains item with given name.
+			/// @param key The name.
+			/// @return true if the collection contains the name, false otherwise.
 			inline bool ContainsItem(String const& key) const { return this->FindItem(key) != this->End(); };
+
+			/// Gets an item with given name.
+			/// @param key The name.
+			/// @return The item, or NULL if not found.
 			T const* GetItem(String const& key) const
 			{
 				typename std::map<String, T const*>::const_iterator it = this->FindItem(key);
@@ -45,6 +82,9 @@ namespace geogen
 				return it->second;
 			};
 
+			/// Gets an item with given name.
+			/// @param key The name.
+			/// @return The item, or NULL if not found.
 			T* GetItem(String const& key)
 			{
 				typename std::map<String, T*>::iterator it = this->FindItem(key);
@@ -56,6 +96,7 @@ namespace geogen
 				return it->second;
 			};
 
+			/// Virtual destructor. Destroys all owned items.
 			virtual ~OwningMap()
 			{
 				for (typename std::map<String, T*>::iterator it = this->table.begin(); it != this->table.end(); it++)
--- branches/ggs/src/GeoGen/utils/StringUtils.hpp
+++ branches/ggs/src/GeoGen/utils/StringUtils.hpp
@@ -8,7 +8,14 @@ namespace geogen
 {
 	namespace utils
 	{
+		/// Converts a string into a collection of lines.
+		/// @param str The string.
+		/// @return A collection of lines;
 		std::vector<String> StringToLines(String const& str);
+
+		/// Format a number as a file size (with appropriate file size units).
+		/// @param sizeInBytes The size in bytes.
+		/// @return The formatted file size.
 		String FormatFileSize(unsigned sizeInBytes);
 	}
 }
