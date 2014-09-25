b4fc89e84cb08bba9418b2cd698736d52d4d56cd
Matěj Zábský <mzabsky@gmail.com>
2014-09-25 18:17:07 +0000

    [GGS Branch] HP and HM Noise now accepts random seed.

--- src/GeoGen/corelib/HeightMapNoiseFunctionDefinition.cpp
+++ src/GeoGen/corelib/HeightMapNoiseFunctionDefinition.cpp
@@ -4,6 +4,7 @@
 #include "../genlib/NoiseLayersFactory.hpp"
 #include "HeightMapTypeDefinition.hpp"
 #include "ArrayTypeDefinition.hpp"
+#include "NumberTypeDefinition.hpp"
 #include "HeightMapFlatRenderingStep.hpp"
 #include "HeightMapNoiseRenderingStep.hpp"
 #include "ParseNoiseInput.hpp"
@@ -19,13 +20,18 @@ using namespace geogen::genlib;
 ManagedObject* HeightMapNoiseFunctionDefinition::CallNative(CodeLocation location, VirtualMachine* vm, ManagedObject* instance, vector<ManagedObject*> arguments) const
 {
 	ArrayTypeDefinition const* arrayTypeDefinition = dynamic_cast<ArrayTypeDefinition const*>(vm->GetTypeDefinition(GG_STR("Array")));
+	NumberTypeDefinition const* numberTypeDefinition = vm->GetNumberTypeDefinition();
 
 	vector<TypeDefinition const*> expectedTypes;
 	expectedTypes.push_back(arrayTypeDefinition);
+	expectedTypes.push_back(numberTypeDefinition);
 	
 	vector<ManagedObjectHolder> convertedObjectHolders = this->CheckArguments(vm, location, expectedTypes, arguments, 0);
 
 	NoiseLayers layers = ParseNoiseInput(vm, location, arguments);
+
+	RandomSeed argumentSeed = arguments.size() > 1 ? (RandomSeed)dynamic_cast<NumberObject*>(arguments[1])->GetValue() : 0;
+	RandomSeed compositeSeed = CombineSeeds(argumentSeed, vm->GetArguments().GetRandomSeed());
 
 	ManagedObject* returnObject = dynamic_cast<HeightMapTypeDefinition const*>(instance->GetType())->CreateInstance(vm);
 	
@@ -41,7 +47,7 @@ ManagedObject* HeightMapNoiseFunctionDefinition::CallNative(CodeLocation locatio
 	unsigned i = 0;
 	for (NoiseLayers::const_iterator it = layers.begin(); it != layers.end(); it++)
 	{
-		RenderingStep* renderingStep = new HeightMapNoiseRenderingStep(location, argumentSlots, objectSlot, it->first, it->second, 0, i);
+		RenderingStep* renderingStep = new HeightMapNoiseRenderingStep(location, argumentSlots, objectSlot, it->first, it->second, compositeSeed, i);
 		vm->GetRenderingSequence().AddStep(renderingStep);
 		i++;
 	}
--- src/GeoGen/corelib/HeightProfileNoiseFunctionDefinition.cpp
+++ src/GeoGen/corelib/HeightProfileNoiseFunctionDefinition.cpp
@@ -2,28 +2,36 @@
 #include "../runtime/VirtualMachine.hpp"
 #include "HeightProfileTypeDefinition.hpp"
 #include "ArrayTypeDefinition.hpp"
+#include "NumberTypeDefinition.hpp"
 #include "../runtime/ManagedObject.hpp"
 #include "HeightProfileFlatRenderingStep.hpp"
 #include "HeightProfileNoiseRenderingStep.hpp"
 #include "ParseNoiseInput.hpp"
+#include "../random/RandomSeed.hpp"
 
 using namespace std;
 using namespace geogen;
-using namespace geogen::corelib;
-using namespace geogen::runtime;
-using namespace geogen::renderer;
-using namespace geogen::genlib;
+using namespace corelib;
+using namespace runtime;
+using namespace renderer;
+using namespace genlib;
+using namespace random;
 
 ManagedObject* HeightProfileNoiseFunctionDefinition::CallNative(CodeLocation location, VirtualMachine* vm, ManagedObject* instance, vector<ManagedObject*> arguments) const
 {
 	ArrayTypeDefinition const* arrayTypeDefinition = dynamic_cast<ArrayTypeDefinition const*>(vm->GetTypeDefinition(GG_STR("Array")));
+	NumberTypeDefinition const* numberTypeDefinition = vm->GetNumberTypeDefinition();
 
 	vector<TypeDefinition const*> expectedTypes;
 	expectedTypes.push_back(arrayTypeDefinition);
+	expectedTypes.push_back(numberTypeDefinition);
 
 	vector<ManagedObjectHolder> convertedObjectHolders = this->CheckArguments(vm, location, expectedTypes, arguments, 0);
 
 	NoiseLayers layers = ParseNoiseInput(vm, location, arguments);
+
+	RandomSeed argumentSeed = arguments.size() > 1 ? (RandomSeed)dynamic_cast<NumberObject*>(arguments[1])->GetValue() : 0;
+	RandomSeed compositeSeed = CombineSeeds(argumentSeed, vm->GetArguments().GetRandomSeed());
 
 	ManagedObject* returnObject = dynamic_cast<HeightProfileTypeDefinition const*>(instance->GetType())->CreateInstance(vm);
 
@@ -39,7 +47,7 @@ ManagedObject* HeightProfileNoiseFunctionDefinition::CallNative(CodeLocation loc
 	unsigned i = 0;
 	for (NoiseLayers::const_iterator it = layers.begin(); it != layers.end(); it++)
 	{
-		RenderingStep* renderingStep = new HeightProfileNoiseRenderingStep(location, argumentSlots, objectSlot, it->first, it->second, 0, i);
+		RenderingStep* renderingStep = new HeightProfileNoiseRenderingStep(location, argumentSlots, objectSlot, it->first, it->second, compositeSeed, i);
 		vm->GetRenderingSequence().AddStep(renderingStep);
 		i++;
 	}
