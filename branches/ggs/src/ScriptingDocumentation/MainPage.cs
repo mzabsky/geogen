e9a30990621f8abac093caf2c58644c684542e61
mzabsky@gmail.com <mzabsky@gmail.com@5049608c-eac8-11dd-b00f-9fabb4b2ff13>
2014-10-15 13:41:43 +0000

    [GGS Branch] Redone the scripting reference main page.

--- branches/ggs/src/ScriptingDocumentation/MainPage.cs
+++ branches/ggs/src/ScriptingDocumentation/MainPage.cs
@@ -1,4 +1,12 @@
-/// @mainpage Introduction
+/// @mainpage Getting started
+/// 
+/// This is a full reference of GeoGen Script and its standard library.
+/// 
 /// GeoGen Script is the language used to write map scripts for GeoGen. It is high level dynamically typed procedural programming language.
 /// 
-/// The language's syntax is inspired by modern programming languages such as C# and Java, but its function is more similar to PHP and JavaScript. The Squirrel programming language was also significant influence, because it was used as primary scripting language in previous version of GeoGen. However, GGS is much simpler language than either of these, because it forgoes more advanced features such as custom classes and inheritance.
+/// The language's syntax is inspired by modern programming languages such as C# and Java, but its function is more similar to PHP and JavaScript. However, GGS is much simpler language than either of these, because it forgoes more advanced features such as custom classes and inheritance.
+/// 
+/// **How to get started?** You can use following resources:
+/// - @link tutorials Tutorials@endlink explain how to write your first map script and how to use the stand alone generator.
+/// - @link language_reference Language reference@endlink describes the programming language in detail.
+/// - @link standard_library Standard library reference@endlink lists all classes and functions available to the scripts.
