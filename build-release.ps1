704f1f4e75252c6bd70e187135589da8cca9912f
Matěj Zábský <mzabsky@gmail.com>
2014-10-24 12:11:30 +0000

    [GGS Branch] Strip doxygen markers from example scripts whenbuilding release archive.

--- build-release.ps1
+++ build-release.ps1
@@ -5,6 +5,14 @@ function ZipFiles( $zipfilename, $sourcedir )
    [System.IO.Compression.ZipFile]::CreateFromDirectory($sourcedir, $zipfilename, $compressionLevel, $false)
 }
 
+function ReplaceDoxygenMarkers($filename)
+{
+	$script = [IO.File]::ReadAllText($filename)
+	$script = [Text.RegularExpressions.Regex]::Replace($script, "^/// \[[A-Za-z]+\]\s*", "`n", [Text.RegularExpressions.RegexOptions]::Multiline)
+	$script = $script.Replace("`n`n`n", "`n`n")
+	[IO.File]::WriteAllText($filename, $script)
+}
+
 del release -recurse 
 del geogen.zip
 mkdir release
@@ -18,6 +26,12 @@ copy documentation/ScriptingLanguage/* release/geogen/reference -recurse
 copy COPYING release/geogen
 copy README-RELEASE release/geogen
 ren release/geogen/README-RELEASE README
+
+#Add-Type -Assembly System.Text
+Get-ChildItem "$(Get-Location)/release/geogen/examples" -Filter *.ggs | `
+Foreach-Object{
+    ReplaceDoxygenMarkers $_.FullName~
+}
 
 Start-Sleep -s 1
 ZipFiles "$(Get-Location)/geogen.zip" "$(Get-Location)/release"
