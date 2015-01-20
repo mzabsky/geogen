84108fbe0044983b6da236f2380283350b3f725f
Matěj Zábský <mzabsky@gmail.com>
2015-01-20 19:03:44 +0000

    [GGS Branch] Fixed reference to an non-existent function in documentation.

--- src/ScriptingDocumentation/pages/Yield.cs
+++ src/ScriptingDocumentation/pages/Yield.cs
@@ -8,10 +8,10 @@
 ///  @code{.cs}
 /// var map = HeightMap.Noise();
 /// yield map;
-/// yield HeightMap.BumpMap(map) as "BumpMap";
+/// yield HeightMap.ConvexityMap(map) as "ConvexityMap";
 /// @endcode
 /// 		
-/// The primary map must always be yieled during execution of the script, otherwise an runtime error will be triggered when the execution finishes. Each map can be only yielded once, otherwise an runtime error is triggered.
+/// The primary map must always be yielded during execution of the script, otherwise an runtime error will be triggered when the execution finishes. Each map can be only yielded once, otherwise an runtime error is triggered.
 /// 
 /// When a map is yielded, a copy of it is made by the API for further processing. The script is allowed to make arbitrary changes to a map that was already yielded.
 /// 
