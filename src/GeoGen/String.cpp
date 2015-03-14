eef628160ac6ed5b7860e8bc210f11462bdf827b
mzabsky <9410272bd>
2015-03-14 14:41:21 +0100

    Fixed several more missing headers.

--- src/GeoGen/String.cpp
+++ src/GeoGen/String.cpp
@@ -15,6 +15,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 #include <cctype>
+#include <cstdlib>
 
 #include "String.hpp"
 #include "InternalErrorException.hpp"
