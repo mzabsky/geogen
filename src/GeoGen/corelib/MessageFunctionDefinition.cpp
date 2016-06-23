68774934a08655b555b4a63fe6c2b1b5586e535e
Matěj Zábský <mzabsky@gmail.com>
2016-06-23 22:20:28 +0200

    Fixed more backslashes.

--- src/GeoGen/corelib/MessageFunctionDefinition.cpp
+++ src/GeoGen/corelib/MessageFunctionDefinition.cpp
@@ -20,7 +20,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 #include "../corelib/StringTypeDefinition.hpp"
 #include "../InternalErrorException.hpp"
 #include "MathDefinitionRangeException.hpp"
-#include "../runtime\UserErrorException.hpp"
+#include "../runtime/UserErrorException.hpp"
 #include "StringFormatException.hpp"
 #include "../String.hpp"
 
