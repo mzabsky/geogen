0c4dc912bbfaa349c01bfe4e33f4992e5969e713
Matěj Zábský <mzabsky@gmail.com>
2016-06-23 21:49:41 +0200

    Added stdint.h directly to antlr headers

--- src/antlr3/antlr3.h
+++ src/antlr3/antlr3.h
@@ -30,6 +30,8 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
 // THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+#include <stdint.h>
+
 #include    <antlr3defs.h>
 
 #include    <antlr3errors.h>
